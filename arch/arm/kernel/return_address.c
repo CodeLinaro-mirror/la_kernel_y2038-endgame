e02107af80a0fc26a7a88f02dc284ddd84d4f93f
sahara <keun-o.park@windriver.com>
2013-01-03 19:12:29 +0900

    [HACK] ARM: make return_address available for ARM_UNWIND
    This is a reminder that we still need to fix the return_address
    function to work correctly with the unwinder. Keun-O Park has
    made this attempt in the past, which is still under discussion[1],
    and Dave Martin has also mentioned that he would provide a
    solution for this problem.
    
    Right now, this patch makes the warning go away and provides
    an implementation of return_address for the arm unwinder, but
    causes other problems, so we should *not* apply it. I will
    keep sending this patch until we have a better solution.
    
    [1] http://lkml.org/lkml/2013/1/11/493
    
    Original changelog:
    
    This fixes a warning saying:
    
        warning: #warning "TODO: return_address should use unwind tables"
    
    And, this enables return_address using unwind information. If ARM_UNWIND is
    selected, unwind_frame in unwind.c will be called in walk_stackframe.
    
    Signed-off-by: sahara <keun-o.park@windriver.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Dave Martin <dave.martin@linaro.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Russell King <linux@arm.linux.org.uk>

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -11,7 +11,7 @@
 #include <linux/export.h>
 #include <linux/ftrace.h>
 
-#if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND)
+#if defined(CONFIG_FRAME_POINTER) || defined(CONFIG_ARM_UNWIND)
 #include <linux/sched.h>
 
 #include <asm/stacktrace.h>
@@ -56,8 +56,13 @@ void *return_address(unsigned int level)
 		return NULL;
 }
 
-#else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
+#else /* CONFIG_FRAME_POINTER || CONFIG_ARM_UNWIND */
 
-#endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
+void *return_address(unsigned int level)
+{
+	return NULL;
+}
+
+#endif /* CONFIG_FRAME_POINTER || CONFIG_ARM_UNWIND */
 
 EXPORT_SYMBOL_GPL(return_address);
