83fccf75880b53d90a2a6ad7e90c38fdcad0f209
Arnd Bergmann <arnd@arndb.de>
2014-01-21 09:32:04 +0100

    [drop] allow modular clock providers (for msm)

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -733,6 +733,7 @@ struct clk *__clk_lookup(const char *name)
 
 	return NULL;
 }
+EXPORT_SYMBOL_GPL(__clk_lookup);
 
 /*
  * Helper for finding best parent to provide a given frequency. This can be used
