3aa783918801fe30c1df5cc3c4442fc7e69a8b74
Arnd Bergmann <arnd@arndb.de>
2014-01-28 21:17:41 +0100

    [HACK] ARM: normalize clock API
    Platforms that don't yet use the common clock API should
    at least provide all the functions that may be used by device
    drivers that can be enabled on these platforms. This adds
    stub functions for all interfaces I could find. The drivers
    won't be used on these platforms, so the code is harmless
    bloat and will warn about accidental use.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-mmp/clock.c
+++ arch/arm/mach-mmp/clock.c
@@ -103,3 +103,25 @@ int clk_set_rate(struct clk *clk, unsigned long rate)
 	return ret;
 }
 EXPORT_SYMBOL(clk_set_rate);
+
+/* Dummy clk routine to build generic kernel parts that may be using them */
+long clk_round_rate(struct clk *clk, unsigned long rate)
+{
+        WARN_ON(clk);
+        return 0;
+}
+EXPORT_SYMBOL(clk_round_rate);
+
+int clk_set_parent(struct clk *clk, struct clk *parent)
+{
+	WARN_ON(clk);
+        return 0;
+}
+EXPORT_SYMBOL(clk_set_parent);
+
+struct clk *clk_get_parent(struct clk *clk)
+{
+	WARN_ON(clk);
+        return NULL;
+}
+EXPORT_SYMBOL(clk_get_parent);
--- arch/arm/mach-sa1100/clock.c
+++ arch/arm/mach-sa1100/clock.c
@@ -35,6 +35,35 @@ struct clk clk_##_name = {				\
 
 static DEFINE_SPINLOCK(clocks_lock);
 
+/* Dummy clk routine to build generic kernel parts that may be using them */
+long clk_round_rate(struct clk *clk, unsigned long rate)
+{
+        WARN_ON(clk);
+        return 0;
+}
+EXPORT_SYMBOL(clk_round_rate);
+
+int clk_set_rate(struct clk *clk, unsigned long rate)
+{
+        WARN_ON(clk);
+        return 0;
+}
+EXPORT_SYMBOL(clk_set_rate);
+
+int clk_set_parent(struct clk *clk, struct clk *parent)
+{
+	WARN_ON(clk);
+        return 0;
+}       
+EXPORT_SYMBOL(clk_set_parent);
+
+struct clk *clk_get_parent(struct clk *clk)
+{
+	WARN_ON(clk);
+        return NULL;
+}       
+EXPORT_SYMBOL(clk_get_parent);
+
 static void clk_gpio27_enable(struct clk *clk)
 {
 	/*
