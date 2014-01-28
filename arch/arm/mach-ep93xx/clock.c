60a799ca142eacc37a64899e7c153b91aa1b3cfc
Arnd Bergmann <arnd@arndb.de>
2014-01-28 21:17:41 +0100

    [HACK] ARM: normalize clock API
    Platforms that don't yet use the common clock API should
    at least provide all the functions that may be used by device
    drivers that can be enabled on these platforms. This adds
    stub functions for all interfaces I could find. The drivers
    won't be used on these platforms, so the code is harmless
    bloat and will warn about accidental use.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ep93xx/clock.c
+++ arch/arm/mach-ep93xx/clock.c
@@ -323,6 +323,27 @@ unsigned long clk_get_rate(struct clk *clk)
 }
 EXPORT_SYMBOL(clk_get_rate);
 
+long clk_round_rate(struct clk *clk, unsigned long rate)
+{
+	WARN_ON(clk);
+	return 0;
+}
+EXPORT_SYMBOL(clk_round_rate);
+
+int clk_set_parent(struct clk *clk, struct clk *parent)
+{
+	WARN_ON(clk);
+        return 0;
+}       
+EXPORT_SYMBOL(clk_set_parent);
+
+struct clk *clk_get_parent(struct clk *clk)
+{
+	WARN_ON(clk);
+        return NULL;
+}       
+EXPORT_SYMBOL(clk_get_parent);
+
 static int set_keytchclk_rate(struct clk *clk, unsigned long rate)
 {
 	u32 val;
