798b2054efc168d91fcc7040c57f569c0ca6f3c6
Arnd Bergmann <arnd@arndb.de>
2014-02-03 17:47:06 +0100

    net: fix randconfig builds
    These are architecture independent patches required for building
    the network subsystems on 3.13. The bugs have likely been reported
    already and will be fixed before this patch makes it to a
    mailing list. If not, verify that the fix is good and come
    up with a proper changelog.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- net/netfilter/nf_tables_inet.c
+++ net/netfilter/nf_tables_inet.c
@@ -22,7 +22,7 @@ static void nft_inet_hook_ops_init(struct nf_hook_ops *ops, unsigned int n)
 
 	if (n == 1)
 		afi = &nft_af_ipv4;
-	else
+	else if(IS_ENABLED(CONFIG_IPV6))
 		afi = &nft_af_ipv6;
 
 	ops->pf = afi->family;
