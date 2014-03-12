3d01d05a4fa98e1dae8077488671eefbe98f20e6
Arnd Bergmann <arnd@arndb.de>
2014-03-12 15:44:37 +0100

    net: fix e100e build
    I'm sure this gets caught easily by the zero-day autobuilder and
    will be fixed before I have a chance to submit it myself.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/net/ethernet/intel/e1000e/netdev.c
+++ drivers/net/ethernet/intel/e1000e/netdev.c
@@ -6255,7 +6255,6 @@ static int e1000e_pm_thaw(struct device *dev)
 	return 0;
 }
 
-#ifdef CONFIG_PM_SLEEP
 static int e1000e_pm_suspend(struct device *dev)
 {
 	struct pci_dev *pdev = to_pci_dev(dev);
@@ -6276,7 +6275,6 @@ static int e1000e_pm_resume(struct device *dev)
 
 	return e1000e_pm_thaw(dev);
 }
-#endif /* CONFIG_PM_SLEEP */
 
 #ifdef CONFIG_PM_RUNTIME
 static int e1000e_pm_runtime_idle(struct device *dev)
