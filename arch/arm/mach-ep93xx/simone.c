778ab3308a5d7d453ac6116f2a14b95b320a9c7c
Arnd Bergmann <arnd@arndb.de>
2014-03-26 00:30:01 +0100

    ARM: ep93xx: enable SPARSE_IRQ
    As a preparation for multiplatform support, this turns on CONFIG_SPARSE_IRQ
    and moves the irqs.h header file into the machine directory, out of the
    visibility of device drivers. To retain the previous behavior, we set the
    nr_irqs value in each board file.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ep93xx/simone.c
+++ arch/arm/mach-ep93xx/simone.c
@@ -80,6 +80,7 @@ static void __init simone_init_machine(void)
 MACHINE_START(SIM_ONE, "Simplemachines Sim.One Board")
 	/* Maintainer: Ryan Mallon */
 	.atag_offset	= 0x100,
+	.nr_irqs	= 120,
 	.map_io		= ep93xx_map_io,
 	.init_irq	= ep93xx_init_irq,
 	.init_time	= ep93xx_timer_init,
