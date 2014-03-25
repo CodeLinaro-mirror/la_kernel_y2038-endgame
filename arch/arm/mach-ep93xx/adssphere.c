a7c93439743059bc8a844242313f51012abdd731
Arnd Bergmann <arnd@arndb.de>
2014-03-26 00:30:01 +0100

    ARM: ep93xx: enable SPARSE_IRQ
    As a preparation for multiplatform support, this turns on CONFIG_SPARSE_IRQ
    and moves the irqs.h header file into the machine directory, out of the
    visibility of device drivers. To retain the previous behavior, we set the
    nr_irqs value in each board file.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ep93xx/adssphere.c
+++ arch/arm/mach-ep93xx/adssphere.c
@@ -36,6 +36,7 @@ static void __init adssphere_init_machine(void)
 MACHINE_START(ADSSPHERE, "ADS Sphere board")
 	/* Maintainer: Lennert Buytenhek <buytenh@wantstofly.org> */
 	.atag_offset	= 0x100,
+	.nr_irqs	= 120,
 	.map_io		= ep93xx_map_io,
 	.init_irq	= ep93xx_init_irq,
 	.init_time	= ep93xx_timer_init,
