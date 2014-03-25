6780df2f1d57f45078370d3ada3ec8c9a94386e6
Arnd Bergmann <arnd@arndb.de>
2014-03-25 22:53:08 +0100

    ARM: gemini: enable multiplatform support
    The gemini platform doesn't do anything spectacular at all,
    and all the headers are used only locally once we turn on
    ARCH_MULTIPLATFORM.
    
    This moves the headers in the platform directory itself,
    changes the irq handling to MULTI_IRQ_HANDLER, and moves
    the Kconfig entry under ARCH_MULTIPLATFORM.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-gemini/devices.c
+++ arch/arm/mach-gemini/devices.c
@@ -14,9 +14,9 @@
 #include <linux/serial_8250.h>
 #include <linux/mtd/physmap.h>
 
-#include <mach/irqs.h>
-#include <mach/hardware.h>
-#include <mach/global_reg.h>
+#include "irqs.h"
+#include "hardware.h"
+#include "global_reg.h"
 
 static struct plat_serial8250_port serial_platform_data[] = {
 	{
