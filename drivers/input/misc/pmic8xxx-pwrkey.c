eb5508e78089980a8aaef0ca340e70dec1406c03
Arnd Bergmann <arnd@arndb.de>
2014-04-02 09:29:57 +0200

    input: pmic8xxx-pwrkey: get it to build without CONFIG_OF
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/input/misc/pmic8xxx-pwrkey.c
+++ drivers/input/misc/pmic8xxx-pwrkey.c
@@ -92,7 +92,7 @@ static int pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 	bool pull_up;
 
 	if (of_property_read_u32(pdev->dev.of_node, "debounce", &kpd_delay))
-		kpd_delay = 0;
+		kpd_delay = 15625;
 
 	pull_up = of_property_read_bool(pdev->dev.of_node, "pull-up");
 
