366bb48030d2bb650b0a47a07769ede6421c3fc0
Arnd Bergmann <arnd@arndb.de>
2014-04-11 20:45:31 +0200

    Revert "[INPROGRESS] unsorted driver fixes"
    This reverts commit 01babe60f8a09785fd2e419762f4846fe5c7369a.

--- drivers/mmc/host/sdhci-bcm-kona.c
+++ drivers/mmc/host/sdhci-bcm-kona.c
@@ -363,7 +363,7 @@ static struct platform_driver sdhci_bcm_kona_driver = {
 		.of_match_table = sdhci_bcm_kona_of_match,
 	},
 	.probe		= sdhci_bcm_kona_probe,
-	.remove		= __exit_p(sdhci_bcm_kona_remove),
+	.remove		= sdhci_bcm_kona_remove,
 };
 module_platform_driver(sdhci_bcm_kona_driver);
 
