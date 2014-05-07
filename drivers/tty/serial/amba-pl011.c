3d899c7b12af063c2433a9e2574caec7e4780271
Arnd Bergmann <arnd@arndb.de>
2014-05-07 15:39:50 +0200

    Revert "serial: pl011: fix build without SYSRQ"
    This reverts commit 35754b4c0014ea4cebfa9211b3236d57a66f9b6d.

--- drivers/tty/serial/amba-pl011.c
+++ drivers/tty/serial/amba-pl011.c
@@ -1974,12 +1974,9 @@ pl011_console_write(struct console *co, const char *s, unsigned int count)
 	clk_enable(uap->clk);
 
 	local_irq_save(flags);
-#ifdef SUPPORT_SYSRQ
 	if (uap->port.sysrq)
 		locked = 0;
-	else
-#endif
-	if (oops_in_progress)
+	else if (oops_in_progress)
 		locked = spin_trylock(&uap->port.lock);
 	else
 		spin_lock(&uap->port.lock);
