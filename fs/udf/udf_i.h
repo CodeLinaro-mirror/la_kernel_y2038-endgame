f67cb3dc0a53f737f80684615cbdd86aea90f68b
Arnd Bergmann <arnd@arndb.de>
2014-05-28 18:06:33 +0200

    udf: convert to struct inode_time
    udf uses 16-bit year numbers for inode timestamps, which will work
    for the 15000 years, but the VFS uses struct timespec for timestamps,
    and the Linux udf implementation internally uses a time_t, both of
    which are only good until 2038 on 32-bit CPUs.
    
    This gets us one small step closer to lifting the VFS limit by using
    struct inode_time in udf, but the implementation still depends on
    the 'year_seconds' array that needs to be extended or replaced by
    an algorithm that calculates the correct time for every year.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Jan Kara <jack@suse.cz>

--- fs/udf/udf_i.h
+++ fs/udf/udf_i.h
@@ -27,7 +27,7 @@ struct udf_ext_cache {
  */
 
 struct udf_inode_info {
-	struct timespec		i_crtime;
+	struct inode_time	i_crtime;
 	/* Physical address of inode */
 	struct kernel_lb_addr		i_location;
 	__u64			i_unique;
