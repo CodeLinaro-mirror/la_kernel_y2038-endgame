8f7951aa91e6e11b73224a07e9c3a91fa79429a2
Arnd Bergmann <arnd@arndb.de>
2014-06-04 08:07:16 +0200

    ARM: exynos: fixup for sysram_ns_base_addr reference
    arch/arm/mach-exynos/built-in.o: In function `exynos_set_cpu_boot_addr':
    :(.text+0x10c): undefined reference to `sysram_ns_base_addr'
    :(.text+0x110): undefined reference to `sysram_ns_base_addr'
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-exynos/firmware.c
+++ arch/arm/mach-exynos/firmware.c
@@ -80,7 +80,7 @@ static int exynos_set_cpu_boot_addr(int cpu, unsigned long boot_addr)
 {
 	void __iomem *boot_reg;
 
-	if (!sysram_ns_base_addr)
+	if (!IS_ENABLED(CONFIG_SMP) || !sysram_ns_base_addr)
 		return -ENODEV;
 
 	boot_reg = sysram_ns_base_addr + 0x1c;
