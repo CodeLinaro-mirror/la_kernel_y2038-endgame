cc0f77bbaaea9084de3bb18d2812e6d775b651fd
Arnd Bergmann <arnd@arndb.de>
2014-06-05 16:56:36 +0200

    ARM: at91: remove NEED_MACH_IO_H
    The mach/io.h header on at91 is used to support a nonstandard I/O space
    window for the cf card driver. This changes the driver to use pci_ioremap_io
    in order to have the standard location, and then removes the custom
    mach/io.h.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/pcmcia/at91_cf.c
+++ drivers/pcmcia/at91_cf.c
@@ -318,12 +318,10 @@ static int at91_cf_probe(struct platform_device *pdev)
 		cf->socket.pci_irq = nr_irqs + 1;
 
 	/* pcmcia layer only remaps "real" memory not iospace */
-	cf->socket.io_offset = (unsigned long) devm_ioremap(&pdev->dev,
-					cf->phys_baseaddr + CF_IO_PHYS, SZ_2K);
-	if (!cf->socket.io_offset) {
-		status = -ENXIO;
+	cf->socket.io_offset = 0;
+	status = pci_ioremap_io(0, cf->phys_baseaddr + CF_IO_PHYS);
+	if (status)
 		goto fail0a;
-	}
 
 	/* reserve chip-select regions */
 	if (!devm_request_mem_region(&pdev->dev, io->start, resource_size(io), "at91_cf")) {
