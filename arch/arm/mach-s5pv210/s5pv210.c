5a4baf148baa8b209a529b2ebaf44cf0e1ca92f9
Arnd Bergmann <arnd@arndb.de>
2014-07-04 15:14:26 +0200

    ARM: s5pv210: change files to local names
    This is a preparation to the move into the Exynos platform,
    there should be no visible changes
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-s5pv210/s5pv210.c
+++ arch/arm/mach-s5pv210/s5pv210.c
@@ -17,10 +17,8 @@
 #include <asm/mach/map.h>
 #include <asm/system_misc.h>
 
-#include <plat/map-base.h>
-
-#include "common.h"
-#include "regs-clock.h"
+#include "s5pv210-clock.h"
+#include "s5pv210.h"
 
 static int __init s5pv210_fdt_map_sys(unsigned long node, const char *uname,
 					int depth, void *data)
