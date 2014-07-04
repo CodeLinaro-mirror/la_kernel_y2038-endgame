36b728f711a25c76273e4f60b39128af768aa091
Arnd Bergmann <arnd@arndb.de>
2014-07-04 16:44:11 +0200

    ARM: exynos: move map*.h headers to mach-exynos
    This untangles the map-base.h, map-s5p.h and map-s3c.h header
    files and moves the regs-srom.h header so that exynos stops
    including most of plat/*.h.
    
    We do get a duplicated S3C_ADDR_BASE definition in the process
    that has to match between plat-samsung and mach-exynos, but that
    will get fixed in one of the follow-up patches as we further
    separate the two directories.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-exynos/pm.c
+++ arch/arm/mach-exynos/pm.c
@@ -25,6 +25,7 @@
 
 #include <plat/pm-common.h>
 #include "common.h"
+#include "regs-srom.h"
 #include "regs-pmu.h"
 #include "regs-sys.h"
 
--- arch/arm/mach-exynos/s5pv210-clock.h
+++ arch/arm/mach-exynos/s5pv210-clock.h
@@ -12,7 +12,7 @@
 #ifndef __ASM_ARCH_REGS_CLOCK_H
 #define __ASM_ARCH_REGS_CLOCK_H __FILE__
 
-#include <plat/map-base.h>
+#include "map.h"
 
 #define S5P_CLKREG(x)		(S3C_VA_SYS + (x))
 
