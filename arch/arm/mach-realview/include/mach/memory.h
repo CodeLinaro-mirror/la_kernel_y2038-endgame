b0606f705440a608e35ad521e23ee9f6ff0ed368
Arnd Bergmann <arnd@arndb.de>
2014-07-08 16:46:56 +0200

    Revert "[EXPERIMENTAL] ARM: realview: move sparsemem hack into a separate option"
    This reverts commit 93e841ec016ca39bd62af7441942072dc52a1306.

--- arch/arm/mach-realview/include/mach/memory.h
+++ arch/arm/mach-realview/include/mach/memory.h
@@ -22,11 +22,6 @@
 
 #ifdef CONFIG_SPARSEMEM
 
-/* these can be moved to arm/memory.h when we get to support multiplatform */
-#define MAX_PHYSMEM_BITS	32
-#define SECTION_SIZE_BITS	28
-
-#ifdef CONFIG_REALVIEW_PBX_SPARSEMEM_HACK
 /*
  * Sparsemem definitions for RealView PBX.
  *
@@ -41,6 +36,13 @@
  * 512MB @ 0x20000000 -> PAGE_OFFSET + 0x10000000
  * 256MB @ 0x80000000 -> PAGE_OFFSET + 0x30000000
  */
+#ifdef CONFIG_REALVIEW_HIGH_PHYS_OFFSET
+#error "SPARSEMEM not available with REALVIEW_HIGH_PHYS_OFFSET"
+#endif
+
+#define MAX_PHYSMEM_BITS	32
+#define SECTION_SIZE_BITS	28
+
 /* bank page offsets */
 #define PAGE_OFFSET1	(PAGE_OFFSET + 0x10000000)
 #define PAGE_OFFSET2	(PAGE_OFFSET + 0x30000000)
@@ -56,8 +58,6 @@
 	 ((virt) >= PAGE_OFFSET2 ? (virt) - PAGE_OFFSET2 + 0x80000000 :	\
 	  (virt) >= PAGE_OFFSET1 ? (virt) - PAGE_OFFSET1 + 0x20000000 :	\
 	  (virt) - PAGE_OFFSET)
-
-#endif
 
 #endif	/* CONFIG_SPARSEMEM */
 
