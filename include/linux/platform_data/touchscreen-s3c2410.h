4fd459c94230546ff8d359eef3d307d4c37f294c
Arnd Bergmann <arnd@arndb.de>
2014-07-15 16:38:27 +0200

    samsung: move adc stuff into touchscreen driver
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/platform_data/touchscreen-s3c2410.h
+++ include/linux/platform_data/touchscreen-s3c2410.h
@@ -9,6 +9,23 @@
 #ifndef __TOUCHSCREEN_S3C2410_H
 #define __TOUCHSCREEN_S3C2410_H
 
+struct s3c_adc_client;
+struct platform_device;
+
+extern int s3c_adc_read(struct s3c_adc_client *client, unsigned int ch);
+
+extern struct s3c_adc_client *
+	s3c_adc_register(struct platform_device *pdev,
+			 void (*select)(struct s3c_adc_client *client,
+					unsigned selected),
+			 void (*conv)(struct s3c_adc_client *client,
+				      unsigned d0, unsigned d1,
+				      unsigned *samples_left),
+			 unsigned int is_ts);
+
+extern void s3c_adc_release(struct s3c_adc_client *client);
+
+
 struct s3c2410_ts_mach_info {
 	int delay;
 	int presc;
