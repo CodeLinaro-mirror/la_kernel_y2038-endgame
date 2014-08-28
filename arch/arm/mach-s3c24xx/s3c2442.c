6101bc3d463956cee04c52835c85bb6de838e4e6
Arnd Bergmann <arnd@arndb.de>
2014-08-28 12:38:00 +0200

    fixup s3c24xx pm_sleep logic

--- arch/arm/mach-s3c24xx/s3c2442.c
+++ arch/arm/mach-s3c24xx/s3c2442.c
@@ -63,8 +63,8 @@ int __init s3c2442_init(void)
 #ifdef CONFIG_PM_SLEEP
 	register_syscore_ops(&s3c2410_pm_syscore_ops);
 	register_syscore_ops(&s3c24xx_irq_syscore_ops);
-#endif
 	register_syscore_ops(&s3c244x_pm_syscore_ops);
+#endif
 
 	return device_register(&s3c2442_dev);
 }
