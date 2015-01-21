65b7fb6056bc87f9a903197f2687edf687596500
Arnd Bergmann <arnd@arndb.de>
2015-01-21 21:48:53 +0100

    Revert "Revert "[HACK] ARM: samsung: use PM_SLEEP instead of PM again""
    This reverts commit 72c27ad9a3a81badee8916c99fc44a0b06411fd9.

--- arch/arm/mach-s3c24xx/s3c2440.c
+++ arch/arm/mach-s3c24xx/s3c2440.c
@@ -57,7 +57,7 @@ int __init s3c2440_init(void)
 
 	/* register suspend/resume handlers */
 
-#ifdef CONFIG_PM
+#ifdef CONFIG_PM_SLEEP
 	register_syscore_ops(&s3c2410_pm_syscore_ops);
 	register_syscore_ops(&s3c24xx_irq_syscore_ops);
 	register_syscore_ops(&s3c244x_pm_syscore_ops);
