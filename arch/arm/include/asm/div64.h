0f61f85477340095c859a275e1d3b4e61d67fd4e
Arnd Bergmann <arnd@arndb.de>
2015-01-27 15:14:22 +0100

    ARM: div64: fix building with gcc-4.6 and lower
    drivers/media/dvb-frontends/s921.c: In function 's921_pll_tune':
    arch/arm/include/asm/div64.h:109: error: can't find a register in class 'GENERAL_REGS' while reloading 'asm'
    arch/arm/include/asm/div64.h:127: error: can't find a register in class 'GENERAL_REGS' while reloading 'asm'
    arch/arm/include/asm/div64.h:109: error: 'asm' operand has impossible constraints
    arch/arm/include/asm/div64.h:127: error: 'asm' operand has impossible constraints
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/include/asm/div64.h
+++ arch/arm/include/asm/div64.h
@@ -67,18 +67,15 @@ static inline uint32_t __div64_32(uint64_t *n, uint32_t base)
  */
 #define do_div(n, base) __div64_32(&(n), base)
 
-#else
+#elif GCC_VERSION < 40700
 
 /*
- * gcc versions earlier than 4.0 are simply too problematic for the
- * __div64_const32() code in asm-generic/div64.h. First there is
- * gcc PR 15089 that tend to trig on more complex constructs, spurious
- * .global __udivsi3 are inserted even if none of those symbols are
- * referenced in the generated code, and those gcc versions are not able
- * to do constant propagation on long long values anyway.
+ * gcc-4.4 through gcc-4.6 tend to run out of registers in their
+ * register allocation in some functions
  */
+#define do_div(n, base) __div64_32(&(n), base)
 
-#define __div64_const32_is_OK (__GNUC__ >= 4)
+#else
 
 static inline uint64_t __arch_xprod_64(uint64_t m, uint64_t n, bool bias)
 {
