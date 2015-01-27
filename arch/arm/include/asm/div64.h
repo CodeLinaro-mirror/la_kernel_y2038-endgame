d318d7ea7cba921b9fd3ac230c1a99fa2d16b1a1
Arnd Bergmann <arnd@arndb.de>
2015-01-27 15:14:22 +0100

    ARM: do not use optimized do_div for ARMv3
    The gcc-4.9 optimization goes wrong while building target_core_iblock.c
    for ARMv3 and leaves a bogus reference to __aeabi_uldivmod in the
    output:
    
    ERROR: "__aeabi_uldivmod" [drivers/target/target_core_iblock.ko] undefined!
    
    I could not find anyone who is interested in fixing it in gcc,
    so as a workaround this disables the do_div magic, just like
    we do for old compilers and for OABI.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/include/asm/div64.h
+++ arch/arm/include/asm/div64.h
@@ -46,7 +46,7 @@
 	__rem;							\
 })
 
-#if __GNUC__ < 4 || !defined(CONFIG_AEABI)
+#if __GNUC__ < 4 || !defined(CONFIG_AEABI) || defined(CONFIG_CPU_32v3)
 
 /*
  * gcc versions earlier than 4.0 are simply too problematic for the
