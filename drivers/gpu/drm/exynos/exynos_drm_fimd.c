7d3e8e4ed2aa9cedc12f7ee7bac615f399dfc9a4
Arnd Bergmann <arnd@arndb.de>
2015-02-24 09:59:54 +0100

    [RESEND] drm: exynos: clean up dma_addr_t use
    dma_addr_t may be 32 or 64 bits long on 32-bit CPUs, so we cannot
    cast it to a pointer without getting a compiler warning:
    
    drivers/gpu/drm/exynos/exynos_drm_buf.c: In function 'lowlevel_buffer_allocate':
    drivers/gpu/drm/exynos/exynos_drm_buf.c:109:18: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
      buf->dma_addr = (dma_addr_t)NULL;
                      ^
    
    The use of pointers is wrong here anyway, and so is the cast
    to the same type, and printing the value as a 32-bit instance.
    
    This patch tries for address all these issues in the exynos
    drm driver, by printing the values as %pad and removing the
    bogus type casts.
    
    I've added '& 0xffffffff' masks in a few places I found where
    a dma_addr_t is assigned to a 32-bit value, as a reminder that
    this code will not work with 64-bit dma addresses. There should
    not be any change in the generated code here.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/exynos/exynos_drm_fimd.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimd.c
@@ -659,16 +659,16 @@ static void fimd_win_commit(struct exynos_drm_crtc *crtc, unsigned int win)
 
 	/* buffer start address */
 	dma_addr = plane->dma_addr[0] + offset;
-	val = (unsigned long)dma_addr;
+	val = (u32)dma_addr & 0xffffffff;
 	writel(val, ctx->regs + VIDWx_BUF_START(win, 0));
 
 	/* buffer end address */
 	size = plane->pitch * plane->crtc_height;
-	val = (unsigned long)(dma_addr + size);
+	val = (u32)((dma_addr + size) & 0xffffffff);
 	writel(val, ctx->regs + VIDWx_BUF_END(win, 0));
 
-	DRM_DEBUG_KMS("start addr = 0x%lx, end addr = 0x%lx, size = 0x%lx\n",
-			(unsigned long)dma_addr, val, size);
+	DRM_DEBUG_KMS("start addr = %pad, end addr = %pad, size = 0x%lx\n",
+			&dma_addr, &val, size);
 	DRM_DEBUG_KMS("ovl_width = %d, ovl_height = %d\n",
 			plane->crtc_width, plane->crtc_height);
 
