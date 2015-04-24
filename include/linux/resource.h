1c2bfbc161c5f8fa5b4d1ac46d8262f97dc787b5
Arnd Bergmann <arnd@arndb.de>
2015-04-24 17:05:17 +0200

    y2038: introduce struct __kernel_rusage
    'struct rusage' is not compatible with user space that defines time_t
    as 64-bit. While there will never be an overflow of the timeval
    members in this structure, the current definition breaks any program
    that relies on the member to be a timeval.
    
    This introduces a new struct __kernel_rusage that is defined to match
    the 64-bit version of struct rusage. 32-bit architectures that use
    CONFIG_COMPAT_TIME can then use compat_sys_getrusage() etc to get
    the original structure, while the normal sys_getrusage() function
    will provide the new layout in both native and compat32 mode.
    
    On 32-bit architectures that do not set CONFIG_COMPAT_TIME, as well
    as all 64-bit architectures, this patch is intented to have no
    user-visible impact.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/resource.h
+++ include/linux/resource.h
@@ -1,12 +1,16 @@
 #ifndef _LINUX_RESOURCE_H
 #define _LINUX_RESOURCE_H
 
-#include <uapi/linux/resource.h>
+#ifndef CONFIG_COMPAT_TIME
+#define __kernel_rusage rusage
+#endif
 
+#include <uapi/linux/resource.h>
 
 struct task_struct;
 
-int getrusage(struct task_struct *p, int who, struct rusage __user *ru);
+int getrusage(struct task_struct *p, int who,
+	      struct __kernel_rusage __user *ru);
 int do_prlimit(struct task_struct *tsk, unsigned int resource,
 		struct rlimit *new_rlim, struct rlimit *old_rlim);
 
