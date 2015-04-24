a3abdae9cacd3db7545bff53c4815b3616298ff9
Arnd Bergmann <arnd@arndb.de>
2015-04-24 17:05:17 +0200

    y2038: introduce and use struct __kernel_rusage
    'struct rusage' is not compatible with user space that defines time_t
    as 64-bit. While there will never be an overflow of the timeval
    members in this structure, the current definition breaks any program
    that relies on the member to be a timeval.
    
    This introduces a new struct __kernel_rusage that is defined to match
    the 64-bit version of struct rusage. 32-bit architectures that use
    CONFIG_COMPAT_TIME can then use compat_sys_getrusage() etc to get
    the original structure, while the normal sys_getrusage() function
    will provide the new layout in both native and compat32 mode.
    
    On 32-bit architectures that do not set CONFIG_COMPAT_TIME, as well
    as all 64-bit architectures, this patch is intented to have no
    user-visible impact.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1529,7 +1529,7 @@ SYSCALL_DEFINE2(setrlimit, unsigned int, resource, struct rlimit __user *, rlim)
  *
  */
 
-static void accumulate_thread_rusage(struct task_struct *t, struct rusage *r)
+static void accumulate_thread_rusage(struct task_struct *t, struct __kernel_rusage *r)
 {
 	r->ru_nvcsw += t->nvcsw;
 	r->ru_nivcsw += t->nivcsw;
@@ -1539,12 +1539,13 @@ static void accumulate_thread_rusage(struct task_struct *t, struct rusage *r)
 	r->ru_oublock += task_io_get_oublock(t);
 }
 
-static void k_getrusage(struct task_struct *p, int who, struct rusage *r)
+static void k_getrusage(struct task_struct *p, int who, struct __kernel_rusage *r)
 {
 	struct task_struct *t;
 	unsigned long flags;
 	cputime_t tgutime, tgstime, utime, stime;
 	unsigned long maxrss = 0;
+	struct timeval tv;
 
 	memset((char *)r, 0, sizeof (*r));
 	utime = stime = 0;
@@ -1599,8 +1600,12 @@ static void k_getrusage(struct task_struct *p, int who, struct rusage *r)
 	unlock_task_sighand(p, &flags);
 
 out:
-	cputime_to_timeval(utime, &r->ru_utime);
-	cputime_to_timeval(stime, &r->ru_stime);
+	cputime_to_timeval(utime, &tv);
+	r->ru_utime.tv_sec = tv.tv_sec;
+	r->ru_utime.tv_usec = tv.tv_usec;
+	cputime_to_timeval(stime, &tv);
+	r->ru_stime.tv_sec = tv.tv_sec;
+	r->ru_stime.tv_usec = tv.tv_usec;
 
 	if (who != RUSAGE_CHILDREN) {
 		struct mm_struct *mm = get_task_mm(p);
@@ -1613,15 +1618,15 @@ out:
 	r->ru_maxrss = maxrss * (PAGE_SIZE / 1024); /* convert pages to KBs */
 }
 
-int getrusage(struct task_struct *p, int who, struct rusage __user *ru)
+int getrusage(struct task_struct *p, int who, struct __kernel_rusage __user *ru)
 {
-	struct rusage r;
+	struct __kernel_rusage r;
 
 	k_getrusage(p, who, &r);
 	return copy_to_user(ru, &r, sizeof(r)) ? -EFAULT : 0;
 }
 
-SYSCALL_DEFINE2(getrusage, int, who, struct rusage __user *, ru)
+SYSCALL_DEFINE2(getrusage, int, who, struct __kernel_rusage __user *, ru)
 {
 	if (who != RUSAGE_SELF && who != RUSAGE_CHILDREN &&
 	    who != RUSAGE_THREAD)
@@ -1629,10 +1634,10 @@ SYSCALL_DEFINE2(getrusage, int, who, struct rusage __user *, ru)
 	return getrusage(current, who, ru);
 }
 
-#ifdef CONFIG_COMPAT
+#ifdef CONFIG_COMPAT_TIME
 COMPAT_SYSCALL_DEFINE2(getrusage, int, who, struct compat_rusage __user *, ru)
 {
-	struct rusage r;
+	struct __kernel_rusage r;
 
 	if (who != RUSAGE_SELF && who != RUSAGE_CHILDREN &&
 	    who != RUSAGE_THREAD)
