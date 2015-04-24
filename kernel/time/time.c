275d52eb961b0a4315203326093adcac49074a91
Arnd Bergmann <arnd@arndb.de>
2015-04-24 17:41:44 +0200

    y2038: introduce struct __kernel_timespec
    A lot of system calls pass a 'struct timespec' from or to user space,
    and we want to change that type to be based on a 64-bit time_t by
    default.
    
    This introduces a new type struct __kernel_timespec, which has the
    format we want to use eventually, but also has an override so all
    architectures that do not define CONFIG_COMPAT_TIME yet still get the
    old behavior.
    Once all architectures set this, we can remove that override.
    
    This also introduces a get_timespec64/put_timespec64 set of functions
    that convert between a __kernel_timespec in user space and a timespec64
    in kernel space.
    
    The current behavior of get_timespec64 explicitly zeroes the upper half
    of the tv_nsec member, to allow user space to define its own 'struct
    timespec' with some padding in it. Whether this is a good or bad idea
    is open for discussion.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/time/time.c
+++ kernel/time/time.c
@@ -30,6 +30,7 @@
 #include <linux/export.h>
 #include <linux/timex.h>
 #include <linux/capability.h>
+#include <linux/compat.h>
 #include <linux/timekeeper_internal.h>
 #include <linux/errno.h>
 #include <linux/syscalls.h>
@@ -37,6 +38,7 @@
 #include <linux/fs.h>
 #include <linux/math64.h>
 #include <linux/ptrace.h>
+#include <linux/time64.h>
 
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
@@ -790,3 +792,33 @@ struct timespec64 timespec64_add_safe(const struct timespec64 lhs,
 
 	return res;
 }
+
+int get_timespec64(struct timespec64 *ts,
+		   const struct __kernel_timespec __user *uts)
+{
+	struct __kernel_timespec kts;
+	int ret;
+
+	ret = copy_from_user(&kts, uts, sizeof(kts));
+	if (ret)
+		return -EFAULT;
+
+	ts->tv_sec = kts.tv_sec;
+	if (!IS_ENABLED(CONFIG_64BIT) || in_compat_syscall())
+		kts.tv_nsec &= 0xfffffffful;
+	ts->tv_nsec = kts.tv_nsec;
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(get_timespec64);
+
+int put_timespec64(const struct timespec64 *ts,
+		   struct __kernel_timespec __user *uts)
+{
+	struct __kernel_timespec kts = {
+		.tv_sec = ts->tv_sec,
+		.tv_nsec = ts->tv_nsec
+	};
+	return copy_to_user(uts, &kts, sizeof(kts)) ? -EFAULT : 0;
+}
+EXPORT_SYMBOL_GPL(put_timespec64);
