a1eec1141618b29ccfcf6e50a93d0467dca0e774
Arnd Bergmann <arnd@arndb.de>
2015-04-30 17:04:46 +0200

    y2038: make linux/compat_time.h usable on 32-bit
    This adds the necessary type definitions to allow using the
    linux/compat_time.h header file on 32-bit architectures without
    adding an asm/compat.h header in each architecture.
    
    The types we define here are used in the compat system call
    definitions of some system calls that also pass a time_t
    in another structure, but are themselves not depending on the
    size of time_t.
    
    This also adds a way to use COMPAT_SYSCALL_DEFINE() by
    including the compat.h header file. Some code may need to
    get reshuffled here.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/compat.h
+++ include/linux/compat.h
@@ -5,8 +5,6 @@
  * syscall compatibility layer.
  */
 
-#ifdef CONFIG_COMPAT
-
 #include <linux/stat.h>
 #include <linux/param.h>	/* for HZ */
 #include <linux/sem.h>
@@ -17,7 +15,9 @@
 #include <linux/unistd.h>
 #include <linux/compat_time.h>
 
+#ifdef CONFIG_COMPAT
 #include <asm/compat.h>
+#endif
 #include <asm/siginfo.h>
 #include <asm/signal.h>
 
@@ -51,6 +51,8 @@
 		return C_SYSC##name(__MAP(x,__SC_DELOUSE,__VA_ARGS__));	\
 	}								\
 	static inline long C_SYSC##name(__MAP(x,__SC_DECL,__VA_ARGS__))
+
+#ifdef CONFIG_COMPAT
 
 #ifndef compat_user_stack_pointer
 #define compat_user_stack_pointer() current_user_stack_pointer()
