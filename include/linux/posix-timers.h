aa439f9d53f72c33b399a1627f0f251096a85249
Arnd Bergmann <arnd@arndb.de>
2015-04-30 23:13:21 +0200

    y2038: introduce {get,put}_itimerspec
    As we change the user space type for the timerfd and posix timer
    functions to __kernel_itimerspec, we need some form of conversion
    helpers to avoid duplicating that logic.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/posix-timers.h
+++ include/linux/posix-timers.h
@@ -111,6 +111,11 @@ void set_process_cpu_timer(struct task_struct *task, unsigned int clock_idx,
 			   u64 *newval, u64 *oldval);
 
 void update_rlimit_cpu(struct task_struct *task, unsigned long rlim_new);
+int get_itimerspec(struct itimerspec *it,
+		   const struct __kernel_itimerspec __user *uit);
+int put_itimerspec(const struct itimerspec *it,
+		   struct __kernel_itimerspec __user *uit);
+
 
 void posixtimer_rearm(struct siginfo *info);
 #endif
--- kernel/time/posix-timers.c
+++ kernel/time/posix-timers.c
@@ -764,6 +764,40 @@ COMPAT_SYSCALL_DEFINE2(timer_gettime, timer_t, timer_id,
 }
 #endif
 
+int get_itimerspec(struct itimerspec64 *it, const struct __kernel_itimerspec __user *uit)
+{
+	int ret;
+	struct timespec64 ts;
+
+	ret = get_timespec64(&ts, &uit->it_interval);
+	if (ret)
+		return ret;
+	it->it_interval = timespec64_to_timespec(ts);
+
+	ret = get_timespec64(&ts, &uit->it_value);
+	if (ret)
+		return ret;
+	it->it_value = timespec64_to_timespec(ts);
+
+	return ret;
+}
+
+int put_itimerspec(const struct itimerspec *it, struct __kernel_itimerspec __user *uit)
+{
+	int ret;
+	struct timespec64 ts;
+
+	ts = timespec_to_timespec64(it->it_interval);
+	ret = put_timespec64(&ts, &uit->it_interval);
+	if (ret)
+		return ret;
+
+	ts = timespec_to_timespec64(it->it_value);
+	ret = put_timespec64(&ts, &uit->it_value);
+
+	return ret;
+}
+
 /*
  * Get the number of overruns of a POSIX.1b interval timer.  This is to
  * be the overrun of the timer last delivered.  At the same time we are
