97025d075a4f35c8f72daf4b3b9cc5d931bf6589
Arnd Bergmann <arnd@arndb.de>
2015-04-30 23:13:21 +0200

    y2038: introduce {get,put}_itimerspec
    As we change the user space type for the timerfd and posix timer
    functions to __kernel_itimerspec, we need some form of conversion
    helpers to avoid duplicating that logic.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/time/posix-timers.c
+++ kernel/time/posix-timers.c
@@ -801,6 +801,40 @@ SYSCALL_DEFINE2(timer_gettime, timer_t, timer_id,
 	return ret;
 }
 
+int get_itimerspec(struct itimerspec *it, const struct __kernel_itimerspec __user *uit)
+{
+	int ret;
+	struct timespec64 ts;
+
+	ret = get_timespec64(&ts, &uit->it_interval);
+	if (ret)
+		return ret;
+	it->it_interval = timespec64_to_timespec(ts);
+
+	ret = get_timespec64(&ts, &uit->it_value);
+	if (ret)
+		return ret;
+	it->it_value = timespec64_to_timespec(ts);
+
+	return ret;
+}
+
+int put_itimerspec(const struct itimerspec *it, struct __kernel_itimerspec __user *uit)
+{
+	int ret;
+	struct timespec64 ts;
+
+	ts = timespec_to_timespec64(it->it_interval);
+	ret = put_timespec64(&ts, &uit->it_interval);
+	if (ret)
+		return ret;
+
+	ts = timespec_to_timespec64(it->it_value);
+	ret = put_timespec64(&ts, &uit->it_value);
+
+	return ret;
+}
+
 /*
  * Get the number of overruns of a POSIX.1b interval timer.  This is to
  * be the overrun of the timer last delivered.  At the same time we are
