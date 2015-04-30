0e60246040a0c8511dc2b4940a6150ca353f1bbb
Arnd Bergmann <arnd@arndb.de>
2015-05-01 00:28:42 +0200

    y2038: use __kernel_timespec for sys_mq_timed{send,receive}
    This performs a complete conversion of the ipc/mqueue code to
    use 64-bit time_t both internally and on the user-facing side
    of 32-bit architectures that use CONFIG_COMPAT_TIME. For
    compatibility with existing user space, these now use
    compat_sys_mq_timed{send,receive}.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- ipc/mqueue.c
+++ ipc/mqueue.c
@@ -663,15 +663,15 @@ static void __do_notify(struct mqueue_inode_info *info)
 	wake_up(&info->wait_q);
 }
 
-static int prepare_timeout(const struct timespec __user *u_abs_timeout,
-			   ktime_t *expires, struct timespec *ts)
+static int prepare_timeout(const struct __kernel_timespec __user *u_abs_timeout,
+			   ktime_t *expires, struct timespec64 *ts)
 {
-	if (copy_from_user(ts, u_abs_timeout, sizeof(struct timespec)))
+	if (get_timespec64(ts, u_abs_timeout))
 		return -EFAULT;
 	if (!timespec_valid(ts))
 		return -EINVAL;
 
-	*expires = timespec_to_ktime(*ts);
+	*expires = timespec64_to_ktime(*ts);
 	return 0;
 }
 
@@ -955,7 +955,7 @@ static inline void pipelined_receive(struct wake_q_head *wake_q,
 
 SYSCALL_DEFINE5(mq_timedsend, mqd_t, mqdes, const char __user *, u_msg_ptr,
 		size_t, msg_len, unsigned int, msg_prio,
-		const struct timespec __user *, u_abs_timeout)
+		const struct __kernel_timespec __user *, u_abs_timeout)
 {
 	struct fd f;
 	struct inode *inode;
@@ -964,7 +964,7 @@ SYSCALL_DEFINE5(mq_timedsend, mqd_t, mqdes, const char __user *, u_msg_ptr,
 	struct msg_msg *msg_ptr;
 	struct mqueue_inode_info *info;
 	ktime_t expires, *timeout = NULL;
-	struct timespec ts;
+	struct timespec64 ts;
 	struct posix_msg_tree_node *new_leaf = NULL;
 	int ret = 0;
 	WAKE_Q(wake_q);
@@ -1076,7 +1076,7 @@ out:
 
 SYSCALL_DEFINE5(mq_timedreceive, mqd_t, mqdes, char __user *, u_msg_ptr,
 		size_t, msg_len, unsigned int __user *, u_msg_prio,
-		const struct timespec __user *, u_abs_timeout)
+		const struct __kernel_timespec __user *, u_abs_timeout)
 {
 	ssize_t ret;
 	struct msg_msg *msg_ptr;
@@ -1085,7 +1085,7 @@ SYSCALL_DEFINE5(mq_timedreceive, mqd_t, mqdes, char __user *, u_msg_ptr,
 	struct mqueue_inode_info *info;
 	struct ext_wait_queue wait;
 	ktime_t expires, *timeout = NULL;
-	struct timespec ts;
+	struct timespec64 ts;
 	struct posix_msg_tree_node *new_leaf = NULL;
 
 	if (u_abs_timeout) {
