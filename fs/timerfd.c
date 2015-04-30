aedb200cb90c1bee4a9699fada9f5042dc333d12
Arnd Bergmann <arnd@arndb.de>
2015-04-30 23:14:53 +0200

    y2038: use __kernel_itimerspec in timerfd
    32-bit architectures that want to use a 64-bit time_t need a
    definition of timerfd_gettime and timerfd_settime that takes
    __kernel_itimerspec arguments, and use the compat_time syscalls
    for the legacy support.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -11,6 +11,7 @@
 #include <linux/alarmtimer.h>
 #include <linux/file.h>
 #include <linux/poll.h>
+#include <linux/posix-timers.h>
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/sched.h>
@@ -532,8 +533,8 @@ static int do_timerfd_gettime(int ufd, struct itimerspec64 *t)
 }
 
 SYSCALL_DEFINE4(timerfd_settime, int, ufd, int, flags,
-		const struct itimerspec __user *, utmr,
-		struct itimerspec __user *, otmr)
+		const struct __kernel_itimerspec __user *, utmr,
+		struct __kernel_itimerspec __user *, otmr)
 {
 	struct itimerspec64 new, old;
 	int ret;
@@ -549,7 +550,7 @@ SYSCALL_DEFINE4(timerfd_settime, int, ufd, int, flags,
 	return ret;
 }
 
-SYSCALL_DEFINE2(timerfd_gettime, int, ufd, struct itimerspec __user *, otmr)
+SYSCALL_DEFINE2(timerfd_gettime, int, ufd, struct __kernel_itimerspec __user *, otmr)
 {
 	struct itimerspec64 kotmr;
 	int ret = do_timerfd_gettime(ufd, &kotmr);
