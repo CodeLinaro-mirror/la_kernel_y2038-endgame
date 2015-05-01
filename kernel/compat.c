68e293a07bdda04d4e2e34c1a8569f6d234cd492
Arnd Bergmann <arnd@arndb.de>
2015-05-01 21:52:54 +0200

    y2038: use __kernel_timespec in sys_sched_rr_get_interval
    sys_sched_rr_get_interval is easily converted to use __kernel_timespec,
    by changing the function prototype. In order to allow compat handling
    on 32-bit architectures, we also move compat_sys_sched_getaffinity
    into the same file and unify the implementation, which avoids converting
    the structure multiple times.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/compat.c
+++ kernel/compat.c
@@ -1290,27 +1290,7 @@ COMPAT_SYSCALL_DEFINE4(migrate_pages, compat_pid_t, pid,
 	return sys_migrate_pages(pid, nr_bits + 1, old, new);
 }
 #endif
-#endif
 
-#ifdef CONFIG_COMPAT_TIME
-COMPAT_SYSCALL_DEFINE2(sched_rr_get_interval,
-		       compat_pid_t, pid,
-		       struct compat_timespec __user *, interval)
-{
-	struct timespec t;
-	int ret;
-	mm_segment_t old_fs = get_fs();
-
-	set_fs(KERNEL_DS);
-	ret = sys_sched_rr_get_interval(pid, (struct timespec __user *)&t);
-	set_fs(old_fs);
-	if (compat_put_timespec(&t, interval))
-		return -EFAULT;
-	return ret;
-}
-#endif
-
-#ifdef CONFIG_COMPAT
 /*
  * Allocate user-space memory for the duration of a single system call,
  * in order to marshall parameters inside a compat thunk.
