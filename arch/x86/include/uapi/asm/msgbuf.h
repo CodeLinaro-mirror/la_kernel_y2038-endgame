7d613233687d9c8b6af8269e142c87942b89330f
Arnd Bergmann <arnd@arndb.de>
2015-05-05 23:05:19 +0200

    y2038: x86: extend sysvipc data structures
    This extends the x86 copy of the sysvipc data structures
    to deal with 32-bit user space that has 64-bit time_t
    and wants to see timestamps beyond 2038.
    
    Fortunately, x86 has padding for this purpose in all the
    data structures, so we can just add extra fields. For consistency
    with big-endian architectures that copied the definitions from
    x86 without moving around that padding, we however don't just
    us a 64-bit value here, but instead use two unsigned 32-bit
    fields, and expect the libc to provide a correct definition
    to user space in a way that works across all architectures.
    
    Interestingly, x86-64 is the only 64-bit architecture that
    uses padding for 128-bit time_t, which seems to be have been
    a mistake in the original x86_64 port. All other 64-bit
    architectures have dropped those padding words.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/x86/include/uapi/asm/msgbuf.h
+++ arch/x86/include/uapi/asm/msgbuf.h
@@ -1,1 +1,40 @@
-#include <asm-generic/msgbuf.h>
+/*
+ * x86 msqid64_ds structure.
+ *
+ * Note extra padding because this structure is passed back and forth
+ * between kernel and user space.
+ *
+ * msqid64_ds was originally meant to be architecture specific, but
+ * everyone just ended up making identical copies without specific
+ * optimizations, so we may just as well all use the same one.
+ *
+ * i386 uses the padding words to extend the time_t values, so user
+ * space must combine the two words. x32 and x86_64 have a 64-bit
+ * __kernel_time_t, so the data is already in the right place.
+ *
+ * Pad space is left for:
+ * - 2 miscellaneous 32-bit values
+ */
+
+struct msqid64_ds {
+	struct ipc64_perm msg_perm;
+#ifdef __i386__
+	unsigned long	msg_stime;	/* last msgsnd time */
+	unsigned long	msg_stime_high;
+	unsigned long	msg_rtime;	/* last msgrcv time */
+	unsigned long	msg_rtime_high;
+	unsigned long	msg_ctime;	/* last change time */
+	unsigned long	msg_ctime_high;
+#else
+	__kernel_time_t msg_stime;	/* last msgsnd time */
+	__kernel_time_t msg_rtime;	/* last msgrcv time */
+	__kernel_time_t msg_ctime;	/* last change time */
+#endif
+	__kernel_ulong_t msg_cbytes;	/* current number of bytes on queue */
+	__kernel_ulong_t msg_qnum;	/* number of messages in queue */
+	__kernel_ulong_t msg_qbytes;	/* max number of bytes on queue */
+	__kernel_pid_t msg_lspid;	/* pid of last msgsnd */
+	__kernel_pid_t msg_lrpid;	/* last receive pid */
+	__kernel_ulong_t __unused4;
+	__kernel_ulong_t __unused5;
+};
