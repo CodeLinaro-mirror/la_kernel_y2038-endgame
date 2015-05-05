6ea97b1f58c9833390549ddc92d12698ff965200
Arnd Bergmann <arnd@arndb.de>
2015-05-05 23:19:43 +0200

    y2038: extend remaining sysvipc data structures
    parisc, powerpc, sparc and xtensa all use incompatible variations
    of the generic sysvipc data structures, intended to have the
    padding moved around so they can deal with big-endian 32-bit
    user space that has 64-bit time_t.
    
    parisc and sparc actually succeeded in defining this right, but
    as everyone else got it wrong, we just use the same hack
    everywhere.
    
    powerpc has the same definition as parisc and sparc, but now also
    supports little-endian mode, which gets it wrong. xtensa tried
    to fix it up many years ago but only got it right for two of the
    three structures, while their struct shmid64_ds ended up being
    defined in two identical copies, and the big-endian one is wrong.
    
    In the end, we just take the same approach here that we have for
    the asm-generic header and add separate 32-bit fields for the
    upper halves of the timestamps, and let libc deal with the mess
    in user space.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/parisc/include/uapi/asm/sembuf.h
+++ arch/parisc/include/uapi/asm/sembuf.h
@@ -15,14 +15,15 @@
 
 struct semid64_ds {
 	struct ipc64_perm sem_perm;		/* permissions .. see ipc.h */
-#if __BITS_PER_LONG != 64
-	unsigned int	__pad1;
-#endif
+#if __BITS_PER_LONG == 64
 	__kernel_time_t	sem_otime;		/* last semop time */
-#if __BITS_PER_LONG != 64
-	unsigned int	__pad2;
-#endif
 	__kernel_time_t	sem_ctime;		/* last change time */
+#else
+	unsigned long	sem_otime_high;
+	unsigned long	sem_otime;		/* last semop time */
+	unsigned long	sem_ctime_high;
+	unsigned long	sem_ctime;		/* last change time */
+#endif
 	unsigned int	sem_nsems;		/* no. of semaphores in array */
 	unsigned int	__unused1;
 	unsigned int	__unused2;
--- arch/powerpc/include/uapi/asm/shmbuf.h
+++ arch/powerpc/include/uapi/asm/shmbuf.h
@@ -21,19 +21,17 @@
 
 struct shmid64_ds {
 	struct ipc64_perm	shm_perm;	/* operation perms */
-#ifndef __powerpc64__
-	unsigned long		__unused1;
-#endif
+#ifdef __powerpc64__
 	__kernel_time_t		shm_atime;	/* last attach time */
-#ifndef __powerpc64__
-	unsigned long		__unused2;
-#endif
 	__kernel_time_t		shm_dtime;	/* last detach time */
-#ifndef __powerpc64__
-	unsigned long		__unused3;
-#endif
 	__kernel_time_t		shm_ctime;	/* last change time */
-#ifndef __powerpc64__
+#else
+	unsigned long		shm_atime_high;
+	unsigned long		shm_atime;	/* last attach time */
+	unsigned long		shm_dtime_high;
+	unsigned long		shm_dtime;	/* last detach time */
+	unsigned long		shm_ctime_high;
+	unsigned long		shm_ctime;	/* last change time */
 	unsigned long		__unused4;
 #endif
 	size_t			shm_segsz;	/* size of segment (bytes) */
