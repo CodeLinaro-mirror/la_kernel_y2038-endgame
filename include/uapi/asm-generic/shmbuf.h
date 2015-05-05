8f9c7888ec0d46e45a7cf8b6ccb6a08b365fef3e
Arnd Bergmann <arnd@arndb.de>
2015-05-05 23:13:15 +0200

    y2038: asm-generic: extend sysvipc data structures
    Most architectures now use the asm-generic copy of the sysvipc
    data structures (msqid64_ds, semid64_ds, shmid64_ds), which use
    32-bit __kernel_time_t on 32-bit architectures but have padding
    behind them to allow extending the type to 64-bit.
    
    Unfortunately, that fails on all big-endian architectures, which
    have the padding on the wrong side. As so many of them get it wrong,
    we decided to not bother even trying to fix it up when we introduced
    the asm-generic copy. Instead we always use the padding word now
    to provide the upper 32 bits of the seconds value, regardless of the
    endianess.
    
    A libc implementation on a typical big-endian system can deal with
    this by providing its own copy of the structure definition to user
    space, and swapping the two 32-bit words before returning from
    the semctl/shmctl/msgctl system calls.
    
    ARM, s/390 and Tile are architectures that use these generic headers
    and also provide support for compat mode on 64-bit kernels, so
    we adapt their copies here as well.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/uapi/asm-generic/shmbuf.h
+++ include/uapi/asm-generic/shmbuf.h
@@ -26,17 +26,17 @@
 struct shmid64_ds {
 	struct ipc64_perm	shm_perm;	/* operation perms */
 	size_t			shm_segsz;	/* size of segment (bytes) */
+#if __BITS_PER_LONG != 64
+	unsigned long		shm_atime;	/* last attach time */
+	unsigned long		shm_atime_high;
+	unsigned long		shm_dtime;	/* last detach time */
+	unsigned long		shm_dtime_high;
+	unsigned long		shm_ctime;	/* last change time */
+	unsigned long		shm_ctime_high;
+#else
 	__kernel_time_t		shm_atime;	/* last attach time */
-#if __BITS_PER_LONG != 64
-	unsigned long		__unused1;
-#endif
 	__kernel_time_t		shm_dtime;	/* last detach time */
-#if __BITS_PER_LONG != 64
-	unsigned long		__unused2;
-#endif
 	__kernel_time_t		shm_ctime;	/* last change time */
-#if __BITS_PER_LONG != 64
-	unsigned long		__unused3;
 #endif
 	__kernel_pid_t		shm_cpid;	/* pid of creator */
 	__kernel_pid_t		shm_lpid;	/* pid of last operator */
