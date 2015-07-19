f78a1ee07fc21153af51fd3aa2c9e1c8fc71f74c
Arnd Bergmann <arnd@arndb.de>
2015-07-19 17:57:37 +0200

    nfds: avoid gettimeofday for nfssvc_boot time
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/nfsd/nfssvc.c
+++ fs/nfsd/nfssvc.c
@@ -492,7 +492,7 @@ int nfsd_create_serv(struct net *net)
 #if IS_ENABLED(CONFIG_IPV6)
 	register_inet6addr_notifier(&nfsd_inet6addr_notifier);
 #endif
-	do_gettimeofday(&nn->nfssvc_boot);		/* record boot time */
+	ktime_get_real_ts64(&nn->nfssvc_boot); /* record boot time */
 	return 0;
 }
 
