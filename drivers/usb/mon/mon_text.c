a3e180a2624cabc92f57c9a0e0d35ee91a0cf0cf
Arnd Bergmann <arnd@arndb.de>
2015-07-27 22:43:48 +0200

    [oldpatch] usb: mon_text: use monotonic timestamps
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/usb/mon/mon_text.c
+++ drivers/usb/mon/mon_text.c
@@ -176,12 +176,12 @@ static inline char mon_text_get_data(struct mon_event_text *ep, struct urb *urb,
 
 static inline unsigned int mon_get_timestamp(void)
 {
-	struct timeval tval;
+	struct timespec64 ts;
 	unsigned int stamp;
 
-	do_gettimeofday(&tval);
-	stamp = tval.tv_sec & 0xFFF;	/* 2^32 = 4294967296. Limit to 4096s. */
-	stamp = stamp * 1000000 + tval.tv_usec;
+	ktime_get_ts64(&ts);
+	stamp = ts.tv_sec & 0xFFF;	/* 2^32 = 4294967296. Limit to 4096s. */
+	stamp = stamp * 1000000 + ts.tv_nsec / NSEC_PER_USEC;
 	return stamp;
 }
 
