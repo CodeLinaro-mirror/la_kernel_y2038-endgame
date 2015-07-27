15ac0ccae990847e450d8fd67205bafe4631634b
Arnd Bergmann <arnd@arndb.de>
2015-07-27 21:46:55 +0200

    tsacct: avoid stupid timeval conversion
    The tsacct code converts a cputime value to jiffies, then to
    timeval, and finally to microseconds. This is pointless because
    we have a cputime_to_usecs function that is even used in the
    same file.
    
    Worse, it breaks when the total time is more than the maximum
    for 'jiffies', which can be as low as 49 days.
    
    This changes the code to use the helper directly.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/tsacct.c
+++ kernel/tsacct.c
@@ -125,16 +125,13 @@ static void __acct_update_integrals(struct task_struct *tsk,
 {
 	if (likely(tsk->mm)) {
 		cputime_t time, dtime;
-		struct timeval value;
 		unsigned long flags;
 		u64 delta;
 
 		local_irq_save(flags);
 		time = stime + utime;
 		dtime = time - tsk->acct_timexpd;
-		jiffies_to_timeval(cputime_to_jiffies(dtime), &value);
-		delta = value.tv_sec;
-		delta = delta * USEC_PER_SEC + value.tv_usec;
+		delta = cputime_to_usecs(dtime);
 
 		if (delta == 0)
 			goto out;
