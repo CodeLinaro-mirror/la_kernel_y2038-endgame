02722bf4a61eec5fc25994dece11fdb1235c0d09
Arnd Bergmann <arnd@arndb.de>
2015-07-29 15:30:00 +0200

    nfs: avoid time_t for internal housekeeping
    time_t is deprecated for y2038 reasons. It is safe to use it for
    storing boottime seconds though, so we can change nfs to simply
    use 'long' instead of 'time_t' when accessing seconds since boot.
    
    This could possibly be converted to using ktime_get_seconds()
    for simplification.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- net/sunrpc/cache.c
+++ net/sunrpc/cache.c
@@ -43,7 +43,7 @@ static void cache_revisit_request(struct cache_head *item);
 
 static void cache_init(struct cache_head *h, struct cache_detail *detail)
 {
-	time_t now = seconds_since_boot();
+	long now = seconds_since_boot();
 	INIT_HLIST_NODE(&h->cache_list);
 	h->flags = 0;
 	kref_init(&h->ref);
@@ -119,10 +119,10 @@ EXPORT_SYMBOL_GPL(sunrpc_cache_lookup);
 
 static void cache_dequeue(struct cache_detail *detail, struct cache_head *ch);
 
-static void cache_fresh_locked(struct cache_head *head, time_t expiry,
+static void cache_fresh_locked(struct cache_head *head, long expiry,
 			       struct cache_detail *detail)
 {
-	time_t now = seconds_since_boot();
+	long now = seconds_since_boot();
 	if (now <= detail->flush_time)
 		/* ensure it isn't immediately treated as expired */
 		now = detail->flush_time + 1;
@@ -1367,7 +1367,7 @@ static int c_show(struct seq_file *m, void *p)
 		return cd->cache_show(m, cd, NULL);
 
 	ifdebug(CACHE)
-		seq_printf(m, "# expiry=%ld refcnt=%d flags=%lx\n",
+		seq_printf(m, "# expiry=%lld refcnt=%d flags=%lx\n",
 			   convert_to_wallclock(cp->expiry_time),
 			   kref_read(&cp->ref), cp->flags);
 	cache_get(cp);
@@ -1440,7 +1440,7 @@ static ssize_t read_flush(struct file *file, char __user *buf,
 	char tbuf[22];
 	size_t len;
 
-	len = snprintf(tbuf, sizeof(tbuf), "%lu\n",
+	len = snprintf(tbuf, sizeof(tbuf), "%llu\n",
 			convert_to_wallclock(cd->flush_time));
 	return simple_read_from_buffer(buf, count, ppos, tbuf, len);
 }
