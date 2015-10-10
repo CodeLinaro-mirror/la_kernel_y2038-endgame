bfd3d708f358c7b2c0e7bfe2fdfe0972cd445b99
Arnd Bergmann <arnd@arndb.de>
2015-10-10 21:19:48 +0200

    [WRONG] davinci_mmc: remove incorrect __exit annotation
    WARNING: drivers/built-in.o(.exit.text+0x28ec): Section mismatch in reference from the function davinci_mmcsd_remove() to the function .init.text:davinci_release_dma_channels()
    The function __exit davinci_mmcsd_remove() references
    a function __init davinci_release_dma_channels().
    This is often seen when error handling in the exit function
    uses functionality in the init path.
    The fix is often to remove the __init annotation of
    davinci_release_dma_channels() so it may be used outside an init section.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/mmc/host/davinci_mmc.c
+++ drivers/mmc/host/davinci_mmc.c
@@ -498,7 +498,7 @@ static int mmc_davinci_start_dma_transfer(struct mmc_davinci_host *host,
 	return ret;
 }
 
-static void __init_or_module
+static void
 davinci_release_dma_channels(struct mmc_davinci_host *host)
 {
 	if (!host->use_dma)
@@ -1362,7 +1362,7 @@ ioremap_fail:
 	return ret;
 }
 
-static int __exit davinci_mmcsd_remove(struct platform_device *pdev)
+static int davinci_mmcsd_remove(struct platform_device *pdev)
 {
 	struct mmc_davinci_host *host = platform_get_drvdata(pdev);
 
@@ -1415,7 +1415,7 @@ static struct platform_driver davinci_mmcsd_driver = {
 		.pm	= davinci_mmcsd_pm_ops,
 		.of_match_table = davinci_mmc_dt_ids,
 	},
-	.remove		= __exit_p(davinci_mmcsd_remove),
+	.remove		= davinci_mmcsd_remove,
 	.id_table	= davinci_mmc_devtype,
 };
 
