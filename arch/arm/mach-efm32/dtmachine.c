e30e77a6bb93cb2f882832d5ee49dd8ed1437e8d
Arnd Bergmann <arnd@arndb.de>
2015-12-08 22:41:33 +0100

    Revert "[HACK] ARM: NOMMU config"
    This reverts commit 3a62ffded8ccea6251a89659402d1427ef51ca3f.

--- arch/arm/mach-efm32/dtmachine.c
+++ arch/arm/mach-efm32/dtmachine.c
@@ -11,4 +11,5 @@ static const char *const efm32gg_compat[] __initconst = {
 
 DT_MACHINE_START(EFM32DT, "EFM32 (Device Tree Support)")
 	.dt_compat = efm32gg_compat,
+	.restart = armv7m_restart,
 MACHINE_END
