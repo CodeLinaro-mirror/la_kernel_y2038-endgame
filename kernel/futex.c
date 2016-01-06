ba7cc3acf188bf54539d0c8b80486aca4bd9c07b
Arnd Bergmann <arnd@arndb.de>
2016-01-06 23:55:27 +0100

    futex: avoid fixups in __init section
    We may get a link error on ARM when we have an SMP fixup in
    a .init.text section:
    
    kernel/built-in.o:(.text.fixup+0x206): relocation truncated to fit: R_ARM_THM_JUMP24 against `.init.text'
    
    gcc does not inline cmpxchg_futex_value_locked, and if we prevent
    clang from inlining it, everything works.
    
    A better fix would probably be to change the fixup assembly
    to be a proper symbol so that ld generates a trampoline.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/futex.c
+++ kernel/futex.c
@@ -664,8 +664,8 @@ static struct futex_q *futex_top_waiter(struct futex_hash_bucket *hb,
 	return NULL;
 }
 
-static int cmpxchg_futex_value_locked(u32 *curval, u32 __user *uaddr,
-				      u32 uval, u32 newval)
+static int noinline cmpxchg_futex_value_locked(u32 *curval, u32 __user *uaddr,
+					       u32 uval, u32 newval)
 {
 	int ret;
 
