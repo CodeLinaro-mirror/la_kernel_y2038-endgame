0a3f4c0556dd1043af63b8fb6affe288507488d0
Arnd Bergmann <arnd@arndb.de>
2016-01-31 22:26:21 +0100

    [RESUBMIT] ARM: prevent tracing IPI_CPU_BACKTRACE
    When function tracing for IPIs is enabled, we get a warning for an
    overflow of the ipi_types array with the IPI_CPU_BACKTRACE type
    as triggered by raise_nmi():
    
    arch/arm/kernel/smp.c: In function 'raise_nmi':
    arch/arm/kernel/smp.c:489:2: error: array subscript is above array bounds [-Werror=array-bounds]
      trace_ipi_raise(target, ipi_types[ipinr]);
    
    This is a correct warning as we actually overflow the array here.
    
    This patch raise_nmi() to call __smp_cross_call() instead of
    smp_cross_call(), to avoid calling into ftrace. For clarification,
    I'm also adding a two new code comments describing how this one
    is special.
    
    The warning appears to have shown up after patch e7273ff49acf
    ("ARM: 8488/1: Make IPI_CPU_BACKTRACE a "non-secure" SGI"), which
    changed the number assignment from '15' to '8', but as far as I can
    tell has existed since the IPI tracepoints were first introduced.
    If we decide to backport this patch to stable kernels, we probably
    need to backport e7273ff49acf as well.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: e7273ff49acf ("ARM: 8488/1: Make IPI_CPU_BACKTRACE a "non-secure" SGI")
    Fixes: 365ec7b17327 ("ARM: add IPI tracepoints") # v3.17
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -72,6 +72,10 @@ enum ipi_msg_type {
 	IPI_CPU_STOP,
 	IPI_IRQ_WORK,
 	IPI_COMPLETION,
+	/*
+	 * CPU_BACKTRACE is special and not included in NR_IPI
+	 * or tracable with trace_ipi_*
+	 */
 	IPI_CPU_BACKTRACE,
 	/*
 	 * SGI8-15 can be reserved by secure firmware, and thus may
@@ -757,7 +761,7 @@ static void raise_nmi(cpumask_t *mask)
 	if (cpumask_test_cpu(smp_processor_id(), mask) && irqs_disabled())
 		nmi_cpu_backtrace(NULL);
 
-	smp_cross_call(mask, IPI_CPU_BACKTRACE);
+	__smp_cross_call(mask, IPI_CPU_BACKTRACE);
 }
 
 void arch_trigger_all_cpu_backtrace(bool include_self)
