05ffe2a4e15f3c20d1dd361fdbdfeebf3aea23f5
Arnd Bergmann <arnd@arndb.de>
2016-05-17 13:34:18 +0200

    [SUBMITTED 20160516] drm: exynos: mark pm functions as __maybe_unused
    The rework of the exynos DRM clock handling introduced
    warnings for configurations that have CONFIG_PM disabled:
    
    drivers/gpu/drm/exynos/exynos_hdmi.c:736:13: error: 'hdmi_clk_disable_gates' defined but not used [-Werror=unused-function]
     static void hdmi_clk_disable_gates(struct hdmi_context *hdata)
                 ^~~~~~~~~~~~~~~~~~~~~~
    drivers/gpu/drm/exynos/exynos_hdmi.c:717:12: error: 'hdmi_clk_enable_gates' defined but not used [-Werror=unused-function]
     static int hdmi_clk_enable_gates(struct hdmi_context *hdata)
    
    The problem is that the PM functions themselves are inside of
    an #ifdef, but some functions they call are not.
    
    This patch removes the #ifdef and instead marks the PM functions
    as __maybe_unused, which is a more reliable way to get it right.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: 9be7e9898444 ("drm/exynos/hdmi: clock code re-factoring")

--- drivers/gpu/drm/exynos/exynos_hdmi.c
+++ drivers/gpu/drm/exynos/exynos_hdmi.c
@@ -1926,8 +1926,7 @@ static int hdmi_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-static int exynos_hdmi_suspend(struct device *dev)
+static int __maybe_unused exynos_hdmi_suspend(struct device *dev)
 {
 	struct hdmi_context *hdata = dev_get_drvdata(dev);
 
@@ -1936,7 +1935,7 @@ static int exynos_hdmi_suspend(struct device *dev)
 	return 0;
 }
 
-static int exynos_hdmi_resume(struct device *dev)
+static int __maybe_unused exynos_hdmi_resume(struct device *dev)
 {
 	struct hdmi_context *hdata = dev_get_drvdata(dev);
 	int ret;
@@ -1947,7 +1946,6 @@ static int exynos_hdmi_resume(struct device *dev)
 
 	return 0;
 }
-#endif
 
 static const struct dev_pm_ops exynos_hdmi_pm_ops = {
 	SET_RUNTIME_PM_OPS(exynos_hdmi_suspend, exynos_hdmi_resume, NULL)
