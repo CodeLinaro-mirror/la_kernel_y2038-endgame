69c6d6da9b57448976bdd774a94994380424077c
Arnd Bergmann <arnd@arndb.de>
2016-06-07 14:03:39 +0200

    usb: ehci: empty COUNT() macro
    /git/arm-soc/drivers/usb/host/ehci-hcd.c: In function 'ehci_irq':
    /git/arm-soc/drivers/usb/host/ehci-hcd.c:739:4: error: suggest braces around empty body in an 'else' statement [-Werror=empty-body]
        COUNT (ehci->stats.error);
        ^
    /git/arm-soc/drivers/usb/host/ehci-hcd.c:763:4: error: suggest braces around empty body in an 'if' statement [-Werror=empty-body]
        COUNT(ehci->stats.iaa);
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -250,7 +250,7 @@ struct ehci_hcd {			/* one per controller */
 	struct ehci_stats	stats;
 #	define COUNT(x) ((x)++)
 #else
-#	define COUNT(x)
+#	define COUNT(x) do { } while (0)
 #endif
 
 	/* debug files */
