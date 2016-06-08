22b83963238f6bd4d4329d6cd61005921d118326
Arnd Bergmann <arnd@arndb.de>
2016-06-08 10:46:40 +0200

    bpf: shut up -Woverride-init warning
    gcc -Woverride-init provides useful warnings about bugs in some drivers,
    but it warns about an intentional duplicate inialization in the bpf
    code:
    
    kernel/bpf/core.c: In function '__bpf_prog_run':
    kernel/bpf/core.c:476:26: error: initialized field overwritten [-Werror=override-init]
    kernel/bpf/core.c:476:26: note: (near initialization for 'jumptable[12]')
    kernel/bpf/core.c:477:26: error: initialized field overwritten [-Werror=override-init]
    kernel/bpf/core.c:477:26: note: (near initialization for 'jumptable[4]')
    kernel/bpf/core.c:478:26: error: initialized field overwritten [-Werror=override-init]
    kernel/bpf/core.c:478:26: note: (near initialization for 'jumptable[28]')
    kernel/bpf/core.c:479:26: error: initialized field overwritten [-Werror=override-init]
    kernel/bpf/core.c:479:26: note: (near initialization for 'jumptable[20]')
    kernel/bpf/core.c:480:26: error: initialized field overwritten [-Werror=override-init]
    kernel/bpf/core.c:480:26: note: (near initialization for 'jumptable[92]')
    kernel/bpf/core.c:481:26: error: initialized field overwritten [-Werror=override-init]
    
    This uses a #pragma to shut up that warning, as a preparation for enabling
    the warning by default in the future.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -763,6 +763,7 @@ EXPORT_SYMBOL_GPL(__bpf_call_base);
  *
  * Decode and execute eBPF instructions.
  */
+#pragma GCC diagnostic ignored "-Woverride-init"
 static unsigned int ___bpf_prog_run(u64 *regs, const struct bpf_insn *insn,
 				    u64 *stack)
 {
@@ -1216,6 +1217,8 @@ load_byte:
 		WARN_RATELIMIT(1, "unknown opcode %02x\n", insn->code);
 		return 0;
 }
+#pragma GCC diagnostic warning "-Woverride-init"
+
 STACK_FRAME_NON_STANDARD(___bpf_prog_run); /* jump table */
 
 #define PROG_NAME(stack_size) __bpf_prog_run##stack_size
