a01f56c2a142e560252df87db061fd4ef5566172
Arnd Bergmann <arnd@arndb.de>
2016-08-02 23:19:28 +0200

    amdgpu: include correct headers
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_pm.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_pm.h
@@ -37,5 +37,6 @@ void amdgpu_pm_compute_clocks(struct amdgpu_device *adev);
 void amdgpu_dpm_thermal_work_handler(struct work_struct *work);
 void amdgpu_dpm_enable_uvd(struct amdgpu_device *adev, bool enable);
 void amdgpu_dpm_enable_vce(struct amdgpu_device *adev, bool enable);
+void amdgpu_pm_acpi_event_handler(struct amdgpu_device *adev);
 
 #endif
--- drivers/gpu/drm/amd/amdgpu/atombios_crtc.c
+++ drivers/gpu/drm/amd/amdgpu/atombios_crtc.c
@@ -30,6 +30,7 @@
 #include "amdgpu.h"
 #include "atom.h"
 #include "atom-bits.h"
+#include "atombios_crtc.h"
 #include "atombios_encoders.h"
 #include "atombios_crtc.h"
 #include "amdgpu_atombios.h"
