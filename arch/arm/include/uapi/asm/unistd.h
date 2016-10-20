df0ec48a4715219eb49f94eb7405f4723e746f52
Arnd Bergmann <arnd@arndb.de>
2016-10-20 15:12:45 +0200

    fixup y2038 syscall table
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -24,28 +24,6 @@
 #endif
 
 #include <asm/unistd-common.h>
-#define __NR_clock_gettime64		(__NR_SYSCALL_BASE+394)
-#define __NR_clock_settime64		(__NR_SYSCALL_BASE+395)
-#define __NR_clock_adjtime64		(__NR_SYSCALL_BASE+396)
-#define __NR_clock_getres64		(__NR_SYSCALL_BASE+397)
-#define __NR_clock_nanosleep64		(__NR_SYSCALL_BASE+398)
-#define __NR_timer_gettime64		(__NR_SYSCALL_BASE+399)
-#define __NR_timer_settime64		(__NR_SYSCALL_BASE+400)
-#define __NR_timerfd_gettime64		(__NR_SYSCALL_BASE+401)
-#define __NR_timerfd_settime64		(__NR_SYSCALL_BASE+402)
-#define __NR_pselect64			(__NR_SYSCALL_BASE+403)
-#define __NR_ppoll64			(__NR_SYSCALL_BASE+404)
-#define __NR_io_getevents64		(__NR_SYSCALL_BASE+405)
-#define __NR_recvmmsg64			(__NR_SYSCALL_BASE+406)
-#define __NR_semtimedop64		(__NR_SYSCALL_BASE+407)
-#define __NR_mq_timedsend64		(__NR_SYSCALL_BASE+408)
-#define __NR_mq_timedreceive64		(__NR_SYSCALL_BASE+409)
-#define __NR_utimensat64		(__NR_SYSCALL_BASE+410)
-#define __NR_newfstat64			(__NR_SYSCALL_BASE+411)
-#define __NR_newfstatat64		(__NR_SYSCALL_BASE+412)
-#define __NR_rt_sigtimedwait64		(__NR_SYSCALL_BASE+413)
-#define __NR_getrusage64		(__NR_SYSCALL_BASE+414)
-#define __NR_waitid64			(__NR_SYSCALL_BASE+415)
 
 /*
  * The following SWIs are ARM private.
