3f9686cff3c337d0c4cd7ee04d62cbeac6eb2b37
Arnd Bergmann <arnd@arndb.de>
2016-12-14 15:46:04 +0100

    [HACK] gcc-4.5: avoid  link errors for unused function pointers
    gcc versions before 4.6 cannot do dead code elimination across
    function pointers, e.g. with a construct like
    
    static int f(void)
    {
    	return declared_extern_but_undefined_function();
    }
    
    static int g(void)
    {
    	if (0)
    		reference_function(&f);
    }
    
    which we rely on in lots of places when registering functions,
    it results in a link error for
    declared_extern_but_undefined_function.
    
    This patch is incomplete and introduces a few other problems
    (at least warnings about unused functions), but it shows what
    we would have to do in order to address this in the kernel.
    
    The three options forward I see are:
    
    - Declare gcc-4.5 (and prior version) fully supported and
      do proper fixes for the bugs we find.
    
    - Officially declare gcc-4.6 the minimum required version
      for the kernel and remove all existing hacks we have for
      older versions.
    
    - Do nothing: if you use an older gcc version and you run into
      this, you are on your own and can submit a patch for the
      specific problem you find.
    
    At the moment, gcc-3.2 is in theory still supported, but I
    found that even gcc-4.2 is almost completely useless these
    days at least on ARM.
    
    If we want to keep gcc-4.5 supported, then we can probably
    have 4.3 as the minimum version without too many other
    workarounds.
    
    Do not apply but feel free to pick out parts of this
    patch and submit them if you use old gcc versions and
    run into the problems.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/dma/ti/dma-crossbar.c
+++ drivers/dma/ti/dma-crossbar.c
@@ -77,6 +77,7 @@ static void ti_am335x_xbar_free(struct device *dev, void *route_data)
 	kfree(map);
 }
 
+#ifdef CONFIG_DMA_OF
 static void *ti_am335x_xbar_route_allocate(struct of_phandle_args *dma_spec,
 					   struct of_dma *ofdma)
 {
@@ -125,6 +126,7 @@ static void *ti_am335x_xbar_route_allocate(struct of_phandle_args *dma_spec,
 
 	return map;
 }
+#endif
 
 static const struct of_device_id ti_am335x_master_match[] = {
 	{ .compatible = "ti,edma3-tpcc", },
@@ -193,8 +195,10 @@ static int ti_am335x_xbar_probe(struct platform_device *pdev)
 	for (i = 0; i < xbar->dma_requests; i++)
 		ti_am335x_xbar_write(xbar->iomem, i, 0);
 
+#ifdef CONFIG_DMA_OF
 	ret = of_dma_router_register(node, ti_am335x_xbar_route_allocate,
 				     &xbar->dmarouter);
+#endif
 
 	return ret;
 }
@@ -241,6 +245,7 @@ static void ti_dra7_xbar_free(struct device *dev, void *route_data)
 	kfree(map);
 }
 
+#ifdef CONFIG_DMA_OF
 static void *ti_dra7_xbar_route_allocate(struct of_phandle_args *dma_spec,
 					 struct of_dma *ofdma)
 {
@@ -290,6 +295,7 @@ static void *ti_dra7_xbar_route_allocate(struct of_phandle_args *dma_spec,
 
 	return map;
 }
+#endif
 
 #define TI_XBAR_EDMA_OFFSET	0
 #define TI_XBAR_SDMA_OFFSET	1
@@ -425,8 +431,10 @@ static int ti_dra7_xbar_probe(struct platform_device *pdev)
 			ti_dra7_xbar_write(xbar->iomem, i, xbar->safe_val);
 	}
 
+#ifdef CONFIG_DMA_OF
 	ret = of_dma_router_register(node, ti_dra7_xbar_route_allocate,
 				     &xbar->dmarouter);
+#endif
 	if (ret) {
 		/* Restore the defaults for the crossbar */
 		for (i = 0; i < xbar->dma_requests; i++) {
