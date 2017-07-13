d09b30c6cb94fe38563c5e46e218523a1edbf307
Arnd Bergmann <arnd@arndb.de>
2017-07-13 10:25:18 +0200

    [SUBMITTED 20170714] proc/kcore: hide a harmless warning
    gcc warns when MODULES_VADDR/END is defined to the same value as
    VMALLOC_START/VMALLOC_END, e.g. on x86-32:
    
    fs/proc/kcore.c: In function ‘add_modules_range’:
    fs/proc/kcore.c:622:161: error: self-comparison always evaluates to false [-Werror=tautological-compare]
      if (/*MODULES_VADDR != VMALLOC_START && */MODULES_END != VMALLOC_END) {
    
    The code is correct as it is required for most other configurations.
    The best workaround I found for shutting up that warning is to make
    it a little more complex by adding a temporary variable. The compiler
    will still optimize away the code as it finds the two to be identical,
    but it no longer warns because it doesn't condider the comparison
    "tautological" any more.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/proc/kcore.c
+++ fs/proc/kcore.c
@@ -619,12 +619,14 @@ static void __init proc_kcore_text_init(void)
 /*
  * MODULES_VADDR has no intersection with VMALLOC_ADDR.
  */
-struct kcore_list kcore_modules;
+static struct kcore_list kcore_modules;
 static void __init add_modules_range(void)
 {
-	if (MODULES_VADDR != VMALLOC_START && MODULES_END != VMALLOC_END) {
-		kclist_add(&kcore_modules, (void *)MODULES_VADDR,
-			MODULES_END - MODULES_VADDR, KCORE_VMALLOC);
+	void *start = (void *)MODULES_VADDR;
+	size_t len = MODULES_END - MODULES_VADDR;
+
+	if (start != (void *)VMALLOC_START && len != VMALLOC_END - VMALLOC_START) {
+		kclist_add(&kcore_modules, start, len, KCORE_VMALLOC);
 	}
 }
 #else
