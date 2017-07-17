8e4e1a6bc2f35193df64fd0c5bb7bba284bba258
Arnd Bergmann <arnd@arndb.de>
2017-07-17 16:41:06 +0200

    Revert "[SUBMITTED 20170714] acpi: thermal: fix gcc-6/ccache warning"
    This reverts commit 1ccd3b54228a67c609b988f9002e0dbd3992af26.

--- drivers/acpi/processor_thermal.c
+++ drivers/acpi/processor_thermal.c
@@ -122,14 +122,11 @@ static int cpufreq_get_cur_state(unsigned int cpu)
 static int cpufreq_set_cur_state(unsigned int cpu, int state)
 {
 	int i;
-	int id;
 
 	if (!cpu_has_cpufreq(cpu))
 		return 0;
 
 	reduction_pctg(cpu) = state;
-
-	id = topology_physical_package_id(cpu);
 
 	/*
 	 * Update all the CPUs in the same package because they all
@@ -137,7 +134,8 @@ static int cpufreq_set_cur_state(unsigned int cpu, int state)
 	 * frequency.
 	 */
 	for_each_online_cpu(i) {
-		if (topology_physical_package_id(i) == id)
+		if (topology_physical_package_id(i) ==
+		    topology_physical_package_id(cpu))
 			cpufreq_update_policy(i);
 	}
 	return 0;
