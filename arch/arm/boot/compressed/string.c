e2e68529da650f3c7e14a0f612b69b1788ad8cd8
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-08-16 11:31:59 +0100

    ARM: decompressor: add strstr() implementation
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

--- arch/arm/boot/compressed/string.c
+++ arch/arm/boot/compressed/string.c
@@ -140,3 +140,20 @@ void *memset(void *s, int c, size_t count)
 		*xs++ = c;
 	return s;
 }
+
+char *strstr(const char *s1, const char *s2)
+{
+	int l1, l2;
+
+	l2 = strlen(s2);
+	if (!l2)
+		return (char *)s1;
+	l1 = strlen(s1);
+	while (l1 >= l2) {
+		l1--;
+		if (!memcmp(s1, s2, l2))
+			return (char *)s1;
+		s1++;
+	}
+	return NULL;
+}
