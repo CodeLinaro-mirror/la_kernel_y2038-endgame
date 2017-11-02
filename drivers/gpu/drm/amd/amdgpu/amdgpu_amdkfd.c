9de6e01df07c1409bf17339fd5a2bda614eabb01
Arnd Bergmann <arnd@arndb.de>
2017-11-02 17:21:43 +0100

    amdgpu: make SOC15 conditional
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
@@ -75,11 +75,13 @@ void amdgpu_amdkfd_device_probe(struct amdgpu_device *adev)
 	case CHIP_POLARIS11:
 		kfd2kgd = amdgpu_amdkfd_gfx_8_0_get_functions();
 		break;
+#ifdef CONFIG_DRM_AMDGPU_SOC15
 	case CHIP_VEGA10:
 	case CHIP_VEGA20:
 	case CHIP_RAVEN:
 		kfd2kgd = amdgpu_amdkfd_gfx_9_0_get_functions();
 		break;
+#endif
 	default:
 		dev_info(adev->dev, "kfd not supported on this ASIC\n");
 		return;
