7926e0dd01547294ef1dd3a102d1cf624864b218
Arnd Bergmann <arnd@arndb.de>
2017-11-07 14:05:59 +0100

    [SUBMITTED 20171108] alpha: osf_sys.c: use timespec64 where appropriate
    Some of the syscall helper functions (do_utimes, poll_select_set_timeout,
    core_sys_select) have changed over the past year or two to use
    'timespec64' pointers rather than 'timespec'. This was fine on alpha,
    since 64-bit architectures treat the two as the same type.
    
    However, I'd like to change that behavior and make 'timespec64' a proper
    type of its own even on 64-bit architectures, and that will introduce
    harmless type mismatch warnings here.
    
    Also, I'm trying to kill off the do_gettimeofday() helper in favor of
    ktime_get() and related interfaces throughout the kernel.
    
    This changes the get_tv32/put_tv32 helper functions to also take a
    timespec64 argument rather than timeval, which allows us to simplify
    some of the syscall helpers a bit and avoid the type warnings.
    
    For the moment, wait4 and adjtimex are still better off with the old
    behavior, so I'm adding a special put_tv_to_tv32() helper for those.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    v2: fix ktime_get_real_ts64()/ktime_get_ts64() typo

--- arch/alpha/kernel/osf_sys.c
+++ arch/alpha/kernel/osf_sys.c
@@ -1274,7 +1274,7 @@ SYSCALL_DEFINE1(old_adjtimex, struct timex32 __user *, txc_p)
 	if (copy_to_user(txc_p, &txc, offsetof(struct timex32, time)) ||
 	    (copy_to_user(&txc_p->tick, &txc.tick, sizeof(struct timex32) - 
 			  offsetof(struct timex32, tick))) ||
-	    (put_tv32(&txc_p->time, &txc.time)))
+	    (put_tv_to_tv32(&txc_p->time, &txc.time)))
 	  return -EFAULT;
 
 	return ret;
