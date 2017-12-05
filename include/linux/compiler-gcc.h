5e3eb3295f84479e74f3f34fdd93352a51b4219f
Arnd Bergmann <arnd@arndb.de>
2017-12-05 11:14:15 +0100

    [SUBMITTED 20171205] [RFC] kbuild: add macro for controlling warnings to linux/compiler.h
    I have occasionally run into a situation where it would make sense to
    control a compiler warning from a source file rather than doing so from
    a Makefile using the $(cc-disable-warning, ...) or $(cc-option, ...)
    helpers.
    
    The approach here is similar to what glibc uses, using __diag() and
    related macros to encapsulate a _Pragma("GCC diagnostic ...") statement
    that gets turned into the respective "#pragma GCC diagnostic ..." by
    the preprocessor when the macro gets expanded.
    
    Like glibc, I also have an argument to pass the affected compiler
    version, but decided to actually evaluate that one. For now, this
    supports GCC_4_6, GCC_4_7, GCC_4_8, GCC_4_9, GCC_5, GCC_6, GCC_7,
    GCC_8 and GCC_9. Adding support for CLANG_5 and other interesting
    versions is straightforward here. GNU compilers starting with gcc-4.2
    could support it in principle, but "#pragma GCC diagnostic push"
    was only added in gcc-4.6, so it seems simpler to not deal with those
    at all. The same versions show a large number of warnings already,
    so it seems easier to just leave it at that and not do a more
    fine-grained control for them.
    
    The use cases I found so far include:
    
    - turning off the gcc-8 -Wattribute-alias warning inside of the
      SYSCALL_DEFINEx() macro without having to do it globally.
    
    - Reducing the build time for a simple re-make after a change,
      once we move the warnings from ./Makefile and
      ./scripts/Makefile.extrawarn into linux/compiler.h
    
    - More control over the warnings based on other configurations,
      using preprocessor syntax instead of Makefile syntax. This should make
      it easier for the average developer to understand and change things.
    
    - Adding an easy way to turn the W=1 option on unconditionally
      for a subdirectory or a specific file. This has been requested
      by several developers in the past that want to have their subsystems
      W=1 clean.
    
    - Integrating clang better into the build systems. Clang supports
      more warnings than GCC, and we probably want to classify them
      as default, W=1, W=2 etc, but there are cases in which the
      warnings should be classified differently due to excessive false
      positives from one or the other compiler.
    
    - Adding a way to turn the default warnings into errors (e.g. using
      a new "make E=0" tag) while not also turning the W=1 warnings into
      errors.
    
    This patch for now just adds the minimal infrastructure in order to
    do the first of the list above. As the #pragma GCC diagnostic
    takes precedence over command line options, the next step would be
    to convert a lot of the individual Makefiles that set nonstandard
    options to use __diag() instead.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    I'm marking this RFC for now, as I'd like to get consensus about
    whether we want to go down this particular route first, and maybe
    if someone can come up with better naming for the macros.

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -172,8 +172,52 @@
 #define __diag_str(s)		__diag_str1(s)
 #define __diag(s)		_Pragma(__diag_str(GCC diagnostic s))
 
+#define __diag_GCC_4_6(s)	__diag(s)
+
+#if GCC_VERSION >= 40700
+#define __diag_GCC_4_7(s)	__diag(s)
+#else
+#define __diag_GCC_4_7(s)
+#endif
+
+#if GCC_VERSION >= 40800
+#define __diag_GCC_4_8(s)	__diag(s)
+#else
+#define __diag_GCC_4_8(s)
+#endif
+
+#if GCC_VERSION >= 40900
+#define __diag_GCC_4_9(s)	__diag(s)
+#else
+#define __diag_GCC_4_9(s)
+#endif
+
+#if GCC_VERSION >= 50000
+#define __diag_GCC_5(s)		__diag(s)
+#else
+#define __diag_GCC_5(s)
+#endif
+
+#if GCC_VERSION >= 60000
+#define __diag_GCC_6(s)		__diag(s)
+#else
+#define __diag_GCC_6(s)
+#endif
+
+#if GCC_VERSION >= 70000
+#define __diag_GCC_7(s)		__diag(s)
+#else
+#define __diag_GCC_7(s)
+#endif
+
 #if GCC_VERSION >= 80000
 #define __diag_GCC_8(s)		__diag(s)
 #else
 #define __diag_GCC_8(s)
+#endif
+
+#if GCC_VERSION >= 90000
+#define __diag_GCC_9(s)		__diag(s)
+#else
+#define __diag_GCC_9(s)
 #endif
