a358b7e0aec0d74a93200a40ecbd0abb9f5a1f06
Arnd Bergmann <arnd@arndb.de>
2018-02-05 17:18:10 +0100

    fixup! [SUBMITTED 20180205] modpost: don't add warnings for LTO-generated symbols
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1027,7 +1027,7 @@ static const struct sectioncheck sectioncheck[] = {
 	.mismatch = DATA_TO_ANY_INIT,
 	.symbol_white_list = {
 		"*_template", "*_timer", "*_sht", "*_ops", "*_ops.lto_priv*",
-		"*_probe", "*_probe_one", "*_console", NULL
+		"*_probe", "*_probe_one", "*_console", "*_console.lto_priv*", NULL
 	},
 },
 {
