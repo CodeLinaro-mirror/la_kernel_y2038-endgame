fe87842b373a9e6ed00b5ad7e677e66677eecb5d
Arnd Bergmann <arnd@arndb.de>
2018-02-27 22:53:17 +0100

    [SUBMITTED 20180228] ext2: fix FS_IOMAP dependency
    I ran into a randconfig link error on next-20180228 that appears to have
    been caused by an older patch:
    
    fs/ext2/inode.o: In function `ext2_setattr':
    inode.c:(.text+0x19eb): undefined reference to `iomap_zero_range'
    
    This adds a compile-time conditional around the code path so the
    compiler can optimize out the reference in configurations that don't
    need it anyway.
    
    Fixes: 00697eed386d ("ext2: use iomap_zero_range() for zeroing truncated page in DAX path")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/ext2/inode.c
+++ fs/ext2/inode.c
@@ -1289,7 +1289,7 @@ static int ext2_setsize(struct inode *inode, loff_t newsize)
 
 	inode_dio_wait(inode);
 
-	if (IS_DAX(inode)) {
+	if (IS_ENABLED(CONFIG_FS_DAX) && IS_DAX(inode)) {
 		error = iomap_zero_range(inode, newsize,
 					 PAGE_ALIGN(newsize) - newsize, NULL,
 					 &ext2_iomap_ops);
