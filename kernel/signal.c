ebc3d1a47db3f8fdcc92bf740a105c31424e9539
Arnd Bergmann <arnd@arndb.de>
2018-04-18 16:18:35 +0200

    y2038: signal: Add compat_sys_rt_sigtimedwait_time64
    Now that 32-bit architectures have two variants of
    sys_rt_sigtimedwaid() for 32-bit and 64-bit time_t, we also
    need to have a second compat system call entry point on the
    corresponding 64-bit architectures.
    
    The traditional system call keeps getting handled
    by compat_sys_rt_sigtimedwait(), and this adds a new
    compat_sys_rt_sigtimedwait_time64() that differs only in
    the timeout argument type.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/signal.c
+++ kernel/signal.c
@@ -3172,6 +3172,38 @@ COMPAT_SYSCALL_DEFINE4(rt_sigtimedwait, compat_sigset_t __user *, uthese,
 }
 #endif
 
+#ifdef CONFIG_COMPAT
+COMPAT_SYSCALL_DEFINE4(rt_sigtimedwait_time64, compat_sigset_t __user *, uthese,
+		struct compat_siginfo __user *, uinfo,
+		struct __kernel_timespec __user *, uts, compat_size_t, sigsetsize)
+{
+	sigset_t s;
+	struct timespec64 t;
+	siginfo_t info;
+	long ret;
+
+	if (sigsetsize != sizeof(sigset_t))
+		return -EINVAL;
+
+	if (get_compat_sigset(&s, uthese))
+		return -EFAULT;
+
+	if (uts) {
+		if (get_timespec64(&t, uts))
+			return -EFAULT;
+	}
+
+	ret = do_sigtimedwait(&s, &info, uts ? &t : NULL);
+
+	if (ret > 0 && uinfo) {
+		if (copy_siginfo_to_user32(uinfo, &info))
+			ret = -EFAULT;
+	}
+
+	return ret;
+}
+#endif
+
 /**
  *  sys_kill - send a signal to a process
  *  @pid: the PID of the process
