27024ed5ebaf2882b14a5128e1af9e90f387de4a
Arnd Bergmann <arnd@arndb.de>
2018-04-20 13:01:34 +0200

    rusage: allow 64-bit times ru_utime/ru_stime
    As part of the y2038 system call work, the getrusage/wait4/waitid system
    calls came up, since they pass time data to user space in 'timeval'
    format as required by POSIX. This means the existing kernel data structure
    is no longer compatible with the one from user space once we have a C
    library exposing 64-bit time_t, requiring the library to convert the
    kernel structure into its own structure.
    
    This patch moves that conversion into the kernel itself, providing a
    set of system calls that can directly be used to implement the libc
    getrusage/wait4/waitid functions as we have traditionally done.
    
    There are two advantages to this:
    
    - The new path becomes the native case, avoiding the conversion
      overhead for future 32-bit C libraries.  At least glibc will still
      have to implement a conversion logic as a fallback in order to run
      new applications on older kernels, but that does not have to be used
      on new kernels.
    - The range for the ru_utime/ru_stime is no longer limited to
      a 31-bit second counter (about 68 years). That limit may theoretically
      be hit on large SMP systems with a single process running for an
      extended time, e.g. 256 concurrent threads running for more than
      97 days.  Note that there is no overflow in 2038, as all the times
      are relative to the start of a process.
    
    The downside of this is obviously the added complexity of having three
    additional system call entry points plus their respective compat handlers,
    and updated syscall tables on each architecture (not included in this
    patch). Overall, I think this is *not* worth it, but I feel it's important
    to show how it can be done and what the cost is.
    
    There are probably some minor improvements that can be implemented on top,
    as well as bugs that I introduce.  When reviewing this patch, let's for
    now focus instead on the question whether we want it at all or not.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -38,6 +38,7 @@ struct pollfd;
 struct rlimit;
 struct rlimit64;
 struct rusage;
+struct __kernel_rusage;
 struct sched_param;
 struct sched_attr;
 struct sel_arg_struct;
@@ -536,6 +537,10 @@ asmlinkage long sys_waitid(int which, pid_t pid,
 			   struct siginfo __user *infop,
 			   int options, struct rusage __user *ru);
 
+asmlinkage long sys_waitid_time64(int which, pid_t pid,
+			   struct siginfo __user *infop,
+			   int options, struct __kernel_rusage __user *ru);
+
 /* kernel/fork.c */
 asmlinkage long sys_set_tid_address(int __user *tidptr);
 asmlinkage long sys_unshare(unsigned long unshare_flags);
@@ -670,6 +675,7 @@ asmlinkage long sys_getrlimit(unsigned int resource,
 asmlinkage long sys_setrlimit(unsigned int resource,
 				struct rlimit __user *rlim);
 asmlinkage long sys_getrusage(int who, struct rusage __user *ru);
+asmlinkage long sys_getrusage_time64(int who, struct __kernel_rusage __user *ru);
 asmlinkage long sys_umask(int mask);
 asmlinkage long sys_prctl(int option, unsigned long arg2, unsigned long arg3,
 			unsigned long arg4, unsigned long arg5);
@@ -835,6 +841,8 @@ asmlinkage long sys_recvmmsg(int fd, struct mmsghdr __user *msg,
 
 asmlinkage long sys_wait4(pid_t pid, int __user *stat_addr,
 				int options, struct rusage __user *ru);
+asmlinkage long sys_wait4_time64(pid_t pid, int __user *stat_addr,
+				int options, struct __kernel_rusage __user *ru);
 asmlinkage long sys_prlimit64(pid_t pid, unsigned int resource,
 				const struct rlimit64 __user *new_rlim,
 				struct rlimit64 __user *old_rlim);
