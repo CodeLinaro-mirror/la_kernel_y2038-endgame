23e3abcc451cabf66e231edb12ba5e67b01b78c6
Arnd Bergmann <arnd@arndb.de>
2018-06-20 10:07:19 +0200

    add fixme for div_s64_rem
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/time/time.c
+++ kernel/time/time.c
@@ -527,7 +527,7 @@ struct timespec64 ns_to_timespec64(const s64 nsec)
 
 	if (!nsec)
 		return (struct timespec64) {0, 0};
-
+/* FIXME! */
 	ts.tv_sec = div_s64_rem(nsec, NSEC_PER_SEC, &rem);
 	if (unlikely(rem < 0)) {
 		ts.tv_sec--;
