14ca6ab606969d2d6ba4b0e37ea66a39df8e5121
Arnd Bergmann <arnd@arndb.de>
2018-07-12 12:35:37 +0200

    fixup io_getevents

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -299,7 +299,7 @@ asmlinkage long sys_io_getevents(aio_context_t ctx_id,
 				long min_nr,
 				long nr,
 				struct io_event __user *events,
-				struct __kernel_timespec __user *timeout);
+				struct __kernel_old_timespec __user *timeout);
 asmlinkage long sys_io_pgetevents(aio_context_t ctx_id,
 				long min_nr,
 				long nr,
