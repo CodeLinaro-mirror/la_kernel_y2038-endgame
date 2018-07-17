0331a816cb1f3ebe4b926d885f4130c695d7f52d
Arnd Bergmann <arnd@arndb.de>
2018-07-17 13:06:51 +0200

    Revert "fixup io_getevents"
    This reverts commit dabeedc0e42b3172b054eec909c655d27a746566.

--- fs/aio.c
+++ fs/aio.c
@@ -2069,12 +2069,12 @@ SYSCALL_DEFINE5(io_getevents, aio_context_t, ctx_id,
 		long, min_nr,
 		long, nr,
 		struct io_event __user *, events,
-		struct __kernel_old_timespec __user *, timeout)
+		struct __kernel_timespec __user *, timeout)
 {
 	struct timespec64	ts;
 	int			ret;
 
-	if (timeout && unlikely(get_old_timespec(&ts, timeout)))
+	if (timeout && unlikely(get_timespec64(&ts, timeout)))
 		return -EFAULT;
 
 	ret = do_io_getevents(ctx_id, min_nr, nr, events, timeout ? &ts : NULL);
