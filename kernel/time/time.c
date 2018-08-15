57ad63229019a7c6d87f8fd58afdf5284add6c31
Arnd Bergmann <arnd@arndb.de>
2018-08-15 14:05:52 +0200

    rename __ARCH_WANT_COMPAT_SYS_TIME to __ARCH_WANT_SYS_TIME32
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- kernel/time/time.c
+++ kernel/time/time.c
@@ -102,7 +102,7 @@ SYSCALL_DEFINE1(stime, time_t __user *, tptr)
 #endif /* __ARCH_WANT_SYS_TIME */
 
 #ifdef CONFIG_COMPAT_32BIT_TIME
-#ifdef __ARCH_WANT_COMPAT_SYS_TIME
+#ifdef __ARCH_WANT_SYS_TIME32
 
 /* old_time32_t is a 32 bit "long" and needs to get converted. */
 COMPAT_SYSCALL_DEFINE1(time, old_time32_t __user *, tloc)
@@ -137,7 +137,7 @@ COMPAT_SYSCALL_DEFINE1(stime, old_time32_t __user *, tptr)
 	return 0;
 }
 
-#endif /* __ARCH_WANT_COMPAT_SYS_TIME */
+#endif /* __ARCH_WANT_SYS_TIME32 */
 #endif
 
 SYSCALL_DEFINE2(gettimeofday, struct timeval __user *, tv,
