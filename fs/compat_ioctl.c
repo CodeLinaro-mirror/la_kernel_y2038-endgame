3c42b92f703df3a485e03163f9e2147895c6b9be
Arnd Bergmann <arnd@arndb.de>
2018-09-11 16:55:03 +0200

    compat_ioctl: add compat_ptr_ioctl()
    Many drivers have ioctl() handlers that are completely compatible between
    32-bit and 64-bit architectures, except for the argument that is passed
    down from user space and may have to be passed through compat_ptr()
    in order to become a valid 64-bit pointer.
    
    Using ".compat_ptr = compat_ptr_ioctl" in file operations should let
    us simplify a lot of those drivers to avoid #ifdef checks, and convert
    additional drivers that don't have proper compat handling yet.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    v2: use compat_ptr_ioctl instead of generic_compat_ioctl_ptrarg,
    as suggested by Al Viro

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -78,6 +78,16 @@
 	get_user(val, srcptr) || put_user(val, dstptr);	\
 })
 
+/* helper function to avoid trivial compat_ioctl() implementations */
+long compat_ptr_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	if (!file->f_op->unlocked_ioctl)
+		return -ENOIOCTLCMD;
+
+	return file->f_op->unlocked_ioctl(file, cmd, (unsigned long)compat_ptr(arg));
+}
+EXPORT_SYMBOL(compat_ptr_ioctl);
+
 static inline int do_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int err;
