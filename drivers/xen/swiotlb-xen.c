70c3b7ac0cd4dbf8f362fcf88385b9b8b217ba21
Arnd Bergmann <arnd@arndb.de>
2019-03-01 09:35:23 +0100

    [SUBMITTED 20190304] Revert "swiotlb: remove SWIOTLB_MAP_ERROR"
    This reverts commit b907e20508d0 ("swiotlb: remove SWIOTLB_MAP_ERROR"), which
    introduced an overflow warning in configurations that have a larger
    dma_addr_t than phys_addr_t:
    
    In file included from include/linux/dma-direct.h:5,
                     from kernel/dma/swiotlb.c:23:
    kernel/dma/swiotlb.c: In function 'swiotlb_tbl_map_single':
    include/linux/dma-mapping.h:136:28: error: conversion from 'long long unsigned int' to 'phys_addr_t' {aka 'unsigned int'} changes value from '18446744073709551615' to '4294967295' [-Werror=overflow]
     #define DMA_MAPPING_ERROR  (~(dma_addr_t)0)
                                ^
    kernel/dma/swiotlb.c:544:9: note: in expansion of macro 'DMA_MAPPING_ERROR'
      return DMA_MAPPING_ERROR;
    
    The configuration that caused this is on 32-bit ARM, where the DMA address
    space depends on the enabled hardware platforms, while the physical
    address space depends on the type of MMU chosen (classic vs LPAE).
    
    I tried a couple of alternative approaches, but the previous version
    seems as good as any other, so I went back to that.
    
    Fixes: b907e20508d0 ("swiotlb: remove SWIOTLB_MAP_ERROR")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/xen/swiotlb-xen.c
+++ drivers/xen/swiotlb-xen.c
@@ -406,7 +406,7 @@ static dma_addr_t xen_swiotlb_map_page(struct device *dev, struct page *page,
 
 	map = swiotlb_tbl_map_single(dev, start_dma_addr, phys, size, dir,
 				     attrs);
-	if (map == DMA_MAPPING_ERROR)
+	if (map == SWIOTLB_MAP_ERROR)
 		return DMA_MAPPING_ERROR;
 
 	dev_addr = xen_phys_to_bus(map);
@@ -557,7 +557,7 @@ xen_swiotlb_map_sg_attrs(struct device *hwdev, struct scatterlist *sgl,
 								 sg_phys(sg),
 								 sg->length,
 								 dir, attrs);
-			if (map == DMA_MAPPING_ERROR) {
+			if (map == SWIOTLB_MAP_ERROR) {
 				dev_warn(hwdev, "swiotlb buffer is full\n");
 				/* Don't panic here, we expect map_sg users
 				   to do proper error handling. */
