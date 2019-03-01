eb1065df8c527941a3a37da7bccdac207ac60364
Arnd Bergmann <arnd@arndb.de>
2019-03-01 21:29:22 +0100

    [SUBMITTED 20190307] [v3] dma-mapping: work around clang bug
    Clang has a rather annoying behavior of checking for integer
    arithmetic problems in code paths that are discarded by gcc
    before that perfoms the same checks.
    
    For DMA_BIT_MASK(64), this leads to a warning despite the
    result of the macro being completely sensible:
    
    arch/arm/plat-iop/adma.c:146:24: error: shift count >= width of type [-Werror,-Wshift-count-overflow]
                    .coherent_dma_mask = DMA_BIT_MASK(64),
    
    The best workaround I could come up with is to shift the
    value twice, which makes the macro way less readable but
    always has the same result.
    
    Link: https://bugs.llvm.org/show_bug.cgi?id=38789
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    v3: use (2ull << n-1) instead of ((1ull << n-1) << 1)
        special-case 0 instead of 64
    v2: fix off-by-one error

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -138,7 +138,11 @@ struct dma_map_ops {
 extern const struct dma_map_ops dma_virt_ops;
 extern const struct dma_map_ops dma_dummy_ops;
 
-#define DMA_BIT_MASK(n)	(((n) == 64) ? ~0ULL : ((1ULL<<(n))-1))
+/*
+ * Shifting '2' instead of '1' because of
+ * https://bugs.llvm.org/show_bug.cgi?id=38789
+ */
+#define DMA_BIT_MASK(n)	(((n) == 0) ? 0ULL : (2ULL<<((n)-1))-1)
 
 #define DMA_MASK_NONE	0x0ULL
 
