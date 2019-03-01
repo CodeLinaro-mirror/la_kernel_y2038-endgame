639fce76d27f21728cda3c5d2218839c59e12364
Arnd Bergmann <arnd@arndb.de>
2019-03-01 21:29:22 +0100

    dma-mapping: work around clang bug
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -138,7 +138,7 @@ struct dma_map_ops {
 extern const struct dma_map_ops dma_virt_ops;
 extern const struct dma_map_ops dma_dummy_ops;
 
-#define DMA_BIT_MASK(n)	(((n) == 64) ? ~0ULL : ((1ULL<<(n))-1))
+#define DMA_BIT_MASK(n)	(((n) == 64) ? ~0ULL : (((1ULL<<((n)-1))-1) << 1))
 
 #define DMA_MASK_NONE	0x0ULL
 
