477a024df6e0ef546653529c8a88cc6336e623c6
Arnd Bergmann <arnd@arndb.de>
2019-03-13 11:12:20 +0100

    ARM: uaccess: reimplement access_ok() in C
    It turns out that modern compilers can do better than the
    inline assembly because they are able to combine repeated
    checks of the same area and schedule the instructions.
    
    The adds/sbcscc pair of instructions is replaced with two
    comparisons here, but we are able to use the resulting
    condition code directly rather than having to go through
    a register.
    
    Note: the return value of __range_ok() is inverted now,
    it returns '1' if the access is ok rather than '0'.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/include/asm/uaccess.h
+++ arch/arm/include/asm/uaccess.h
@@ -81,16 +81,13 @@ static inline void set_fs(mm_segment_t fs)
 
 #define segment_eq(a, b)	((a) == (b))
 
-/* We use 33-bit arithmetic here... */
-#define __range_ok(addr, size) ({ \
-	unsigned long flag, roksum; \
-	__chk_user_ptr(addr);	\
-	__asm__(".syntax unified\n" \
-		"adds %1, %2, %3; sbcscc %1, %1, %0; movcc %0, #0" \
-		: "=&r" (flag), "=&r" (roksum) \
-		: "r" (addr), "Ir" (size), "0" (current_thread_info()->addr_limit) \
-		: "cc"); \
-	flag; })
+static inline int __access_ok(unsigned long addr, unsigned long size)
+{
+	unsigned long limit = current_thread_info()->addr_limit;
+
+	return (addr+size) > addr && (addr+size) < limit;
+}
+#define access_ok(addr, size)	__access_ok((unsigned long)addr, size)
 
 /*
  * This is a type: either unsigned long, if the argument fits into
@@ -266,8 +263,7 @@ extern int __put_user_8(void *, unsigned long long);
 #define USER_DS			KERNEL_DS
 
 #define segment_eq(a, b)		(1)
-#define __addr_ok(addr)		((void)(addr), 1)
-#define __range_ok(addr, size)	((void)(addr), 0)
+#define access_ok(addr, size)	((void)(addr), 1)
 #define get_fs()		(KERNEL_DS)
 
 static inline void set_fs(mm_segment_t fs)
@@ -279,7 +275,6 @@ static inline void set_fs(mm_segment_t fs)
 
 #endif /* CONFIG_MMU */
 
-#define access_ok(addr, size)	(__range_ok(addr, size) == 0)
 
 #define user_addr_max() \
 	(uaccess_kernel() ? ~0UL : get_fs())
