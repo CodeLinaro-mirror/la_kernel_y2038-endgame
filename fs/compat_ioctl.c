91280d2ca057c5b259c27e46eceb3c313f7790c2
Arnd Bergmann <arnd@arndb.de>
2019-03-14 11:22:38 +0100

    compat_ioctl: remove unused convert_in_user macro
    The last users are all gone, so let's remove the macro as well.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -52,13 +52,6 @@
 
 #include <linux/sort.h>
 
-#define convert_in_user(srcptr, dstptr)			\
-({							\
-	typeof(*srcptr) val;				\
-							\
-	get_user(val, srcptr) || put_user(val, dstptr);	\
-})
-
 /* helper function to avoid trivial compat_ioctl() implementations */
 long compat_ptr_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
