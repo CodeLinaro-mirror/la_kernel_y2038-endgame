d32e2762c4d0a59b15a0aab694b84fcc815f88d9
Arnd Bergmann <arnd@arndb.de>
2019-03-15 17:39:44 +0100

    compat_ioctl: scsi: move ioctl handling into drivers
    Each driver calling scsi_cmd_blk_ioctl() and/or scsi_ioctl() gets
    an equivalent compat_ioctl() handler that implements the same
    commands by calling scsi_cmd_blk_compat_ioctl()/scsi_compat_ioctl().
    
    With this, we can remove the entries from fs/compat_ioctl.c.
    The new code is larger, but should be easier to maintain
    and keep updated with newly added commands.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/scsi/st.c
+++ drivers/scsi/st.c
@@ -3501,7 +3501,7 @@ out:
 
 
 /* The ioctl command */
-static long st_ioctl(struct file *file, unsigned int cmd_in, unsigned long arg)
+static long st_ioctl_common(struct file *file, unsigned int cmd_in, void __user *p)
 {
 	int i, cmd_nr, cmd_type, bt;
 	int retval = 0;
@@ -3509,7 +3509,6 @@ static long st_ioctl(struct file *file, unsigned int cmd_in, unsigned long arg)
 	struct scsi_tape *STp = file->private_data;
 	struct st_modedef *STm;
 	struct st_partstat *STps;
-	void __user *p = (void __user *)arg;
 
 	if (mutex_lock_interruptible(&STp->lock))
 		return -ERESTARTSYS;
@@ -3822,7 +3821,22 @@ static long st_ioctl(struct file *file, unsigned int cmd_in, unsigned long arg)
 		retval = put_user_mtpos(p, &mt_pos);
 		goto out;
 	}
+ out:
 	mutex_unlock(&STp->lock);
+	return retval;
+}
+
+static long st_ioctl(struct file *file, unsigned int cmd_in, unsigned long arg)
+{
+	void __user *p = (void __user *)arg;
+	struct scsi_tape *STp = file->private_data;
+	int i, retval;
+
+	retval = st_ioctl_common(file, cmd_in, p);
+
+	if (retval != -ENOIOCTLCMD)
+		return retval;
+
 	switch (cmd_in) {
 		case SCSI_IOCTL_GET_IDLUN:
 		case SCSI_IOCTL_GET_BUS_NUMBER:
@@ -3846,10 +3860,6 @@ static long st_ioctl(struct file *file, unsigned int cmd_in, unsigned long arg)
 		STp->ready = ST_NO_TAPE;
 	}
 	return retval;
-
- out:
-	mutex_unlock(&STp->lock);
-	return retval;
 }
 
 #ifdef CONFIG_COMPAT
@@ -3857,25 +3867,44 @@ static long st_compat_ioctl(struct file *file, unsigned int cmd_in, unsigned lon
 {
 	void __user *p = compat_ptr(arg);
 	struct scsi_tape *STp = file->private_data;
-	struct scsi_device *sdev = STp->device;
-	int ret = -ENOIOCTLCMD;
+	int i, retval;
 
 	/* argument conversion is handled using put_user_mtpos/put_user_mtget */
 	switch (cmd_in) {
-	case MTIOCTOP:
-		return st_ioctl(file, MTIOCTOP, (unsigned long)p);
 	case MTIOCPOS32:
-		return st_ioctl(file, MTIOCPOS, (unsigned long)p);
+		return st_ioctl_common(file, MTIOCPOS, p);
 	case MTIOCGET32:
-		return st_ioctl(file, MTIOCGET, (unsigned long)p);
+		return st_ioctl_common(file, MTIOCGET, p);
 	}
 
-	if (sdev->host->hostt->compat_ioctl) { 
+	retval = st_ioctl_common(file, cmd_in, p);
 
-		ret = sdev->host->hostt->compat_ioctl(sdev, cmd_in, (void __user *)arg);
+	if (retval != -ENOIOCTLCMD)
+		return retval;
 
+	switch (cmd_in) {
+		case SCSI_IOCTL_GET_IDLUN:
+		case SCSI_IOCTL_GET_BUS_NUMBER:
+			break;
+		default:
+			if ((cmd_in == SG_IO ||
+			     cmd_in == SCSI_IOCTL_SEND_COMMAND ||
+			     cmd_in == CDROM_SEND_PACKET) &&
+			    !capable(CAP_SYS_RAWIO))
+				i = -EPERM;
+			else
+				i = scsi_cmd_compat_ioctl(STp->disk->queue, STp->disk,
+							  file->f_mode, cmd_in, p);
+			if (i != -ENOTTY)
+				return i;
+			break;
 	}
-	return ret;
+	retval = scsi_compat_ioctl(STp->device, cmd_in, p);
+	if (!retval && cmd_in == SCSI_IOCTL_STOP_UNIT) { /* unload */
+		STp->rew_at_close = 0;
+		STp->ready = ST_NO_TAPE;
+	}
+	return retval;
 }
 #endif
 
