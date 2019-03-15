f27369aa297d828d51d8c8f7ea79dc3d973ca493
Arnd Bergmann <arnd@arndb.de>
2019-03-15 17:39:44 +0100

    compat_ioctl: scsi: move ioctl handling into drivers
    Each driver calling scsi_cmd_blk_ioctl() and/or scsi_ioctl() gets
    an equivalent compat_ioctl() handler that implements the same
    commands by calling scsi_cmd_blk_compat_ioctl()/scsi_compat_ioctl().
    
    With this, we can remove the entries from fs/compat_ioctl.c.
    The new code is larger, but should be easier to maintain
    and keep updated with newly added commands.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -1695,15 +1695,18 @@ static int sd_compat_ioctl(struct block_device *bdev, fmode_t mode,
 {
 	struct gendisk *disk = bdev->bd_disk;
 	struct scsi_disk *sdkp = scsi_disk(disk);
-	struct scsi_device *sdev = sdkp->device;
+	struct scsi_device *sdp = sdkp->device;
 	void __user *p = compat_ptr(arg);
 	int error;
+
+	SCSI_LOG_IOCTL(1, sd_printk(KERN_INFO, sdkp, "sd_ioctl: disk=%s, "
+				    "cmd=0x%x\n", disk->disk_name, cmd));
 
 	error = scsi_verify_blk_ioctl(bdev, cmd);
 	if (error < 0)
 		return error;
 
-	error = scsi_ioctl_block_when_processing_errors(sdev, cmd,
+	error = scsi_ioctl_block_when_processing_errors(sdp, cmd,
 			(mode & FMODE_NDELAY) != 0);
 	if (error)
 		return error;
@@ -1711,12 +1714,19 @@ static int sd_compat_ioctl(struct block_device *bdev, fmode_t mode,
 	if (is_sed_ioctl(cmd))
 		return sed_ioctl(sdkp->opal_dev, cmd, p);
 	       
-	/* 
-	 * Let the static ioctl translation table take care of it.
-	 */
-	if (!sdev->host->hostt->compat_ioctl)
-		return -ENOIOCTLCMD; 
-	return sdev->host->hostt->compat_ioctl(sdev, cmd, p);
+	switch (cmd) {
+		case SCSI_IOCTL_GET_IDLUN:
+		case SCSI_IOCTL_GET_BUS_NUMBER:
+			error = scsi_compat_ioctl(sdp, cmd, p);
+			break;
+		default:
+			error = scsi_cmd_blk_compat_ioctl(bdev, mode, cmd, p);
+			if (error != -ENOTTY)
+				break;
+			error = scsi_compat_ioctl(sdp, cmd, p);
+			break;
+	}
+	return error;
 }
 #endif
 
