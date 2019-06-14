ac662a8e67016011f93a78760696c2d39fea98c2
Arnd Bergmann <arnd@arndb.de>
2019-06-14 22:50:09 +0200

    [SUBMITTED 20190617] ima: dynamically allocate shash_desc
    On 32-bit ARM, we get a warning about excessive stack usage when
    building with clang.
    
    security/integrity/ima/ima_crypto.c:504:5: error: stack frame size of 1152 bytes in function 'ima_calc_field_array_hash' [-Werror,-Wframe-larger-than=]
    
    Using kmalloc to get the descriptor reduces this to 320 bytes.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- security/integrity/ima/ima_crypto.c
+++ security/integrity/ima/ima_crypto.c
@@ -461,8 +461,13 @@ static int ima_calc_field_array_hash_tfm(struct ima_field_data *field_data,
 					 struct ima_digest_data *hash,
 					 struct crypto_shash *tfm)
 {
-	SHASH_DESC_ON_STACK(shash, tfm);
+	struct shash_desc *shash;
 	int rc, i;
+
+	shash = kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(tfm),
+			GFP_KERNEL);
+	if (!shash)
+		return -ENOMEM;
 
 	shash->tfm = tfm;
 
@@ -470,7 +475,7 @@ static int ima_calc_field_array_hash_tfm(struct ima_field_data *field_data,
 
 	rc = crypto_shash_init(shash);
 	if (rc != 0)
-		return rc;
+		goto out;
 
 	for (i = 0; i < num_fields; i++) {
 		u8 buffer[IMA_EVENT_NAME_LEN_MAX + 1] = { 0 };
@@ -497,7 +502,8 @@ static int ima_calc_field_array_hash_tfm(struct ima_field_data *field_data,
 
 	if (!rc)
 		rc = crypto_shash_final(shash, hash->digest);
-
+out:
+	kfree(shash);
 	return rc;
 }
 
