0bb120c1b95216b888b84c0648244c92d7bf5cc9
Arnd Bergmann <arnd@arndb.de>
2019-07-09 17:33:02 +0200

    [SUBMITTED 20190709] mm/kasan: fix kasan_check_read() compiler warning
    The kasan_check_read() is marked 'inline', which usually includes
    the 'always_inline' attribute. In some configuration, gcc decides that
    it cannot inline this, causing a build failure:
    
    In file included from include/linux/compiler.h:257,
                     from arch/x86/include/asm/current.h:5,
                     from include/linux/sched.h:12,
                     from include/linux/ratelimit.h:6,
                     from fs/dcache.c:18:
    include/linux/compiler.h: In function 'read_word_at_a_time':
    include/linux/kasan-checks.h:31:20: error: inlining failed in call to always_inline 'kasan_check_read': function attribute mismatch
     static inline bool kasan_check_read(const volatile void *p, unsigned int size)
                        ^~~~~~~~~~~~~~~~
    In file included from arch/x86/include/asm/current.h:5,
                     from include/linux/sched.h:12,
                     from include/linux/ratelimit.h:6,
                     from fs/dcache.c:18:
    include/linux/compiler.h:280:2: note: called from here
      kasan_check_read(addr, 1);
      ^~~~~~~~~~~~~~~~~~~~~~~~~
    
    While I have no idea why it does this, but changing the call to the
    internal __kasan_check_read() fixes the issue.
    
    Fixes: dc55b51f312c ("mm/kasan: introduce __kasan_check_{read,write}")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -277,7 +277,7 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 static __no_kasan_or_inline
 unsigned long read_word_at_a_time(const void *addr)
 {
-	kasan_check_read(addr, 1);
+	__kasan_check_read(addr, 1);
 	return *(unsigned long *)addr;
 }
 
