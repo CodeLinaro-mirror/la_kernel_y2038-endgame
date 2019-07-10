4b93c888ab82dce323d04797a9e111faab7c9a51
Arnd Bergmann <arnd@arndb.de>
2019-07-10 23:33:38 +0200

    [ignored] drm/amd/display: reduce stack usage in dcn20_create_resource_pool()
    There are several functions in dcn20_resource.c that can all get
    inlined into dcn20_create_resource_pool(), leading to a rather
    large combined stack usage beyond the 32-bit warning level
    when building with clang:
    
    drivers/gpu/drm/amd/amdgpu/../display/dc/dcn20/dcn20_resource.c:3176:23: error: stack frame size of 1196 bytes in function 'dcn20_create_resource_pool' [-Werror,-Wframe-larger-than=]
    
    Mark some of the largest ones as noinline_for_stack so they
    do not get combined into the same stack frame but rather free
    up their space before we call the next function.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/dcn20/dcn20_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn20/dcn20_resource.c
@@ -3666,8 +3666,8 @@ static enum dml_project get_dml_project_version(uint32_t hw_internal_rev)
 #define fixed16_to_double(x) (((double) x) / ((double) (1 << 16)))
 #define fixed16_to_double_to_cpu(x) fixed16_to_double(le32_to_cpu(x))
 
-static bool init_soc_bounding_box(struct dc *dc,
-				  struct dcn20_resource_pool *pool)
+static bool noinline_for_stack
+init_soc_bounding_box(struct dc *dc, struct dcn20_resource_pool *pool)
 {
 	struct _vcs_dpi_soc_bounding_box_st *loaded_bb =
 			get_asic_rev_soc_bb(dc->ctx->asic_id.hw_internal_rev);
