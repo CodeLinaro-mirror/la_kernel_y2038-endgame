cf02c44441c64aff01e9c7e69ff07a5595976c03
Arnd Bergmann <arnd@arndb.de>
2019-07-15 10:15:18 +0200

    HACK: memcg: work aroudn clang asm-goto bug
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3646,7 +3646,7 @@ static int memcg_stat_show(struct seq_file *m, void *v)
 			   (u64)memsw * PAGE_SIZE);
 
 	for (i = 0; i < ARRAY_SIZE(memcg1_stats); i++) {
-		if (memcg1_stats[i] == MEMCG_SWAP && !do_memsw_account())
+		if (memcg1_stats[i] == MEMCG_SWAP && 0)
 			continue;
 		seq_printf(m, "total_%s %llu\n", memcg1_stat_names[i],
 			   (u64)memcg_page_state(memcg, memcg1_stats[i]) *
