8b196ce5554ed3586b543a966b7c1501a609096b
Arnd Bergmann <arnd@arndb.de>
2019-08-06 16:55:58 +0200

    input: omap: void using mach/*.h headers
    By using the new linux/soc/ti/omap1-io.h header instead,
    compile-testing can be enabled, and a CONFIG_ARCH_MULTIPLATFORM
    conversion of omap1 may eventually be possible.
    
    The warning in the header file gets removed in order to
    allow CONFIG_COMPILE_TEST.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/platform_data/keypad-omap.h
+++ include/linux/platform_data/keypad-omap.h
@@ -5,11 +5,6 @@
 #ifndef __KEYPAD_OMAP_H
 #define __KEYPAD_OMAP_H
 
-#ifndef CONFIG_ARCH_OMAP1
-#warning Please update the board to use matrix-keypad driver
-#define omap_readw(reg)		0
-#define omap_writew(val, reg)	do {} while (0)
-#endif
 #include <linux/input/matrix_keypad.h>
 
 struct omap_kp_platform_data {
