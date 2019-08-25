c57a50e63a848a4fa8571f12557e78abb64b526d
Arnd Bergmann <arnd@arndb.de>
2019-08-25 22:55:25 +0200

    ixp4xx_eth: move platform_data definition
    The platform data is needed to compile the driver as standalone,
    so move it to a global location along with similar files.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ixp4xx/include/mach/platform.h
+++ arch/arm/mach-ixp4xx/include/mach/platform.h
@@ -15,6 +15,7 @@
 #ifndef __ASSEMBLY__
 
 #include <linux/reboot.h>
+#include <linux/platform_data/eth_ixp4xx.h>
 
 #include <asm/types.h>
 
@@ -77,18 +78,6 @@ extern unsigned long ixp4xx_exp_bus_size;
  */
 #define IXP4XX_PERIPHERAL_BUS_CLOCK 	(66) /* 66MHzi APB BUS   */ 
 #define IXP4XX_UART_XTAL        	14745600
-
-#define IXP4XX_ETH_NPEA		0x00
-#define IXP4XX_ETH_NPEB		0x10
-#define IXP4XX_ETH_NPEC		0x20
-
-/* Information about built-in Ethernet MAC interfaces */
-struct eth_plat_info {
-	u8 phy;		/* MII PHY ID, 0 - 31 */
-	u8 rxq;		/* configurable, currently 0 - 31 only */
-	u8 txreadyq;
-	u8 hwaddr[6];
-};
 
 /*
  * Frequency of clock used for primary clocksource
