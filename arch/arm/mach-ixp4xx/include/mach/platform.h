80566cee96519f31e1d60801e831d99d73efe79b
Arnd Bergmann <arnd@arndb.de>
2019-08-25 22:14:01 +0200

    pata: ixp4xx: split platform data to its own header
    Portable drivers cannot use mach/platform.h, so move the
    structure into its own header. With this, compile testing
    can be enabled.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ixp4xx/include/mach/platform.h
+++ arch/arm/mach-ixp4xx/include/mach/platform.h
@@ -78,20 +78,6 @@ extern unsigned long ixp4xx_exp_bus_size;
 #define IXP4XX_PERIPHERAL_BUS_CLOCK 	(66) /* 66MHzi APB BUS   */ 
 #define IXP4XX_UART_XTAL        	14745600
 
-/*
- * This structure provide a means for the board setup code
- * to give information to th pata_ixp4xx driver. It is
- * passed as platform_data.
- */
-struct ixp4xx_pata_data {
-	volatile u32	*cs0_cfg;
-	volatile u32	*cs1_cfg;
-	unsigned long	cs0_bits;
-	unsigned long	cs1_bits;
-	void __iomem	*cs0;
-	void __iomem	*cs1;
-};
-
 #define IXP4XX_ETH_NPEA		0x00
 #define IXP4XX_ETH_NPEB		0x10
 #define IXP4XX_ETH_NPEC		0x20
