eb8347cecfce4eaaca914f1bea024a5024833598
Arnd Bergmann <arnd@arndb.de>
2019-08-26 16:38:14 +0200

    watchdog: ixp4xx: move mmio access to timer driver
    The registers for watchdog and timer overlap, and the timer
    driver already has a way to get hold of them, which the watchdog
    does not.
    
    Move the low-level access for the timeout and the boot status
    into the clocksource driver to make the watchdog driver more
    portable and enable compile testing it.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/platform_data/timer-ixp4xx.h
+++ include/linux/platform_data/timer-ixp4xx.h
@@ -8,4 +8,17 @@ void __init ixp4xx_timer_setup(resource_size_t timerbase,
 			       int timer_irq,
 			       unsigned int timer_freq);
 
+#ifdef CONFIG_IXP4XX_TIMER
+void ixp4xx_wdt_set(int heartbeat);
+int ixp4xx_boot_status(void);
+#else
+static inline void ixp4xx_wdt_set(int heartbeat)
+{
+}
+static inline int ixp4xx_boot_status(void)
+{
+	return -ENXIO;
+}
+#endif
+
 #endif
