30dc88bb51a1eb7744ad9e807e098b77b5e441b7
Arnd Bergmann <arnd@arndb.de>
2019-09-01 22:26:10 +0200

    ARM: pxa: split up mach/hardware.h
    The mach/hardware.h is included in lots of places, and it provides
    three different things on pxa:
    
    - the cpu_is_pxa* macros
    - an indirect inclusion of mach/addr-map.h
    - the __REG() and io_pv2() helper macros
    
    Split it up into separate <linux/soc/pxa/cpu.h> and mach/pxa-regs.h
    headers, then change all the files that use mach/hardware.h to
    include the exact set of those three headers that they actually
    need, allowing for further more targeted cleanup.
    
    linux/soc/pxa/cpu.h can remain permanently exported and is now in
    a global location along with similar headers. pxa-regs.h and
    addr-map.h are only used in a very small number of drivers now
    and can be moved to arch/arm/mach-pxa/ directly when those drivers
    are to pass the necessary data as resources.
    
    Cc: Michael Turquette <mturquette@baylibre.com>
    Cc: Stephen Boyd <sboyd@kernel.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Cc: Jacek Anaszewski <jacek.anaszewski@gmail.com>
    Cc: Pavel Machek <pavel@ucw.cz>
    Acked-by: Ulf Hansson <ulf.hansson@linaro.org>
    Cc: Dominik Brodowski <linux@dominikbrodowski.net>
    Acked-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Mark Brown <broonie@kernel.org>
    Cc: linux-clk@vger.kernel.org
    Cc: linux-pm@vger.kernel.org
    Cc: linux-input@vger.kernel.org
    Cc: linux-leds@vger.kernel.org
    Cc: linux-mmc@vger.kernel.org
    Cc: linux-mtd@lists.infradead.org
    Cc: linux-rtc@vger.kernel.org
    Cc: linux-usb@vger.kernel.org
    Cc: dri-devel@lists.freedesktop.org
    Cc: linux-fbdev@vger.kernel.org
    Cc: linux-watchdog@vger.kernel.org
    Cc: alsa-devel@alsa-project.org
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/pcmcia/pxa2xx_cm_x2xx.c
+++ drivers/pcmcia/pxa2xx_cm_x2xx.c
@@ -7,9 +7,9 @@
  */
 
 #include <linux/module.h>
+#include <linux/soc/pxa/cpu.h>
 
 #include <asm/mach-types.h>
-#include <mach/hardware.h>
 
 int cmx255_pcmcia_init(void);
 int cmx270_pcmcia_init(void);
