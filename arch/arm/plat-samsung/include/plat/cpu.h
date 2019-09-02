d8d6f444dd973be24dbe2ced92e86065ce54a957
Arnd Bergmann <arnd@arndb.de>
2019-09-02 15:52:13 +0200

    ARM: exynos: use private samsung_cpu_id copy
    The only part of plat-samsung that is shared with arch-exynos
    is the CPU identification code.
    
    Having a separate exynos_cpu_id variable makes the two completely
    independent and is actually a bit less code in total.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/plat-samsung/include/plat/cpu.h
+++ arch/arm/plat-samsung/include/plat/cpu.h
@@ -111,8 +111,6 @@ extern void s3c24xx_init_io(struct map_desc *mach_desc, int size);
 extern void s3c64xx_init_cpu(void);
 extern void s5p_init_cpu(const void __iomem *cpuid_addr);
 
-extern unsigned int samsung_rev(void);
-
 extern void s3c24xx_init_uarts(struct s3c2410_uartcfg *cfg, int no);
 
 extern void s3c24xx_init_clocks(int xtal);
