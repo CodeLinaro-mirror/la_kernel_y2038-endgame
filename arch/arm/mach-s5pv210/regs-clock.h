a5f14cd1e52afaf4f200dfd6c2c9f2556b1d51c1
Arnd Bergmann <arnd@arndb.de>
2019-09-02 16:56:00 +0200

    ARM: s5pv210: use private pm save/restore
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-s5pv210/regs-clock.h
+++ arch/arm/mach-s5pv210/regs-clock.h
@@ -9,7 +9,7 @@
 #ifndef __ASM_ARCH_REGS_CLOCK_H
 #define __ASM_ARCH_REGS_CLOCK_H __FILE__
 
-#include <plat/map-base.h>
+#define S3C_VA_SYS		((void __iomem __force *)0xF6100000)
 
 #define S5P_CLKREG(x)		(S3C_VA_SYS + (x))
 
