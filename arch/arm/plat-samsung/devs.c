004c6f1349872abc4f4776ea6e3b1895d3636dba
Arnd Bergmann <arnd@arndb.de>
2019-09-02 21:59:32 +0200

    usb: gadget: s3c-hsudc: remove platform header dependency
    There is no real phy driver, so s3c-hsudc just pokes the registers
    itself. Improve this a little by making it a platform data callback
    like we do for gpios.
    
    There is only one board using this driver, and it's unlikely
    that another would be added, so this is a minimal workaround.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/plat-samsung/devs.c
+++ arch/arm/plat-samsung/devs.c
@@ -40,6 +40,7 @@
 #include <mach/dma.h>
 #include <mach/irqs.h>
 #include <mach/map.h>
+#include <mach/regs-s3c2443-clock.h>
 
 #include <plat/cpu.h>
 #include <plat/devs.h>
@@ -1038,6 +1039,8 @@ struct platform_device s3c_device_usb_hsudc = {
 void __init s3c24xx_hsudc_set_platdata(struct s3c24xx_hsudc_platdata *pd)
 {
 	s3c_set_platdata(pd, sizeof(*pd), &s3c_device_usb_hsudc);
+	pd->phy_init = s3c_hsudc_init_phy;
+	pd->phy_uninit = s3c_hsudc_uninit_phy;
 }
 #endif /* CONFIG_PLAT_S3C24XX */
 
