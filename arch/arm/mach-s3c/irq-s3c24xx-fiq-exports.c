62a2850c4f252b13f8d46ad350fe7f605a07fe46
Arnd Bergmann <arnd@arndb.de>
2019-09-03 11:31:09 +0200

    ARM: s3c: move spi fiq handler into platform
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

---
+++ arch/arm/mach-s3c/irq-s3c24xx-fiq-exports.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <linux/export.h>
+#include <linux/spi/s3c24xx-fiq.h>
+
+EXPORT_SYMBOL(s3c24xx_spi_fiq_rx);
+EXPORT_SYMBOL(s3c24xx_spi_fiq_txrx);
+EXPORT_SYMBOL(s3c24xx_spi_fiq_tx);
