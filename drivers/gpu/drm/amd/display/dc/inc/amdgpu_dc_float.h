1e01d9e5bf06c014191ae46c0319e46f84d0c065
Arnd Bergmann <arnd@arndb.de>
2019-09-30 16:35:22 +0200

    amdgpu: use a union
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/inc/amdgpu_dc_float.h
+++ drivers/gpu/drm/amd/display/dc/inc/amdgpu_dc_float.h
@@ -3,7 +3,27 @@
 #ifndef __AMDGPU_DC_FLOAT_H__
 #define __AMDGPU_DC_FLOAT_H__
 
+#if 0
+typedef union {
+	float f;
+	unsigned int x;
+} amdgpu_dc_float;
+
+typedef union {
+	double d;
+	unsigned long long x;
+} amdgpu_dc_double;
+
+#define amdgpu_dc_float_constant(x) ((amdgpu_dc_float){ .f = (x) })
+#define amdgpu_dc_double_constant(x) ((amdgpu_dc_double){ .d = (x) })
+
+#else
 typedef float amdgpu_dc_float;
 typedef double amdgpu_dc_double;
+
+#define amdgpu_dc_float_constant(x) ((amdgpu_dc_float)(x))
+#define amdgpu_dc_double_constant(x) ((amdgpu_dc_double)(x))
+
+#endif
 
 #endif
