ee046427b5c6acc45bcbde07de719c1a561d7c4e
Arnd Bergmann <arnd@arndb.de>
2019-09-30 22:48:41 +0200

    Revert "[SUBMITTED 20190919] iwlwifi: fix building without CONFIG_THERMAL"
    This reverts commit 1ec78c36658b4ad74e5b4baf967260c21549b46b.

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -542,6 +542,7 @@ struct iwl_mvm_tt_mgmt {
 	bool throttle;
 };
 
+#ifdef CONFIG_THERMAL
 /**
  *struct iwl_mvm_thermal_device - thermal zone related data
  * @temp_trips: temperature thresholds for report
@@ -554,7 +555,6 @@ struct iwl_mvm_thermal_device {
 	struct thermal_zone_device *tzone;
 };
 
-#ifdef CONFIG_THERMAL
 /*
  * struct iwl_mvm_cooling_device
  * @cur_state: current state
@@ -1034,8 +1034,8 @@ struct iwl_mvm {
 
 	/* Thermal Throttling and CTkill */
 	struct iwl_mvm_tt_mgmt thermal_throttle;
-	struct iwl_mvm_thermal_device tz_device;
 #ifdef CONFIG_THERMAL
+	struct iwl_mvm_thermal_device tz_device;
 	struct iwl_mvm_cooling_device cooling_dev;
 #endif
 
