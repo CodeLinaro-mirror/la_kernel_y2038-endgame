7b39feb110503e7623c4cb344e8a8012af04888c
Arnd Bergmann <arnd@arndb.de>
2019-10-07 18:23:04 +0200

    [HACK] ARM: s3c: test multiplatform
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-s3c/include/mach/io-s3c24xx.h
+++ arch/arm/mach-s3c/include/mach/io-s3c24xx.h
@@ -12,16 +12,7 @@
 
 #include <mach/map-base.h>
 
-/*
- * ISA style IO, for each machine to sort out mappings for,
- * if it implements it. We reserve two 16M regions for ISA,
- * so the PC/104 can use separate addresses for 8-bit and
- * 16-bit port I/O.
- */
-#define PCIO_BASE		S3C_ADDR(0x02000000)
 #define IO_SPACE_LIMIT		0x00ffffff
-#define S3C24XX_VA_ISA_WORD	(PCIO_BASE)
-#define S3C24XX_VA_ISA_BYTE	(PCIO_BASE + 0x01000000)
 
 #ifdef CONFIG_ISA
 
