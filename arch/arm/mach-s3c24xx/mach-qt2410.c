701f3d26943e2bcd97cb7c505a009f6aaacf0bae
Arnd Bergmann <arnd@arndb.de>
2019-10-07 11:32:53 +0200

    ARM: s3c: move iis pinctrl config into boards
    The s3c_gpio_cfgall_range() function is an internal interface of the
    samsung gpio driver and should not be called directly by drivers, so
    move the iis pin initialization into the boards.
    
    This means the pin configuration is only run once at early boot, rather
    than each time the driver binds, but the effect should be the same.
    
    Note that the s3c2412-i2s driver has no boards using it in mainline linux,
    the driver gets selected for the jive machine but is never instantiated.
    
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-s3c24xx/mach-qt2410.c
+++ arch/arm/mach-s3c24xx/mach-qt2410.c
@@ -337,6 +337,9 @@ static void __init qt2410_machine_init(void)
 	s3c24xx_udc_set_platdata(&qt2410_udc_cfg);
 	s3c_i2c0_set_platdata(NULL);
 
+	/* Configure the I2S pins (GPE0...GPE4) in correct mode */
+	s3c_gpio_cfgall_range(S3C2410_GPE(0), 5, S3C_GPIO_SFN(2),
+			      S3C_GPIO_PULL_NONE);
 	gpiod_add_lookup_table(&qt2410_spi_gpiod_table);
 	platform_add_devices(qt2410_devices, ARRAY_SIZE(qt2410_devices));
 	s3c_pm_init();
