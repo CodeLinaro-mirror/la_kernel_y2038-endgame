67a5fc1e85873f41284e965495b46366d1ee098f
Vincenzo Frascino <(address hidden)>
2019-10-24 15:09:28 +0200

    Add clock_gettime64 entry point
    With the release of Linux 5.1 has been added a new syscall,
    clock_gettime64, that provided a 64 bit time value for a specified
    clock_ID to make the kernel Y2038 safe on 32 bit architectures.
    
    Update the arm specific vDSO library accordingly with what it has
    been done for the kernel syscall exposing the clock_gettime64 entry
    point.
    
    Cc: Russell King <(address hidden)>
    Signed-off-by: Vincenzo Frascino <(address hidden)>

--- arch/arm/vdso/vgettimeofday.c
+++ arch/arm/vdso/vgettimeofday.c
@@ -15,6 +15,12 @@ notrace int __vdso_clock_gettime(clockid_t clock,
 	return __cvdso_clock_gettime32(clock, ts);
 }
 
+notrace int __vdso_clock_gettime64(clockid_t clock,
+				 struct __kernel_timespec *ts)
+{
+	return __cvdso_clock_gettime(clock, ts);
+}
+
 notrace int __vdso_gettimeofday(struct __kernel_old_timeval *tv,
 				struct timezone *tz)
 {
