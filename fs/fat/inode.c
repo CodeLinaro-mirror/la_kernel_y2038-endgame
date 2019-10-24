6ead3fba4efabf7cecbc1fff8de5521baa862528
Arnd Bergmann <arnd@arndb.de>
2019-10-24 15:31:19 +0200

    fat: use prandom_u32() for i_generation
    Similar to commit 46c9a946d766 ("shmem: use monotonic time for i_generation")
    we should not use the deprecated get_seconds() interface for i_generation.
    
    prandom_u32() is the replacement used in other file systems.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/fat/inode.c
+++ fs/fat/inode.c
@@ -521,7 +521,7 @@ int fat_fill_inode(struct inode *inode, struct msdos_dir_entry *de)
 	inode->i_uid = sbi->options.fs_uid;
 	inode->i_gid = sbi->options.fs_gid;
 	inode_inc_iversion(inode);
-	inode->i_generation = get_seconds();
+	inode->i_generation = prandom_u32();
 
 	if ((de->attr & ATTR_DIR) && !IS_FREE(de->name)) {
 		inode->i_generation &= ~1;
