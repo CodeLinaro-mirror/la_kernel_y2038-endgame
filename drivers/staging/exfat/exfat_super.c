fdf34ab9dc9a8a186380f18bd2a22e53e0866b87
Arnd Bergmann <arnd@arndb.de>
2019-10-24 15:31:19 +0200

    staging: exfat: use prandom_u32() for i_generation
    Similar to commit 46c9a946d766 ("shmem: use monotonic time for i_generation")
    we should not use the deprecated get_seconds() interface for i_generation.
    
    prandom_u32() is the replacement used in other file systems.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/staging/exfat/exfat_super.c
+++ drivers/staging/exfat/exfat_super.c
@@ -3314,7 +3314,7 @@ static int exfat_fill_inode(struct inode *inode, struct file_id_t *fid)
 	inode->i_uid = sbi->options.fs_uid;
 	inode->i_gid = sbi->options.fs_gid;
 	INC_IVERSION(inode);
-	inode->i_generation = get_seconds();
+	inode->i_generation = prandom_u32();
 
 	if (info.Attr & ATTR_SUBDIR) { /* directory */
 		inode->i_generation &= ~1;
