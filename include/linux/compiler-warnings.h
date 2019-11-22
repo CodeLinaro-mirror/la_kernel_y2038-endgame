6a5218299444e88b7d071fb58213d2d8f87ffa23
Arnd Bergmann <arnd@arndb.de>
2019-11-22 21:25:40 +0100

    add compiler-warnings infrastructure
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

---
+++ include/linux/compiler-warnings.h
@@ -0,0 +1,131 @@
+#ifndef __LINUX_COMPILER_WARNINGS_H
+#define __LINUX_COMPILER_WARNINGS_H
+
+/* building blocks: support for "make W=0123" and "E=0123" */
+#ifdef KBUILD_ERR0
+#define KBUILD_WARN_LEVEL0 error
+#elif defined(KBUILD_WARN0)
+#define KBUILD_WARN_LEVEL0 warning
+#else
+#define KBUILD_WARN_LEVEL0 ignored
+#endif
+
+#ifdef KBUILD_ERR1
+#define KBUILD_WARN_LEVEL1 error
+#elif defined(KBUILD_WARN1)
+#define KBUILD_WARN_LEVEL1 warning
+#else
+#define KBUILD_WARN_LEVEL1 ignored
+#endif
+
+#ifdef KBUILD_ERR2
+#define KBUILD_WARN_LEVEL2 error
+#elif defined(KBUILD_WARN2)
+#define KBUILD_WARN_LEVEL2 warning
+#else
+#define KBUILD_WARN_LEVEL2 ignored
+#endif
+
+#ifdef KBUILD_ERR3
+#define KBUILD_WARN_LEVEL3 error
+#elif defined(KBUILD_WARN3)
+#define KBUILD_WARN_LEVEL3 warning
+#else
+#define KBUILD_WARN_LEVEL3 ignored
+#endif
+
+_Pragma("GCC diagnostic warning \"-Wall\"");
+
+/* building blocks: support for compiler versions */
+#define __KBUILD_WARN_LEVEL(arg) _Pragma(#arg)
+
+#define KBUILD_WARN_LEVEL(level, warning)  \
+	__KBUILD_WARN_LEVEL(GCC diagnostic level warning)
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 40600
+#define KBUILD_WARN_LEVEL_GCC_4_6(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_4_6(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 40700
+#define KBUILD_WARN_LEVEL_GCC_4_7(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_4_7(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 40800
+#define KBUILD_WARN_LEVEL_GCC_4_8(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_4_8(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 40900
+#define KBUILD_WARN_LEVEL_GCC_4_9(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_4_9(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 50000
+#define KBUILD_WARN_LEVEL_GCC_5(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_5(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 60000
+#define KBUILD_WARN_LEVEL_GCC_6(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_6(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 70000
+#define KBUILD_WARN_LEVEL_GCC_7(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_7(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 80000
+#define KBUILD_WARN_LEVEL_GCC_8(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_8(level, warning)
+#endif
+
+#if defined(GCC_VERSION) && GCC_VERSION >= 90000
+#define KBUILD_WARN_LEVEL_GCC_9(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_GCC_9(level, warning)
+#endif
+
+#if defined(CLANG_VERSION) && CLANG_VERSION >= 80000
+#define KBUILD_WARN_LEVEL_CLANG_8(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_CLANG_8(level, warning)
+#endif
+
+#if defined(CLANG_VERSION) && CLANG_VERSION >= 90000
+#define KBUILD_WARN_LEVEL_CLANG_9(level, warning) \
+	KBUILD_WARN_LEVEL(level, warning)
+#else
+#define KBUILD_WARN_LEVEL_CLANG_9(level, warning)
+#endif
+
+#define KBUILD_WARN(level, ver, warning) \
+	KBUILD_WARN_LEVEL_ ## ver(KBUILD_WARN_LEVEL ## level, warning)
+
+KBUILD_WARN(0, GCC_4_6, "-Wall")
+KBUILD_WARN(0, CLANG_8, "-Wall")
+KBUILD_WARN(0, GCC_4_6, "-Wattributes")
+
+#include <linux/warnings.h>
+
+#endif /* __LINUX_COMPILER_WARNINGS_H */
--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -234,4 +234,6 @@ struct ftrace_likely_data {
 #define __diag_error(compiler, version, option, comment) \
 	__diag_ ## compiler(version, error, option)
 
+#include <linux/compiler-warnings.h>
+
 #endif /* __LINUX_COMPILER_TYPES_H */
