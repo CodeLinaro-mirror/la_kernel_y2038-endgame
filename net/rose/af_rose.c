144716b7e4a7413994279b17b6d8d3407b9c93a5
Arnd Bergmann <arnd@arndb.de>
2019-12-01 10:45:01 +0100

    drivers: avoid -Wtrigraph warning
    There are few trigraph warnings in the kernel, so just address that to
    be able to stop passing -Wno-trigraphs on the command line.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- net/rose/af_rose.c
+++ net/rose/af_rose.c
@@ -1401,7 +1401,7 @@ static int rose_info_show(struct seq_file *seq, void *v)
 		const struct net_device *dev = rose->device;
 
 		if (!dev)
-			devname = "???";
+			devname = "\?\?\?";
 		else
 			devname = dev->name;
 
@@ -1410,7 +1410,7 @@ static int rose_info_show(struct seq_file *seq, void *v)
 			   ax2asc(buf, &rose->dest_call));
 
 		if (ax25cmp(&rose->source_call, &null_ax25_address) == 0)
-			callsign = "??????-?";
+			callsign = "\?\?\?\?\?\?-?";
 		else
 			callsign = ax2asc(buf, &rose->source_call);
 
