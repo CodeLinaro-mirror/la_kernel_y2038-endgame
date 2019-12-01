e43b98e5f2aa499fd3c9cda762bb23da04e50d92
Arnd Bergmann <arnd@arndb.de>
2019-12-01 10:45:01 +0100

    drivers: avoid -Wtrigraph warning
    There are few trigraph warnings in the kernel, so just address that to
    be able to stop passing -Wno-trigraphs on the command line.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/mtd/maps/sun_uflash.c
+++ drivers/mtd/maps/sun_uflash.c
@@ -42,7 +42,7 @@ struct uflash_dev {
 };
 
 struct map_info uflash_map_templ = {
-	.name =		"SUNW,???-????",
+	.name =		"SUNW,??\?-????",
 	.size =		UFLASH_WINDOW_SIZE,
 	.bankwidth =	UFLASH_BUSWIDTH,
 };
