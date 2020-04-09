fcc67e1f32baba037b4d81b573f3fcbdda3718bd
Arnd Bergmann <arnd@arndb.de>
2020-04-09 13:41:58 +0200

    mm: hugetlb: fix build CMD failure
    for_each_mem_pfn_range() is defined conditionally, causing a
    build failure when CONFIG_HAVE_MEMBLOCK_NODE_MAP is disabled:
    
    mm/hugetlb.c: In function 'hugetlb_cma_reserve':
    mm/hugetlb.c:5580:3: error: implicit declaration of
    function 'for_each_mem_pfn_range'; did you mean 'for_each_mem_range'?
    [-Werror=implicit-function-declaration]
     5580 |   for_each_mem_pfn_range(i, nid, &start_pfn, &end_pfn, NULL) {
          |   ^~~~~~~~~~~~~~~~~~~~~~
          |   for_each_mem_range
    mm/hugetlb.c:5580:61: error: expected ';' before '{' token
     5580 |   for_each_mem_pfn_range(i, nid, &start_pfn, &end_pfn, NULL) {
          |                                                             ^~
          |                                                             ;
    
    Make the code that relies on this also conditional on the same
    symbol.
    
    Fixes: 7cfe2df30b35 ("mm: hugetlb: optionally allocate gigantic hugepages using cma")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5596,7 +5596,8 @@ void move_hugetlb_state(struct page *oldpage, struct page *newpage, int reason)
 	}
 }
 
-#ifdef CONFIG_CMA
+#if defined(CONFIG_CMA) && defined(CONFIG_HAVE_MEMBLOCK_NODE_MAP)
+static unsigned long hugetlb_cma_size __initdata;
 static bool cma_reserve_called __initdata;
 
 static int __init cmdline_parse_hugetlb_cma(char *p)
