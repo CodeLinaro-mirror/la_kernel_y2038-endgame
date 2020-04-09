4357dfbc5d45e378664915519189539cc295e249
Arnd Bergmann <arnd@arndb.de>
2020-04-09 21:08:23 +0200

    fixup! [NEEDS MORE TESTING] drm/mediatek: fix dependencies

--- include/linux/soc/mediatek/mtk-mmsys.h
+++ include/linux/soc/mediatek/mtk-mmsys.h
@@ -9,6 +9,7 @@
 enum mtk_ddp_comp_id;
 struct device;
 
+#if IS_ENABLED(CONFIG_MTK_MMSYS)
 void mtk_mmsys_ddp_connect(struct device *dev,
 			   enum mtk_ddp_comp_id cur,
 			   enum mtk_ddp_comp_id next);
@@ -16,5 +17,18 @@ void mtk_mmsys_ddp_connect(struct device *dev,
 void mtk_mmsys_ddp_disconnect(struct device *dev,
 			      enum mtk_ddp_comp_id cur,
 			      enum mtk_ddp_comp_id next);
+#else
+static inline void mtk_mmsys_ddp_connect(struct device *dev,
+					 enum mtk_ddp_comp_id cur,
+					 enum mtk_ddp_comp_id next)
+{
+}
+
+static inline void mtk_mmsys_ddp_disconnect(struct device *dev,
+					    enum mtk_ddp_comp_id cur,
+					    enum mtk_ddp_comp_id next)
+{
+}
+#endif
 
 #endif /* __MTK_MMSYS_H */
