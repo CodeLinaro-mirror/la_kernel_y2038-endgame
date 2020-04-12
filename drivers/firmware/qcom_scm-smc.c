143188db54fc2474cabe2a2db85e33434018ef14
Arnd Bergmann <arnd@arndb.de>
2020-04-12 16:03:44 +0200

    [SUBMITTED 20200429] firmware: qcom: avoid struct initialization warning on gcc-4.8
    When building with old compilers like gcc-4.8, a struct initializer
    can warn about invalid syntax:
    
    drivers/firmware/qcom_scm-smc.c: In function 'scm_smc_call':
    drivers/firmware/qcom_scm-smc.c:94:9: error: missing braces around initializer [-Werror=missing-braces]
      struct arm_smccc_args smc = {0};
             ^
    drivers/firmware/qcom_scm-smc.c:94:9: error: (near initialization for 'smc.args') [-Werror=missing-braces]
    cc1: some warnings being treated as errors
    drivers/firmware/qcom_scm-legacy.c: In function 'scm_legacy_call':
    drivers/firmware/qcom_scm-legacy.c:139:9: error: missing braces around initializer [-Werror=missing-braces]
      struct arm_smccc_args smc = {0};
             ^
    drivers/firmware/qcom_scm-legacy.c:139:9: error: (near initialization for 'smc.args') [-Werror=missing-braces]
    
    Change this to use the empty initializer extension that
    works with all compilers.
    
    Fixes: 590e92809a58 ("firmware: qcom_scm-32: Move SMCCC register filling to qcom_scm_call")
    Fixes: 3f951ea627da ("firmware: qcom_scm-64: Move SMC register filling to qcom_scm_call_smccc")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/firmware/qcom_scm-smc.c
+++ drivers/firmware/qcom_scm-smc.c
@@ -91,7 +91,7 @@ int scm_smc_call(struct device *dev, const struct qcom_scm_desc *desc,
 			(qcom_scm_convention == SMC_CONVENTION_ARM_32) ?
 			ARM_SMCCC_SMC_32 : ARM_SMCCC_SMC_64;
 	struct arm_smccc_res smc_res;
-	struct arm_smccc_args smc = {0};
+	struct arm_smccc_args smc = { };
 
 	smc.args[0] = ARM_SMCCC_CALL_VAL(
 		smccc_call_type,
