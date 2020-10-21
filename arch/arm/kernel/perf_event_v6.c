77bb562581e31511b5080da0344203651817d488
Arnd Bergmann <arnd@arndb.de>
2020-10-21 21:41:20 +0200

    ARM: perf: hide initializer override warnings
    There are many warnings in this file like
    
    arch/arm/kernel/perf_event_v7.c:532:42: warning: initializer overrides prior initialization of this subobject [-Winitializer-overrides]
    
    Disable them locally to allow enabling the warning everywhere
    in the kernel.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/kernel/perf_event_v6.c
+++ arch/arm/kernel/perf_event_v6.c
@@ -68,6 +68,10 @@ enum armv6_counters {
 	ARMV6_COUNTER1,
 };
 
+__diag_push()
+__diag_ignore(GCC, 5, "-Woverride-init", "for PERF_MAP_ALL_UNSUPPORTED")
+__diag_ignore(clang, 9, "-Winitializer-overrides", "for PERF_MAP_ALL_UNSUPPORTED")
+
 /*
  * The hardware events that we support. We do support cache operations but
  * we have harvard caches and no way to combine instruction and data
@@ -175,6 +179,8 @@ static const unsigned armv6mpcore_perf_cache_map[PERF_COUNT_HW_CACHE_MAX]
 	[C(ITLB)][C(OP_READ)][C(RESULT_MISS)]	= ARMV6MPCORE_PERFCTR_ITLB_MISS,
 	[C(ITLB)][C(OP_WRITE)][C(RESULT_MISS)]	= ARMV6MPCORE_PERFCTR_ITLB_MISS,
 };
+
+__diag_pop()
 
 static inline unsigned long
 armv6_pmcr_read(void)
