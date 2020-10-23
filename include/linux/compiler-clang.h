c3283b935073c959a4e559956050f03eace95e97
Arnd Bergmann <arnd@arndb.de>
2020-10-23 17:07:38 +0200

    compiler.h: add more __diag() variants
    The __diag() macros currently only allow controlling diagnostics
    that first showed up with gcc-8, which is a bit pointless.
    In order to turn specific warnings off on other compilers,
    add the corresponding macros for all supported versions of
    GCC and clang.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -114,11 +114,41 @@
 #define __diag_str(s)		__diag_str1(s)
 #define __diag(s)		_Pragma(__diag_str(clang diagnostic s))
 
+#if CONFIG_CLANG_VERSION >= 90000
+#define __diag_clang_9(s)	__diag(s)
+#else
+#define __diag_clang_9(s)
+#endif
+
+#if CONFIG_CLANG_VERSION >= 100000
+#define __diag_clang_10(s)	__diag(s)
+#else
+#define __diag_clang_10(s)
+#endif
+
 #if CONFIG_CLANG_VERSION >= 110000
 #define __diag_clang_11(s)	__diag(s)
 #else
 #define __diag_clang_11(s)
 #endif
 
+#if CONFIG_CLANG_VERSION >= 120000
+#define __diag_clang_12(s)	__diag(s)
+#else
+#define __diag_clang_12(s)
+#endif
+
+#if CONFIG_CLANG_VERSION >= 130000
+#define __diag_clang_13(s)	__diag(s)
+#else
+#define __diag_clang_13(s)
+#endif
+
+#if CONFIG_CLANG_VERSION >= 140000
+#define __diag_clang_13(s)	__diag(s)
+#else
+#define __diag_clang_13(s)
+#endif
+
 #define __diag_ignore_all(option, comment) \
-	__diag_clang(11, ignore, option)
+	__diag_clang(9, ignore, option)
