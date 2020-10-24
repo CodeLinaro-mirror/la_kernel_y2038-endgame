a4e03bda5b77e393b302a19f1330499149bf09a0
Arnd Bergmann <arnd@arndb.de>
2020-10-24 16:23:39 +0200

    [SUBMITTED 20201026] arm64: traps: fix -Woverride-init warnings
    There are many warnings in this file when we re-enable the
    Woverride-init flag:
    
    arch/arm64/kernel/traps.c:704:26: warning: initialized field overwritten [-Woverride-init]
      704 |  [ESR_ELx_EC_UNKNOWN]  = "Unknown/Uncategorized",
          |                          ^~~~~~~~~~~~~~~~~~~~~~~
    arch/arm64/kernel/traps.c:704:26: note: (near initialization for 'esr_class_str[0]')
    arch/arm64/kernel/traps.c:705:22: warning: initialized field overwritten [-Woverride-init]
      705 |  [ESR_ELx_EC_WFx]  = "WFI/WFE",
          |                      ^~~~~~~~~
    
    This is harmless since they are only informational strings,
    but it's easy to change the code to ignore missing initialization
    and instead warn about possible duplicate initializers.
    
    Fixes: 60a1f02c9e91 ("arm64: decode ESR_ELx.EC when reporting exceptions")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -804,7 +804,6 @@ void do_el0_sys(unsigned long esr, struct pt_regs *regs)
 }
 
 static const char *esr_class_str[] = {
-	[0 ... ESR_ELx_EC_MAX]		= "UNRECOGNIZED EC",
 	[ESR_ELx_EC_UNKNOWN]		= "Unknown/Uncategorized",
 	[ESR_ELx_EC_WFx]		= "WFI/WFE",
 	[ESR_ELx_EC_CP15_32]		= "CP15 MCR/MRC",
@@ -852,7 +851,7 @@ static const char *esr_class_str[] = {
 
 const char *esr_get_class_string(unsigned long esr)
 {
-	return esr_class_str[ESR_ELx_EC(esr)];
+	return esr_class_str[ESR_ELx_EC(esr)] ?: "UNRECOGNIZED EC";
 }
 
 /*
