c270aebe233822973691b87e864754ad266ee986
Arnd Bergmann <arnd@arndb.de>
2020-11-03 00:00:35 +0100

    wireless: remove old ioctls
    Commit 8bfb36766064 ("wireless: wext: remove ndo_do_ioctl fallback")
    removed the ability to have private wireless ioctl commands implemented
    in the normal ndo_do_ioctl handler, under the assumption that no remaining
    drivers used that.
    
    This turned out to be incorrect, as both the atmel and intersil drivers
    do in fact implement a few such commands, along with SIOCDEVPRIVATE
    commands and infrastructure that would allow handling them using the
    more modern abstraction.
    
    Since nobody noticed that these interfaces have been broken for over 3.5
    years and the drivers are still usable without them (provided there are
    still users), it seems better to just remove the now dead code rather
    than trying to make it work again.  An alternative would be to just
    remove the drivers entirely.
    
    There are also a few staging drivers that got merged with the same
    type of interface, which was either broken by the same commit, or
    it never worked for the drivers that got merged later.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/staging/rtl8188eu/include/osdep_intf.h
+++ drivers/staging/rtl8188eu/include/osdep_intf.h
@@ -21,7 +21,6 @@ u8 rtw_reset_drv_sw(struct adapter *padapter);
 void rtw_stop_drv_threads(struct adapter *padapter);
 void rtw_cancel_all_timer(struct adapter *padapter);
 
-int rtw_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
 int rtw_android_priv_cmd(struct net_device *dev, struct ifreq *rq,
 			 void __user *data, int cmd);
 
--- drivers/staging/rtl8723bs/os_dep/os_intfs.c
+++ drivers/staging/rtl8723bs/os_dep/os_intfs.c
@@ -416,30 +416,13 @@ u16 rtw_recv_select_queue(struct sk_buff *skb)
 	return rtw_1d_to_queue[priority];
 }
 
-static int rtw_ndev_notifier_call(struct notifier_block *nb, unsigned long state, void *ptr)
-{
-	struct net_device *dev = netdev_notifier_info_to_dev(ptr);
-
-	if (dev->netdev_ops->ndo_do_ioctl != rtw_ioctl)
-		return NOTIFY_DONE;
-
-	DBG_871X_LEVEL(_drv_info_, FUNC_NDEV_FMT " state:%lu\n", FUNC_NDEV_ARG(dev), state);
-
-	return NOTIFY_DONE;
-}
-
-static struct notifier_block rtw_ndev_notifier = {
-	.notifier_call = rtw_ndev_notifier_call,
-};
-
 int rtw_ndev_notifier_register(void)
 {
-	return register_netdevice_notifier(&rtw_ndev_notifier);
+	return 0;
 }
 
 void rtw_ndev_notifier_unregister(void)
 {
-	unregister_netdevice_notifier(&rtw_ndev_notifier);
 }
 
 
@@ -469,7 +452,6 @@ static const struct net_device_ops rtw_netdev_ops = {
 	.ndo_select_queue	= rtw_select_queue,
 	.ndo_set_mac_address = rtw_net_set_mac_address,
 	.ndo_get_stats = rtw_net_get_stats,
-	.ndo_do_ioctl = rtw_ioctl,
 	.ndo_siocdevprivate = rtw_siocdevprivate,
 };
 
