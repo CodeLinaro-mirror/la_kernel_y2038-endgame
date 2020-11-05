de3a24a9ffc187c9bb604d126ddee8a85958b160
Arnd Bergmann <arnd@arndb.de>
2020-11-05 16:42:53 +0100

    qeth: use ndo_siocdevprivate
    qeth has both standard MII ioctls and custom SIOCDEVPRIVATE ones,
    all of which work correctly with compat user space.
    
    Move the private ones over to the new ndo_siocdevprivate callback.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/s390/net/qeth_core.h
+++ drivers/s390/net/qeth_core.h
@@ -1129,6 +1129,8 @@ int qeth_do_send_packet(struct qeth_card *card, struct qeth_qdio_out_q *queue,
 			unsigned int offset, unsigned int hd_len,
 			int elements_needed);
 int qeth_do_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
+int qeth_siocdevprivate(struct net_device *dev, struct ifreq *rq,
+			void __user *data, int cmd);
 void qeth_dbf_longtext(debug_info_t *id, int level, char *text, ...);
 int qeth_configure_cq(struct qeth_card *, enum qeth_cq);
 int qeth_hw_trap(struct qeth_card *, enum qeth_diags_trap_action);
