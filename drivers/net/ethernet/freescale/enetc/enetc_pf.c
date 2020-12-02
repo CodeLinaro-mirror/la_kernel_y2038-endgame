6e8b3228bdeb07ab7cdb8aca37057f53bb5afd33
Arnd Bergmann <arnd@arndb.de>
2020-12-02 17:11:20 +0100

    [SUBMITTED 20201203] enetc: fix build warning
    When CONFIG_OF is disabled, there is a harmless warning about
    an unused variable:
    
    enetc_pf.c: In function 'enetc_phylink_create':
    enetc_pf.c:981:17: error: unused variable 'dev' [-Werror=unused-variable]
    
    Slightly rearrange the code to pass around the of_node as a
    function argument, which avoids the problem without hurting
    readability.
    
    Fixes: 71b77a7a27a3 ("enetc: Migrate to PHYLINK and PCS_LYNX")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/net/ethernet/freescale/enetc/enetc_pf.c
+++ drivers/net/ethernet/freescale/enetc/enetc_pf.c
@@ -1205,6 +1205,7 @@ static int enetc_pf_probe(struct pci_dev *pdev,
 	struct net_device *ndev;
 	struct enetc_si *si;
 	struct enetc_pf *pf;
+	struct device_node *node = pdev->dev.of_node;
 	int err;
 
 	err = enetc_pf_register_with_ierb(pdev);
