4318e7788ce56ad848b96b9d12d0caed0e61b2a0
Arnd Bergmann <arnd@arndb.de>
2020-12-18 19:20:33 +0100

    vmd: simplify PCI probing
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -828,11 +828,15 @@ static struct irq_domain *pci_host_bridge_msi_domain(struct pci_bus *bus)
 {
 	struct irq_domain *d;
 
+	/* Default set up by host bridge driver */
+	d = dev_get_msi_domain(bus->bridge);
+
 	/*
 	 * Any firmware interface that can resolve the msi_domain
 	 * should be called from here.
 	 */
-	d = pci_host_bridge_of_msi_domain(bus);
+	if (!d)
+		d = pci_host_bridge_of_msi_domain(bus);
 	if (!d)
 		d = pci_host_bridge_acpi_msi_domain(bus);
 
