030fe95073f2929f65648396f19a791b133390ab
Arnd Bergmann <arnd@arndb.de>
2020-12-24 09:30:27 +0100

    ARM: orion5x: simplify PCI init
    The only real difference in the PCI setup between orion5x boards
    is the map_irq() callback, so move all the instances into one
    the common file and only pass that one callback pointer.
    
    The boards that have a preinit() callback can just call that
    directly.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-orion/ts409-setup.c
+++ arch/arm/mach-orion/ts409-setup.c
@@ -134,17 +134,10 @@ static int __init qnap_ts409_pci_map_irq(const struct pci_dev *dev, u8 slot,
 	return -1;
 }
 
-static struct hw_pci qnap_ts409_pci __initdata = {
-	.nr_controllers	= 2,
-	.setup		= orion5x_pci_sys_setup,
-	.scan		= orion5x_pci_sys_scan_bus,
-	.map_irq	= qnap_ts409_pci_map_irq,
-};
-
 static int __init qnap_ts409_pci_init(void)
 {
 	if (machine_is_ts409())
-		pci_common_init(&qnap_ts409_pci);
+		orion5x_pci_init(qnap_ts409_pci_map_irq);
 
 	return 0;
 }
