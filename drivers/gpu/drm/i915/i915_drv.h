932fbd7e2bafdd2cc68d1157dfba4814faa77b5e
Arnd Bergmann <arnd@arndb.de>
2020-12-31 11:13:02 +0100

    Revert "[SUBMITTED 20201230] i915: fix shift warning"
    This reverts commit 0973690e13e30d99ecbd4ab045ba42b93527bfa0.

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1545,7 +1545,7 @@ IS_SUBPLATFORM(const struct drm_i915_private *i915,
 	unsigned int first__ = (first);					\
 	unsigned int count__ = (count);					\
 	((gt)->info.engine_mask &						\
-	 GENMASK_ULL(first__ + count__ - 1, first__)) >> first__;		\
+	 GENMASK(first__ + count__ - 1, first__)) >> first__;		\
 })
 #define VDBOX_MASK(gt) \
 	ENGINE_INSTANCES_MASK(gt, VCS0, I915_MAX_VCS)
