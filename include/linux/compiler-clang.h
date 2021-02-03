c699bef605ad5a61a315ede9b4b048ed61007804
Arnd Bergmann <arnd@arndb.de>
2021-02-03 22:48:46 +0100

    amdgpu: disable one frame size warning
    drivers/gpu/drm/amd/amdgpu/../display/dc/dml/dcn31/display_mode_vba_31.c:3956:6: error: stack frame size (2104) exceeds limit (2048) in function 'dml31_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
    void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -68,3 +68,19 @@
 
 #define __nocfi		__attribute__((__no_sanitize__("cfi")))
 #define __cficanonical	__attribute__((__cfi_canonical_jump_table__))
+
+/*
+ * Turn individual warnings and errors on and off locally, depending
+ * on version.
+ */
+#define __diag_CLANG(version, severity, s) \
+	__diag(__diag_CLANG_ ## severity s)
+
+#define __diag_str1(s)		#s
+#define __diag_str(s)		__diag_str1(s)
+#define __diag(s)		_Pragma(__diag_str(GCC diagnostic s))
+
+/* Severity used in pragma directives */
+#define __diag_CLANG_ignore	ignored
+#define __diag_CLANG_warn	warning
+#define __diag_CLANG_error	error
