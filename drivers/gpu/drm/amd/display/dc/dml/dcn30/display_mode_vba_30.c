a01f4df1bd2f2e70054b40b10994bf20bae7abef
Arnd Bergmann <arnd@arndb.de>
2021-02-03 22:48:46 +0100

    amdgpu: disable one frame size warning
    drivers/gpu/drm/amd/amdgpu/../display/dc/dml/dcn31/display_mode_vba_31.c:3956:6: error: stack frame size (2104) exceeds limit (2048) in function 'dml31_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
    void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/dml/dcn30/display_mode_vba_30.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn30/display_mode_vba_30.c
@@ -3641,6 +3641,8 @@ static double TruncToValidBPP(
 	return BPP_INVALID;
 }
 
+__diag_push()
+__diag_ignore(CLANG, 10, "-Wframe-larger-than=", "this exceeds 2048 bytes")
 void dml30_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
 {
 	struct vba_vars_st *v = &mode_lib->vba;
@@ -5344,6 +5346,7 @@ void dml30_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_l
 		v->maxMpcComb = MaximumMPCCombine;
 	}
 }
+__diag_pop()
 
 static void CalculateWatermarksAndDRAMSpeedChangeSupport(
 		struct display_mode_lib *mode_lib,
--- drivers/gpu/drm/amd/display/dc/dml/dcn31/display_mode_vba_31.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn31/display_mode_vba_31.c
@@ -3953,6 +3953,8 @@ static double TruncToValidBPP(
 	return BPP_INVALID;
 }
 
+__diag_push()
+__diag_ignore(CLANG, 10, "-Wframe-larger-than=", "this exceeds 2048 bytes")
 void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
 {
 	struct vba_vars_st *v = &mode_lib->vba;
@@ -5739,6 +5741,7 @@ void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_l
 		v->maxMpcComb = MaximumMPCCombine;
 	}
 }
+__diag_pop()
 
 static void CalculateWatermarksAndDRAMSpeedChangeSupport(
 		struct display_mode_lib *mode_lib,
