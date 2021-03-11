73d1662e3a18d8105b4d61b26aa029a93d8d5a5a
Arnd Bergmann <arnd@arndb.de>
2021-03-11 18:10:53 +0100

    amdgpu: address -Woverride-init/-Winitializer-override warnings better
    drivers/gpu/drm/amd/amdgpu/../display/dc/dce120/dce120_resource.c:277:15: error: initialized field overwritten [-Werror=override-init]
      277 |  .TMDS_CNTL = 0,\
          |               ^
    drivers/gpu/drm/amd/amdgpu/../display/dc/dce120/dce120_resource.c:281:2: note: in expansion of macro 'stream_enc_regs'
      281 |  stream_enc_regs(0),
          |  ^~~~~~~~~~~~~~~
    drivers/gpu/drm/amd/amdgpu/../display/dc/dce120/dce120_resource.c:277:15: note: (near initialization for 'stream_enc_regs[0].TMDS_CNTL')
      277 |  .TMDS_CNTL = 0,\
          |               ^
    drivers/gpu/drm/amd/amdgpu/../display/dc/dce120/dce120_resource.c:281:2: note: in expansion of macro 'stream_enc_regs'
      281 |  stream_enc_regs(0),
          |  ^~~~~~~~~~~~~~~
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/dce/dce_stream_encoder.h
+++ drivers/gpu/drm/amd/display/dc/dce/dce_stream_encoder.h
@@ -28,6 +28,9 @@
 
 #include "stream_encoder.h"
 
+__diag_ignore(GCC, 8, "-Woverride-init", "for PERF_MAP_ALL_UNSUPPORTED")
+__diag_ignore(CLANG, 9, "-Winitializer-overrides", "for PERF_MAP_ALL_UNSUPPORTED")
+
 #define DCE110STRENC_FROM_STRENC(stream_encoder)\
 	container_of(stream_encoder, struct dce110_stream_encoder, base)
 
