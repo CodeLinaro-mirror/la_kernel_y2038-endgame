eb6da81daa22b9ea02ed46d1eab1406c3b87c37b
Arnd Bergmann <arnd@arndb.de>
2021-03-12 17:54:56 +0100

    [SUBMITTED 20210323] net: skbuff: fix stack variable out of bounds access
    gcc-11 warns that the TS_SKB_CB(&state)) cast in skb_find_text()
    leads to an out-of-bounds access in skb_prepare_seq_read() after
    the addition of a new struct member made skb_seq_state longer
    than ts_state:
    
    net/core/skbuff.c: In function ‘skb_find_text’:
    net/core/skbuff.c:3498:26: error: array subscript ‘struct skb_seq_state[0]’ is partly outside array bounds of ‘struct ts_state[1]’ [-Werror=array-bounds]
     3498 |         st->lower_offset = from;
          |         ~~~~~~~~~~~~~~~~~^~~~~~
    net/core/skbuff.c:3659:25: note: while referencing ‘state’
     3659 |         struct ts_state state;
          |                         ^~~~~
    
    The warning is currently disabled globally, but I found this
    instance during experimental build testing, and it seems
    legitimate.
    
    Make the textsearch buffer longer and add a compile-time check to
    ensure the two remain the same length.
    
    Fixes: 97550f6fa592 ("net: compound page support in skb_seq_read")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3697,6 +3697,7 @@ static unsigned int skb_ts_get_next_block(unsigned int offset, const u8 **text,
 					  struct ts_config *conf,
 					  struct ts_state *state)
 {
+	BUILD_BUG_ON(sizeof(struct skb_seq_state) > sizeof(state->cb));
 	return skb_seq_read(offset, text, TS_SKB_CB(state));
 }
 
