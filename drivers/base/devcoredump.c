5fd97a48162ca76670aa5e2ee6e0bc6fa95035c4
Arnd Bergmann <arnd@arndb.de>
2021-03-18 18:00:36 +0100

    fixup devcoredump
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/base/devcoredump.c
+++ drivers/base/devcoredump.c
@@ -296,7 +296,7 @@ void dev_coredumpm(struct device *dev, struct module *owner,
 				       "failing_device"));
 
 	WARN_ON_ONCE(sysfs_create_link(&dev->kobj, &devcd->devcd_dev.kobj,
-				       "devcoredump"))
+				       "devcoredump"));
 
 	INIT_DELAYED_WORK(&devcd->del_wk, devcd_del);
 	schedule_delayed_work(&devcd->del_wk, DEVCD_TIMEOUT);
