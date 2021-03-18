109d5eed2655c8593baedd28c6177cc2be536b3b
Arnd Bergmann <arnd@arndb.de>
2021-03-18 14:03:25 +0100

    [SUBMITTED 20210322] cgroup: fix -Wzero-length-bounds warnings
    When cgroups are enabled, but every single subsystem is turned off,
    CGROUP_SUBSYS_COUNT is zero, and the cgrp->subsys[] array has no
    members.
    
    gcc-11 points out that this leads to an invalid access in any function
    that might access this array:
    
    kernel/cgroup/cgroup.c: In function 'cgroup_addrm_files':
    kernel/cgroup/cgroup.c:460:58: warning: array subscript '<unknown>' is outside the bounds of an interior zero-length array 'struct cgroup_subsys_state *[0]' [-Wzero-length-bounds]
    kernel/cgroup/cgroup.c:460:24: note: in expansion of macro 'rcu_dereference_check'
      460 |                 return rcu_dereference_check(cgrp->subsys[ss->id],
          |                        ^~~~~~~~~~~~~~~~~~~~~
    In file included from include/linux/cgroup.h:28,
                     from kernel/cgroup/cgroup-internal.h:5,
                     from kernel/cgroup/cgroup.c:31:
    include/linux/cgroup-defs.h:422:43: note: while referencing 'subsys'
      422 |         struct cgroup_subsys_state __rcu *subsys[CGROUP_SUBSYS_COUNT];
    
    I'm not sure what is expected to happen for such a configuration,
    presumably these functions are never calls in that case. Adding a
    sanity check in each function we get the warning for manages to shut
    up the warnings and do nothing instead.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    I'm grouping this together with the -Wstringop-overread warnings,
    since the underlying logic in gcc seems to be the same.

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -472,7 +472,7 @@ static u16 cgroup_ss_mask(struct cgroup *cgrp)
 static struct cgroup_subsys_state *cgroup_css(struct cgroup *cgrp,
 					      struct cgroup_subsys *ss)
 {
-	if (ss)
+	if (ss && (CGROUP_SUBSYS_COUNT > 0))
 		return rcu_dereference_check(cgrp->subsys[ss->id],
 					lockdep_is_held(&cgroup_mutex));
 	else
@@ -550,6 +550,9 @@ struct cgroup_subsys_state *cgroup_e_css(struct cgroup *cgrp,
 {
 	struct cgroup_subsys_state *css;
 
+	if (CGROUP_SUBSYS_COUNT == 0)
+		return NULL;
+
 	do {
 		css = cgroup_css(cgrp, ss);
 
@@ -576,6 +579,9 @@ struct cgroup_subsys_state *cgroup_get_e_css(struct cgroup *cgrp,
 					     struct cgroup_subsys *ss)
 {
 	struct cgroup_subsys_state *css;
+
+	if (CGROUP_SUBSYS_COUNT == 0)
+		return NULL;
 
 	rcu_read_lock();
 
@@ -647,7 +653,7 @@ struct cgroup_subsys_state *of_css(struct kernfs_open_file *of)
 	 * the matching css from the cgroup's subsys table is guaranteed to
 	 * be and stay valid until the enclosing operation is complete.
 	 */
-	if (cft->ss)
+	if (cft->ss && CGROUP_SUBSYS_COUNT > 0)
 		return rcu_dereference_raw(cgrp->subsys[cft->ss->id]);
 	else
 		return &cgrp->self;
@@ -2372,6 +2378,9 @@ struct task_struct *cgroup_taskset_next(struct cgroup_taskset *tset,
 {
 	struct css_set *cset = tset->cur_cset;
 	struct task_struct *task = tset->cur_task;
+
+	if (CGROUP_SUBSYS_COUNT == 0)
+		return NULL;
 
 	while (&cset->mg_node != tset->csets) {
 		if (!task)
@@ -4644,7 +4653,7 @@ void css_task_iter_start(struct cgroup_subsys_state *css, unsigned int flags,
 	it->ss = css->ss;
 	it->flags = flags;
 
-	if (it->ss)
+	if (it->ss && CGROUP_SUBSYS_COUNT > 0)
 		it->cset_pos = &css->cgroup->e_csets[css->ss->id];
 	else
 		it->cset_pos = &css->cgroup->cset_links;
