b7b834566c3a8ccc22f3f06a778f14412ac2941a
Arnd Bergmann <arnd@arndb.de>
2021-03-18 18:18:43 +0100

    media: flexcop: avoid -Wempty-body warning
    drivers/media/common/b2c2/flexcop-misc.c: In function 'flexcop_determine_revision':
    drivers/media/common/b2c2/flexcop-misc.c:35:85: error: suggest braces around empty body in an 'else' statement [-Werror=empty-body]
       35 |                 deb_info("this FlexCop has the 6 basic main hardware pid filter.\n");
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/media/common/b2c2/flexcop.h
+++ drivers/media/common/b2c2/flexcop.h
@@ -17,7 +17,8 @@ extern int b2c2_flexcop_debug;
 #define dprintk(level,args...) \
 	do { if ((b2c2_flexcop_debug & level)) printk(args); } while (0)
 #else
-#define dprintk(level,args...)
+#define dprintk(level,args...) \
+	do { } while (0)
 #endif
 
 #define deb_info(args...) dprintk(0x01, args)
