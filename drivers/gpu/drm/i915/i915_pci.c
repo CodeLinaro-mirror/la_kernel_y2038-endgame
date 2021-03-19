a46becded9299038dcbbcc3b6a34363cd1c72587
Arnd Bergmann <arnd@arndb.de>
2021-03-19 11:29:41 +0100

    global: -Woverride-init warnings
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -35,6 +35,10 @@
 #include "i915_globals.h"
 #include "i915_selftest.h"
 
+
+__diag_ignore(GCC, 8, "-Woverride-init", "features all override one another")
+__diag_ignore(CLANG, 9, "-Winitializer-overrides", "features all override one another")
+
 #define PLATFORM(x) .platform = (x)
 #define GEN(x) \
 	.graphics_ver = (x), \
