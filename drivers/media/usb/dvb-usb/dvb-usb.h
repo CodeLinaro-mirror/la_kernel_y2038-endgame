9ff7f45cd875a9b3e80a78e8dd112e759ca6ec14
Arnd Bergmann <arnd@arndb.de>
2021-03-19 11:19:08 +0100

    media: dvb-usb: avoid -Wempty-body warnings
    drivers/media/usb/dvb-usb/vp702x-fe.c: In function 'vp702x_fe_set_frontend':
    drivers/media/usb/dvb-usb/vp702x-fe.c:190:46: error: suggest braces around empty body in an 'else' statement [-Werror=empty-body]
      190 |                 deb_fe("tuning succeeded.\n");
    drivers/media/usb/dvb-usb/dtt200u.c: In function 'dtt200u_rc_query':
    drivers/media/usb/dvb-usb/dtt200u.c:124:58: error: suggest braces around empty body in an 'if' statement [-Werror=empty-body]
      124 |                 deb_info("st->data: %*ph\n", 5, st->data);
    drivers/media/usb/dvb-usb/m920x.c: In function 'm920x_rc_query':
    drivers/media/usb/dvb-usb/m920x.c:207:58: error: suggest braces around empty body in an 'if' statement [-Werror=empty-body]
      207 |                 deb("Unknown rc key %02x\n", rc_state[1]);
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/media/usb/dvb-usb/dvb-usb.h
+++ drivers/media/usb/dvb-usb/dvb-usb.h
@@ -38,8 +38,8 @@
 }
 #define DVB_USB_DEBUG_STATUS
 #else
-#define dprintk(args...)
-#define debug_dump(b,l,func)
+#define dprintk(args...) no_printk(args)
+#define debug_dump(b,l,func) do { } while (0)
 
 #define DVB_USB_DEBUG_STATUS " (debugging is not enabled)"
 
