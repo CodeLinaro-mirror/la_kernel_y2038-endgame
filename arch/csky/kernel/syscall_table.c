d57dbf13893f704ebf718bcf13f364452de01b28
Arnd Bergmann <arnd@arndb.de>
2021-03-19 11:29:41 +0100

    global: -Woverride-init warnings
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/csky/kernel/syscall_table.c
+++ arch/csky/kernel/syscall_table.c
@@ -8,6 +8,10 @@
 #define __SYSCALL(nr, call)[nr] = (call),
 
 #define sys_fadvise64_64 sys_csky_fadvise64_64
+
+__diag_ignore(GCC, 5, "-Woverride-init", "default to sys_ni_syscall")
+__diag_ignore(clang, 9, "-Winitializer-overrides", "default to sys_ni_syscall")
+
 void * const sys_call_table[__NR_syscalls] __page_aligned_data = {
 	[0 ... __NR_syscalls - 1] = sys_ni_syscall,
 #include <asm/unistd.h>
