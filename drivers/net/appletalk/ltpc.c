bc9012078591cbab11caad15ca990eb67ea35959
Arnd Bergmann <arnd@arndb.de>
2021-04-13 17:42:19 +0200

    [net-next] appletalk: ltpc: remove static probing
    This driver never relies on the netdev_boot_setup_check()
    to get its configuration, so it can just as well do its
    own probing all the time.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/net/appletalk/ltpc.c
+++ drivers/net/appletalk/ltpc.c
@@ -1013,7 +1013,7 @@ static const struct net_device_ops ltpc_netdev = {
 	.ndo_set_rx_mode	= set_multicast_list,
 };
 
-struct net_device * __init ltpc_probe(void)
+static struct net_device * __init ltpc_probe(void)
 {
 	struct net_device *dev;
 	int err = -ENOMEM;
@@ -1219,11 +1219,9 @@ static int __init ltpc_setup(char *str)
 }
 
 __setup("ltpc=", ltpc_setup);
-#endif /* MODULE */
+#endif
 
 static struct net_device *dev_ltpc;
-
-#ifdef MODULE
 
 MODULE_LICENSE("GPL");
 module_param(debug, int, 0);
@@ -1242,7 +1240,6 @@ static int __init ltpc_module_init(void)
 	return PTR_ERR_OR_ZERO(dev_ltpc);
 }
 module_init(ltpc_module_init);
-#endif
 
 static void __exit ltpc_cleanup(void)
 {
