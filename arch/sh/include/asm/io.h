29aae404c6c72b677bc8d5f82ebfe17dd313eb1f
Arnd Bergmann <arnd@arndb.de>
2021-04-25 16:51:30 +0200

    sh: define nommio ioremap/iounmap as inline functions
    Build testing se7619_defconfig produces a harmless warning:
    
    drivers/tty/serial/sh-sci.c: In function 'sci_remap_port':
    drivers/tty/serial/sh-sci.c:2666:19: error: unused variable 'sport' [-Werror=unused-variable]
     2666 |  struct sci_port *sport = to_sci_port(port);
    
    Change the definition of the two helpers to inline functions
    to get proper type checking as well as telling the compiler
    when these veriables are actually used.
    
    Fixes: e4d6f911a7ce ("sh-sci: Get register size from platform device")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/sh/include/asm/io.h
+++ arch/sh/include/asm/io.h
@@ -271,8 +271,13 @@ static inline void __iomem *ioremap_prot(phys_addr_t offset, unsigned long size,
 #endif /* CONFIG_HAVE_IOREMAP_PROT */
 
 #else /* CONFIG_MMU */
-#define iounmap(addr)		do { } while (0)
-#define ioremap(offset, size)	((void __iomem *)(unsigned long)(offset))
+static inline void iounmap(void __iomem *p)
+{
+}
+static inline void __iomem *ioremap(phys_addr_t offset, unsigned long size)
+{
+	return (void __iomem *)(unsigned long)offset;
+}
 #endif /* CONFIG_MMU */
 
 #define ioremap_uc	ioremap
