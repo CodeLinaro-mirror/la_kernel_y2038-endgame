c216b8b0b24ae6cb1646904a4886d80efff49877
Arnd Bergmann <arnd@arndb.de>
2021-09-22 13:11:34 +0200

    [SUBMITTED 20240328] infiniband: uverbs: avoid out-of-range warnings
    clang warns for comparisons that are always true, which is the case
    for these two page size checks on architectures with 64KB pages:
    
    drivers/infiniband/core/uverbs_ioctl.c:90:39: error: result of comparison of constant 65536 with expression of type 'u16' (aka 'unsigned short') is always false [-Werror,-Wtautological-constant-out-of-range-compare]
            WARN_ON_ONCE(method_elm->bundle_size > PAGE_SIZE);
            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~
    include/asm-generic/bug.h:104:25: note: expanded from macro 'WARN_ON_ONCE'
            int __ret_warn_on = !!(condition);                      \
                                   ^~~~~~~~~
    drivers/infiniband/core/uverbs_ioctl.c:621:17: error: result of comparison of constant 65536 with expression of type '__u16' (aka 'unsigned short') is always false [-Werror,-Wtautological-constant-out-of-range-compare]
            if (hdr.length > PAGE_SIZE ||
                ~~~~~~~~~~ ^ ~~~~~~~~~
    
    Add a cast to u32 in both cases, so it never warns about this.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/infiniband/core/uverbs_ioctl.c
+++ drivers/infiniband/core/uverbs_ioctl.c
@@ -90,7 +90,7 @@ void uapi_compute_bundle_size(struct uverbs_api_ioctl_method *method_elm,
 		ALIGN(bundle_size + 256, sizeof(*pbundle->internal_buffer));
 
 	/* Do not want order-2 allocations for this. */
-	WARN_ON_ONCE(method_elm->bundle_size > PAGE_SIZE);
+	WARN_ON_ONCE((u32)method_elm->bundle_size > PAGE_SIZE);
 }
 
 /**
@@ -636,7 +636,7 @@ long ib_uverbs_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	if (err)
 		return -EFAULT;
 
-	if (hdr.length > PAGE_SIZE ||
+	if ((u32)hdr.length > PAGE_SIZE ||
 	    hdr.length != struct_size(&hdr, attrs, hdr.num_attrs))
 		return -EINVAL;
 
