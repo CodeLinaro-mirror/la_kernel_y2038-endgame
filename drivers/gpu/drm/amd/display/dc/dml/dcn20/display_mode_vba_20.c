d96895ae1e00293965391074d390ceb2959f73ba
Arnd Bergmann <arnd@arndb.de>
2021-09-22 14:18:09 +0200

    [HACK] amdgpu: dml: disable all stack-heavy functions
    clang-14 on x86-32 produces a ton of warnings for possible stack
    overflow:
    
    dml/dcn20/display_mode_vba_20.c:1085:13: error: stack frame size (1308) exceeds limit (1024) in function 'dml20_DISPCLKDPPCLKDCFCLKDeepSleepPrefetchParametersWatermarksAndPerformanceCalculation' [-Werror,-Wframe-larger-than]
    dml/dcn20/display_mode_vba_20.c:3286:6: error: stack frame size (1500) exceeds limit (1024) in function 'dml20_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
    dml/dcn20/display_mode_vba_20v2.c:1145:13: error: stack frame size (1196) exceeds limit (1024) in function 'dml20v2_DISPCLKDPPCLKDCFCLKDeepSleepPrefetchParametersWatermarksAndPerformanceCalculation' [-Werror,-Wframe-larger-than]
    dml/dcn20/display_mode_vba_20v2.c:3393:6: error: stack frame size (1356) exceeds limit (1024) in function 'dml20v2_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
    dml/dcn20/display_rq_dlg_calc_20.c:1549:6: error: stack frame size (1356) exceeds limit (1024) in function 'dml20_rq_dlg_get_dlg_reg' [-Werror,-Wframe-larger-than]
    dml/dcn20/display_rq_dlg_calc_20v2.c:1550:6: error: stack frame size (1356) exceeds limit (1024) in function 'dml20v2_rq_dlg_get_dlg_reg' [-Werror,-Wframe-larger-than]
    dml/dcn21/display_mode_vba_21.c:1466:13: error: stack frame size (1276) exceeds limit (1024) in function 'DISPCLKDPPCLKDCFCLKDeepSleepPrefetchParametersWatermarksAndPerformanceCalculation' [-Werror,-Wframe-larger-than]
    dml/dcn21/display_mode_vba_21.c:3397:6: error: stack frame size (1564) exceeds limit (1024) in function 'dml21_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
    dml/dcn21/display_rq_dlg_calc_21.c:1657:6: error: stack frame size (1452) exceeds limit (1024) in function 'dml21_rq_dlg_get_dlg_reg' [-Werror,-Wframe-larger-than]
    dml/dcn30/display_rq_dlg_calc_30.c:1831:6: error: stack frame size (1500) exceeds limit (1024) in function 'dml30_rq_dlg_get_dlg_reg' [-Werror,-Wframe-larger-than]
    dml/dcn31/display_rq_dlg_calc_31.c:1676:6: error: stack frame size (1564) exceeds limit (1024) in function 'dml31_rq_dlg_get_dlg_reg' [-Werror,-Wframe-larger-than]
    dml/dml1_display_rq_dlg_calc.c:997:6: error: stack frame size (1148) exceeds limit (1024) in function 'dml1_rq_dlg_get_dlg_params' [-Werror,-Wframe-larger-than]
    
    Shut those up the wrong way for compile-testing by always returning
    early from the affected functions.
    
    Obviously this must not get applied, but fixed properly instead.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/dml/dcn20/display_mode_vba_20.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn20/display_mode_vba_20.c
@@ -1084,6 +1084,8 @@ static void dml20_DISPCLKDPPCLKDCFCLKDeepSleepPrefetchParametersWatermarksAndPer
 {
 	unsigned int j, k;
 
+	return;
+
 	mode_lib->vba.WritebackDISPCLK = 0.0;
 	mode_lib->vba.DISPCLKWithRamping = 0;
 	mode_lib->vba.DISPCLKWithoutRamping = 0;
@@ -3286,6 +3288,8 @@ void dml20_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_l
 
 	int i;
 	unsigned int j, k, m;
+
+	return;
 
 	/*MODE SUPPORT, VOLTAGE STATE AND SOC CONFIGURATION*/
 
--- drivers/gpu/drm/amd/display/dc/dml/dcn20/display_mode_vba_20v2.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn20/display_mode_vba_20v2.c
@@ -1144,6 +1144,8 @@ static void dml20v2_DISPCLKDPPCLKDCFCLKDeepSleepPrefetchParametersWatermarksAndP
 {
 	unsigned int j, k;
 
+	return;
+
 	mode_lib->vba.WritebackDISPCLK = 0.0;
 	mode_lib->vba.DISPCLKWithRamping = 0;
 	mode_lib->vba.DISPCLKWithoutRamping = 0;
@@ -3393,6 +3395,8 @@ void dml20v2_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode
 
 	int i;
 	unsigned int j, k, m;
+
+	return;
 
 	/*MODE SUPPORT, VOLTAGE STATE AND SOC CONFIGURATION*/
 
