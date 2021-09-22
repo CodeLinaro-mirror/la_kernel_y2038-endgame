f12825bafda6525cf7e46380fdae896340919bb7
Arnd Bergmann <arnd@arndb.de>
2021-09-22 13:41:53 +0200

    ipv4: tcp_output
    net/ipv4/tcp_output.c:188:3: error: result of comparison of constant -1 with expression of type 'u8' (aka 'unsigned char') is always false [-Werror,-Wtautological-constant-out-of-range-compare]
                    NET_ADD_STATS(sock_net(sk), LINUX_MIB_TCPACKCOMPRESSED,
                    ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    include/net/ip.h:291:41: note: expanded from macro 'NET_ADD_STATS'
     #define NET_ADD_STATS(net, field, adnd) SNMP_ADD_STATS((net)->mib.net_statistics, field, adnd)
                                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    include/net/snmp.h:143:4: note: expanded from macro 'SNMP_ADD_STATS'
                            this_cpu_add(mib->mibs[field], addend)
                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    include/linux/percpu-defs.h:509:33: note: expanded from macro 'this_cpu_add'
     #define this_cpu_add(pcp, val)          __pcpu_size_call(this_cpu_add_, pcp, val)
                                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    note: (skipping 1 expansions in backtrace; use -fmacro-backtrace-limit=0 to see all)
    <scratch space>:187:1: note: expanded from here
    this_cpu_add_8
    ^
    arch/x86/include/asm/percpu.h:326:35: note: expanded from macro 'this_cpu_add_8'
     #define this_cpu_add_8(pcp, val)                percpu_add_op(8, volatile, (pcp), val)
                                                    ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    arch/x86/include/asm/percpu.h:127:31: note: expanded from macro 'percpu_add_op'
                                  ((val) == 1 || (val) == -1)) ?            \
                                                 ~~~~~ ^  ~~
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -186,7 +186,7 @@ static inline void tcp_event_ack_sent(struct sock *sk, unsigned int pkts,
 
 	if (unlikely(tp->compressed_ack)) {
 		NET_ADD_STATS(sock_net(sk), LINUX_MIB_TCPACKCOMPRESSED,
-			      tp->compressed_ack);
+			      (int)tp->compressed_ack);
 		tp->compressed_ack = 0;
 		if (hrtimer_try_to_cancel(&tp->compressed_ack_timer) == 1)
 			__sock_put(sk);
