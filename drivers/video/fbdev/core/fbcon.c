91d95923740d4296288090882c7d99824622d6e6
Arnd Bergmann <arnd@arndb.de>
2021-10-28 14:18:11 +0200

    fbdev: move logo code into separate file
    This is purely cosmetic, to declutter fbmem.c
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -483,7 +483,7 @@ static int __init fb_console_setup(char *this_opt)
 			continue;
 		}
 #endif
-
+#ifdef CONFIG_LOGO
 		if (!strncmp(options, "logo-pos:", 9)) {
 			options += 9;
 			if (!strcmp(options, "center"))
@@ -497,6 +497,7 @@ static int __init fb_console_setup(char *this_opt)
 				fb_logo_count = simple_strtol(options, &options, 0);
 			continue;
 		}
+#endif
 	}
 	return 1;
 }
