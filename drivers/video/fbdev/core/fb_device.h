f2c31cd2ba5ab6bb288bd80f4d359543b87473db
Arnd Bergmann <arnd@arndb.de>
2021-10-28 15:34:33 +0200

    fbdev: decouple user access
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/video/fbdev/core/fb_device.h
+++ drivers/video/fbdev/core/fb_device.h
@@ -1,10 +1,28 @@
 
+#ifdef CONFIG_FB_DEVICE
 int fb_chrdev_register(void);
 void fb_chrdev_unregister(void);
 
 struct fb_info;
 void fb_chrdev_create(struct fb_info *fb_info);
 void fb_chrdev_destroy(struct fb_info *fb_info);
+#else
+static inline int fb_chrdev_register(void)
+{
+	return 0;
+}
+static inline void fb_chrdev_unregister(void)
+{
+}
+
+struct fb_info;
+static inline void fb_chrdev_create(struct fb_info *fb_info)
+{
+}
+static inline void fb_chrdev_destroy(struct fb_info *fb_info)
+{
+}
+#endif
 
 struct fb_info *get_fb_info(unsigned int idx);
 void put_fb_info(struct fb_info *fb_info);
--- include/linux/fb.h
+++ include/linux/fb.h
@@ -694,8 +694,18 @@ static inline bool fb_be_math(struct fb_info *info)
 /* drivers/video/fbsysfs.c */
 extern struct fb_info *framebuffer_alloc(size_t size, struct device *dev);
 extern void framebuffer_release(struct fb_info *info);
+#ifdef CONFIG_FB_DEVICE
 extern int fb_init_device(struct fb_info *fb_info);
 extern void fb_cleanup_device(struct fb_info *head);
+#else
+static inline int fb_init_device(struct fb_info *fb_info)
+{
+	return 0;
+}
+static inline void fb_cleanup_device(struct fb_info *head)
+{
+}
+#endif
 extern void fb_bl_default_curve(struct fb_info *fb_info, u8 off, u8 min, u8 max);
 
 /* drivers/video/fbmon.c */
