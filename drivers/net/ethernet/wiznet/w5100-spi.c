068a53cdef3a097f44cfef748755bc37855fc2a5
Arnd Bergmann <arnd@arndb.de>
2021-11-12 10:49:15 +0100

    wiznet: convert to GPIO descriptors
    The w5100/w5300 drivers only support probing with old platform data in
    MMIO mode, or probing with DT in SPI mode. There are no users of this
    platform data in tree, and from the git history it appears that the only
    users of MMIO mode were on the (since removed) blackfin architecture.
    
    Remove the platform data option, as it's unlikely to still be needed, and
    change the internal operation to GPIO descriptors, making the behavior
    the same for SPI and MMIO mode. The other data in the platform_data
    structure is the MAC address, so make that also handled the same for both.
    
    It would probably be possible to just remove the MMIO mode driver
    completely, but it seems fine otherwise, and fixing it to use the modern
    interface seems easy enough.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/net/ethernet/wiznet/w5100-spi.c
+++ drivers/net/ethernet/wiznet/w5100-spi.c
@@ -423,23 +423,12 @@ static int w5100_spi_probe(struct spi_device *spi)
 	const struct of_device_id *of_id;
 	const struct w5100_ops *ops;
 	kernel_ulong_t driver_data;
-	const void *mac = NULL;
-	u8 tmpmac[ETH_ALEN];
 	int priv_size;
-	int ret;
 
-	ret = of_get_mac_address(spi->dev.of_node, tmpmac);
-	if (!ret)
-		mac = tmpmac;
-
-	if (spi->dev.of_node) {
-		of_id = of_match_device(w5100_of_match, &spi->dev);
-		if (!of_id)
-			return -ENODEV;
-		driver_data = (kernel_ulong_t)of_id->data;
-	} else {
-		driver_data = spi_get_device_id(spi)->driver_data;
-	}
+	of_id = of_match_device(w5100_of_match, &spi->dev);
+	if (!of_id)
+		return -ENODEV;
+	driver_data = (kernel_ulong_t)of_id->data;
 
 	switch (driver_data) {
 	case W5100:
@@ -458,7 +447,7 @@ static int w5100_spi_probe(struct spi_device *spi)
 		return -EINVAL;
 	}
 
-	return w5100_probe(&spi->dev, ops, priv_size, mac, spi->irq, -EINVAL);
+	return w5100_probe(&spi->dev, ops, priv_size, spi->irq);
 }
 
 static void w5100_spi_remove(struct spi_device *spi)
