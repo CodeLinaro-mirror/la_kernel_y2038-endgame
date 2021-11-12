3986e4810210dfea273262b4e9f6b49d1930039f
Arnd Bergmann <arnd@arndb.de>
2021-11-12 16:36:17 +0100

    ASoC: samsung: remove DMA filter function and data
    This data is no longer passed by the platform code, so
    there is no point passing it down at all.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- sound/soc/samsung/dma.h
+++ sound/soc/samsung/dma.h
@@ -12,7 +12,7 @@
  * @tx, @rx arguments can be NULL if the DMA channel names are "tx", "rx",
  * otherwise actual DMA channel names must be passed to this function.
  */
-int samsung_asoc_dma_platform_register(struct device *dev, dma_filter_fn filter,
+int samsung_asoc_dma_platform_register(struct device *dev,
 				       const char *tx, const char *rx,
 				       struct device *dma_dev);
 #endif /* _SAMSUNG_DMA_H */
