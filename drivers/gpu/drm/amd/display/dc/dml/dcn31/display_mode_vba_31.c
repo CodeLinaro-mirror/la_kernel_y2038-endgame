074c502a1f654110a64cb8b8eb0985a50869fed8
Arnd Bergmann <arnd@arndb.de>
2021-12-07 12:15:27 +0100

    amdgpu: disable one frame size warning
    drivers/gpu/drm/amd/amdgpu/../display/dc/dml/dcn31/display_mode_vba_31.c:3956:6: error: stack frame size (2104) exceeds limit (2048) in function 'dml31_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
    void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/dml/dcn31/display_mode_vba_31.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn31/display_mode_vba_31.c
@@ -3794,6 +3794,8 @@ static void PatchDETBufferSizeInKByte(unsigned int NumberOfActivePlanes, int NoO
 }
 
 
+__diag_push()
+__diag_ignore(clang, 10, "-Wframe-larger-than=", "this exceeds 2048 bytes")
 void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
 {
 	struct vba_vars_st *v = &mode_lib->vba;
@@ -5551,6 +5553,7 @@ void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_l
 		v->maxMpcComb = MaximumMPCCombine;
 	}
 }
+__diag_pop()
 
 static void CalculateWatermarksAndDRAMSpeedChangeSupport(
 		struct display_mode_lib *mode_lib,
