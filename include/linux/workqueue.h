eca7d0a29075221f245a5204218491bb5f900f9a
Arnd Bergmann <arnd@arndb.de>
2021-12-08 14:01:06 +0100

    workqueue: split out linux/workqueue_types.h
    Work queues are commonly embedded in other structures, but the definition
    in linux/workqueue.h pulls in other large headers like linux/atomic.h
    and linux/rcupdate.h.
    
    Provide a minimal version of the header that can be included in places
    that just need the type definition itself.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/workqueue.h
+++ include/linux/workqueue.h
@@ -6,6 +6,7 @@
 #ifndef _LINUX_WORKQUEUE_H
 #define _LINUX_WORKQUEUE_H
 
+#include <linux/workqueue_types.h>
 #include <linux/timer.h>
 #include <linux/linkage.h>
 #include <linux/bitops.h>
@@ -15,10 +16,6 @@
 #include <linux/cpumask.h>
 #include <linux/rcupdate.h>
 
-struct workqueue_struct;
-
-struct work_struct;
-typedef void (*work_func_t)(struct work_struct *work);
 void delayed_work_timer_fn(struct timer_list *t);
 
 /*
@@ -26,103 +23,6 @@ void delayed_work_timer_fn(struct timer_list *t);
  * one
  */
 #define work_data_bits(work) ((unsigned long *)(&(work)->data))
-
-enum {
-	WORK_STRUCT_PENDING_BIT	= 0,	/* work item is pending execution */
-	WORK_STRUCT_INACTIVE_BIT= 1,	/* work item is inactive */
-	WORK_STRUCT_PWQ_BIT	= 2,	/* data points to pwq */
-	WORK_STRUCT_LINKED_BIT	= 3,	/* next work is linked to this one */
-#ifdef CONFIG_DEBUG_OBJECTS_WORK
-	WORK_STRUCT_STATIC_BIT	= 4,	/* static initializer (debugobjects) */
-	WORK_STRUCT_COLOR_SHIFT	= 5,	/* color for workqueue flushing */
-#else
-	WORK_STRUCT_COLOR_SHIFT	= 4,	/* color for workqueue flushing */
-#endif
-
-	WORK_STRUCT_COLOR_BITS	= 4,
-
-	WORK_STRUCT_PENDING	= 1 << WORK_STRUCT_PENDING_BIT,
-	WORK_STRUCT_INACTIVE	= 1 << WORK_STRUCT_INACTIVE_BIT,
-	WORK_STRUCT_PWQ		= 1 << WORK_STRUCT_PWQ_BIT,
-	WORK_STRUCT_LINKED	= 1 << WORK_STRUCT_LINKED_BIT,
-#ifdef CONFIG_DEBUG_OBJECTS_WORK
-	WORK_STRUCT_STATIC	= 1 << WORK_STRUCT_STATIC_BIT,
-#else
-	WORK_STRUCT_STATIC	= 0,
-#endif
-
-	WORK_NR_COLORS		= (1 << WORK_STRUCT_COLOR_BITS),
-
-	/* not bound to any CPU, prefer the local CPU */
-	WORK_CPU_UNBOUND	= NR_CPUS,
-
-	/*
-	 * Reserve 8 bits off of pwq pointer w/ debugobjects turned off.
-	 * This makes pwqs aligned to 256 bytes and allows 16 workqueue
-	 * flush colors.
-	 */
-	WORK_STRUCT_FLAG_BITS	= WORK_STRUCT_COLOR_SHIFT +
-				  WORK_STRUCT_COLOR_BITS,
-
-	/* data contains off-queue information when !WORK_STRUCT_PWQ */
-	WORK_OFFQ_FLAG_BASE	= WORK_STRUCT_COLOR_SHIFT,
-
-	__WORK_OFFQ_CANCELING	= WORK_OFFQ_FLAG_BASE,
-	WORK_OFFQ_CANCELING	= (1 << __WORK_OFFQ_CANCELING),
-
-	/*
-	 * When a work item is off queue, its high bits point to the last
-	 * pool it was on.  Cap at 31 bits and use the highest number to
-	 * indicate that no pool is associated.
-	 */
-	WORK_OFFQ_FLAG_BITS	= 1,
-	WORK_OFFQ_POOL_SHIFT	= WORK_OFFQ_FLAG_BASE + WORK_OFFQ_FLAG_BITS,
-	WORK_OFFQ_LEFT		= BITS_PER_LONG - WORK_OFFQ_POOL_SHIFT,
-	WORK_OFFQ_POOL_BITS	= WORK_OFFQ_LEFT <= 31 ? WORK_OFFQ_LEFT : 31,
-	WORK_OFFQ_POOL_NONE	= (1LU << WORK_OFFQ_POOL_BITS) - 1,
-
-	/* convenience constants */
-	WORK_STRUCT_FLAG_MASK	= (1UL << WORK_STRUCT_FLAG_BITS) - 1,
-	WORK_STRUCT_WQ_DATA_MASK = ~WORK_STRUCT_FLAG_MASK,
-	WORK_STRUCT_NO_POOL	= (unsigned long)WORK_OFFQ_POOL_NONE << WORK_OFFQ_POOL_SHIFT,
-
-	/* bit mask for work_busy() return values */
-	WORK_BUSY_PENDING	= 1 << 0,
-	WORK_BUSY_RUNNING	= 1 << 1,
-
-	/* maximum string length for set_worker_desc() */
-	WORKER_DESC_LEN		= 24,
-};
-
-struct work_struct {
-	atomic_long_t data;
-	struct list_head entry;
-	work_func_t func;
-#ifdef CONFIG_LOCKDEP
-	struct lockdep_map lockdep_map;
-#endif
-};
-
-#define WORK_DATA_INIT()	ATOMIC_LONG_INIT((unsigned long)WORK_STRUCT_NO_POOL)
-#define WORK_DATA_STATIC_INIT()	\
-	ATOMIC_LONG_INIT((unsigned long)(WORK_STRUCT_NO_POOL | WORK_STRUCT_STATIC))
-
-struct delayed_work {
-	struct work_struct work;
-	struct timer_list timer;
-
-	/* target workqueue and CPU ->timer uses to queue ->work */
-	struct workqueue_struct *wq;
-	int cpu;
-};
-
-struct rcu_work {
-	struct work_struct work;
-	struct rcu_head rcu;
-
-	/* target workqueue ->rcu uses to queue ->work */
-	struct workqueue_struct *wq;
-};
 
 /**
  * struct workqueue_attrs - A struct for workqueue attributes.
@@ -164,40 +64,6 @@ struct execute_work {
 	struct work_struct work;
 };
 
-#ifdef CONFIG_LOCKDEP
-/*
- * NB: because we have to copy the lockdep_map, setting _key
- * here is required, otherwise it could get initialised to the
- * copy of the lockdep_map!
- */
-#define __WORK_INIT_LOCKDEP_MAP(n, k) \
-	.lockdep_map = STATIC_LOCKDEP_MAP_INIT(n, k),
-#else
-#define __WORK_INIT_LOCKDEP_MAP(n, k)
-#endif
-
-#define __WORK_INITIALIZER(n, f) {					\
-	.data = WORK_DATA_STATIC_INIT(),				\
-	.entry	= { &(n).entry, &(n).entry },				\
-	.func = (f),							\
-	__WORK_INIT_LOCKDEP_MAP(#n, &(n))				\
-	}
-
-#define __DELAYED_WORK_INITIALIZER(n, f, tflags) {			\
-	.work = __WORK_INITIALIZER((n).work, (f)),			\
-	.timer = __TIMER_INITIALIZER(delayed_work_timer_fn,\
-				     (tflags) | TIMER_IRQSAFE),		\
-	}
-
-#define DECLARE_WORK(n, f)						\
-	struct work_struct n = __WORK_INITIALIZER(n, f)
-
-#define DECLARE_DELAYED_WORK(n, f)					\
-	struct delayed_work n = __DELAYED_WORK_INITIALIZER(n, f, 0)
-
-#define DECLARE_DEFERRABLE_WORK(n, f)					\
-	struct delayed_work n = __DELAYED_WORK_INITIALIZER(n, f, TIMER_DEFERRABLE)
-
 #ifdef CONFIG_DEBUG_OBJECTS_WORK
 extern void __init_work(struct work_struct *work, int onstack);
 extern void destroy_work_on_stack(struct work_struct *work);
@@ -212,74 +78,6 @@ static inline void destroy_work_on_stack(struct work_struct *work) { }
 static inline void destroy_delayed_work_on_stack(struct delayed_work *work) { }
 static inline unsigned int work_static(struct work_struct *work) { return 0; }
 #endif
-
-/*
- * initialize all of a work item in one go
- *
- * NOTE! No point in using "atomic_long_set()": using a direct
- * assignment of the work data initializer allows the compiler
- * to generate better code.
- */
-#ifdef CONFIG_LOCKDEP
-#define __INIT_WORK(_work, _func, _onstack)				\
-	do {								\
-		static struct lock_class_key __key;			\
-									\
-		__init_work((_work), _onstack);				\
-		(_work)->data = (atomic_long_t) WORK_DATA_INIT();	\
-		lockdep_init_map(&(_work)->lockdep_map, "(work_completion)"#_work, &__key, 0); \
-		INIT_LIST_HEAD(&(_work)->entry);			\
-		(_work)->func = (_func);				\
-	} while (0)
-#else
-#define __INIT_WORK(_work, _func, _onstack)				\
-	do {								\
-		__init_work((_work), _onstack);				\
-		(_work)->data = (atomic_long_t) WORK_DATA_INIT();	\
-		INIT_LIST_HEAD(&(_work)->entry);			\
-		(_work)->func = (_func);				\
-	} while (0)
-#endif
-
-#define INIT_WORK(_work, _func)						\
-	__INIT_WORK((_work), (_func), 0)
-
-#define INIT_WORK_ONSTACK(_work, _func)					\
-	__INIT_WORK((_work), (_func), 1)
-
-#define __INIT_DELAYED_WORK(_work, _func, _tflags)			\
-	do {								\
-		INIT_WORK(&(_work)->work, (_func));			\
-		__init_timer(&(_work)->timer,				\
-			     delayed_work_timer_fn,			\
-			     (_tflags) | TIMER_IRQSAFE);		\
-	} while (0)
-
-#define __INIT_DELAYED_WORK_ONSTACK(_work, _func, _tflags)		\
-	do {								\
-		INIT_WORK_ONSTACK(&(_work)->work, (_func));		\
-		__init_timer_on_stack(&(_work)->timer,			\
-				      delayed_work_timer_fn,		\
-				      (_tflags) | TIMER_IRQSAFE);	\
-	} while (0)
-
-#define INIT_DELAYED_WORK(_work, _func)					\
-	__INIT_DELAYED_WORK(_work, _func, 0)
-
-#define INIT_DELAYED_WORK_ONSTACK(_work, _func)				\
-	__INIT_DELAYED_WORK_ONSTACK(_work, _func, 0)
-
-#define INIT_DEFERRABLE_WORK(_work, _func)				\
-	__INIT_DELAYED_WORK(_work, _func, TIMER_DEFERRABLE)
-
-#define INIT_DEFERRABLE_WORK_ONSTACK(_work, _func)			\
-	__INIT_DELAYED_WORK_ONSTACK(_work, _func, TIMER_DEFERRABLE)
-
-#define INIT_RCU_WORK(_work, _func)					\
-	INIT_WORK(&(_work)->work, (_func))
-
-#define INIT_RCU_WORK_ONSTACK(_work, _func)				\
-	INIT_WORK_ONSTACK(&(_work)->work, (_func))
 
 /**
  * work_pending - Find out whether a work item is currently pending
