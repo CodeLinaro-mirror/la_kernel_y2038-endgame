26f7332636d281360e148f7a90062ee25e0b0e36
Arnd Bergmann <arnd@arndb.de>
2022-12-12 21:05:38 +0100

    [HACK] cpufreq: e_powersaver: work around kmalloc size limit warning
    drivers/cpufreq/e_powersaver.c: In function 'eps_cpu_init':
    drivers/cpufreq/e_powersaver.c:324:19: error: argument 1 value '4294967295' exceeds maximum object size 2147483647 [-Werror=alloc-size-larger-than=]
      324 |         centaur = kzalloc(struct_size(centaur, freq_table, states + 1),
          |                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      325 |                           GFP_KERNEL);
          |                           ~~~~~~~~~~~
    In file included from drivers/cpufreq/e_powersaver.c:15:
    include/linux/slab.h:687:37: note: in a call to allocation function 'kzalloc.constprop' declared here
      687 | static inline __alloc_size(1) void *kzalloc(size_t size, gfp_t flags)
          |                                     ^~~~~~~
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/cpufreq/e_powersaver.c
+++ drivers/cpufreq/e_powersaver.c
@@ -184,6 +184,7 @@ static int eps_cpu_init(struct cpufreq_policy *policy)
 	struct cpufreq_frequency_table *f_table;
 	int k, step, voltage;
 	int states;
+	size_t size;
 #if IS_ENABLED(CONFIG_ACPI_PROCESSOR)
 	unsigned int limit;
 #endif
@@ -321,8 +322,11 @@ static int eps_cpu_init(struct cpufreq_policy *policy)
 		states = 2;
 
 	/* Allocate private data and frequency table for current cpu */
-	centaur = kzalloc(struct_size(centaur, freq_table, states + 1),
-			  GFP_KERNEL);
+	size = struct_size(centaur, freq_table, states + 1);
+	if (size > KMALLOC_MAX_SIZE)
+		return -ENOMEM;
+
+	centaur = kzalloc(size, GFP_KERNEL);
 	if (!centaur)
 		return -ENOMEM;
 	eps_cpu[0] = centaur;
