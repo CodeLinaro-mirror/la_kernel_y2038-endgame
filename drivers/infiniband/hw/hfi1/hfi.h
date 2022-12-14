bb15369d267552ac871e0906aac8aa4c8340c0df
Arnd Bergmann <arnd@arndb.de>
2022-12-14 12:51:52 +0100

    rdma: hfi1: avoid field overflow warning
    In function 'fortify_memset_chk',
        inlined from '__subn_get_opa_hfi1_cong_log' at drivers/infiniband/hw/hfi1/mad.c:3873:2:
    include/linux/fortify-string.h:423:25: error: call to '__write_overflow_field' declared with attribute warning: detected write beyond size of field (1st parameter); maybe use struct_group()? [-Werror=attribute-warning]
      423 |                         __write_overflow_field(p_size_field, size);
          |                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/infiniband/hw/hfi1/hfi.h
+++ drivers/infiniband/hw/hfi1/hfi.h
@@ -883,7 +883,9 @@ struct hfi1_pportdata {
 	 * cc_log_lock protects all congestion log related data
 	 */
 	spinlock_t cc_log_lock ____cacheline_aligned_in_smp;
-	u8 threshold_cong_event_map[OPA_MAX_SLS / 8];
+	struct_group (__threshold_cong_event_map,
+		u8 threshold_cong_event_map[OPA_MAX_SLS / 8];
+	);
 	u16 threshold_event_counter;
 	struct opa_hfi1_cong_log_event_internal cc_events[OPA_CONG_LOG_ELEMS];
 	int cc_log_idx; /* index for logging events */
