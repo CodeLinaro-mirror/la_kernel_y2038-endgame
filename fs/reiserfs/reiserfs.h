be81062be1c1647583082edd9bfebab934ad2d0c
Arnd Bergmann <arnd@arndb.de>
2022-12-15 12:20:45 +0100

    [SUBMITTED 20221215] reiserfs: avoid objtool warning after panic
    Calling reiserfs_panic() leaves the calling function in an
    undefined state that objtool complains about, because of the
    __noreturn attribute:
    
    fs/reiserfs/do_balan.o: warning: objtool: balance_leaf+0x109fb: stack state mismatch: cfa1=4+576 cfa2=4+584
    fs/reiserfs/ibalance.o: warning: objtool: balance_internal+0x6099: stack state mismatch: cfa1=4+512 cfa2=4+536
    fs/reiserfs/ibalance.o: warning: objtool: internal_insert_key+0xa1c: stack state mismatch: cfa1=4+224 cfa2=4+216
    
    Avoid that by removing the attribute, at the possible cost of
    a few extra cycles. Mark the panic and error functions as __cold
    instead to at least give the compiler a hint that this is not
    the fast path.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/reiserfs/reiserfs.h
+++ fs/reiserfs/reiserfs.h
@@ -3189,11 +3189,13 @@ int fix_nodes(int n_op_mode, struct tree_balance *tb,
 void unfix_nodes(struct tree_balance *);
 
 /* prints.c */
-void __reiserfs_panic(struct super_block *s, const char *id,
-		      const char *function, const char *fmt, ...)
-    __attribute__ ((noreturn));
-#define reiserfs_panic(s, id, fmt, args...) \
-	__reiserfs_panic(s, id, __func__, fmt, ##args)
+void __cold __reiserfs_panic(struct super_block *s, const char *id,
+		      const char *function, const char *fmt, ...);
+#define reiserfs_panic(s, id, fmt, args...)			\
+	do {							\
+		__reiserfs_panic(s, id, __func__, fmt, ##args);	\
+		BUG();						\
+	} while (0)
 void __reiserfs_error(struct super_block *s, const char *id,
 		      const char *function, const char *fmt, ...);
 #define reiserfs_error(s, id, fmt, args...) \
