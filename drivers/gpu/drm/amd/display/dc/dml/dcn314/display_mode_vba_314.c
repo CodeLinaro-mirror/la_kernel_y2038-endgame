85fdcc4a68835d108161595a334af849b7548e52
Arnd Bergmann <arnd@arndb.de>
2022-12-15 14:05:36 +0100

    fixup! amdgpu: disable one frame size warning

--- drivers/gpu/drm/amd/display/dc/dml/dcn314/display_mode_vba_314.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn314/display_mode_vba_314.c
@@ -3889,6 +3889,8 @@ static noinline void CalculatePrefetchSchedulePerPlane(
 		&v->VReadyOffsetPix[k]);
 }
 
+__diag_push()
+__diag_ignore(clang, 10, "-Wframe-larger-than=", "this exceeds 2048 bytes")
 void dml314_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
 {
 	struct vba_vars_st *v = &mode_lib->vba;
@@ -5585,6 +5587,7 @@ void dml314_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_
 		v->maxMpcComb = MaximumMPCCombine;
 	}
 }
+__diag_pop()
 
 static void CalculateWatermarksAndDRAMSpeedChangeSupport(
 		struct display_mode_lib *mode_lib,
