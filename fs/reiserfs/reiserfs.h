08e45bf3308e9075ff9ee6f88d0dd95bd448f504
Arnd Bergmann <arnd@arndb.de>
2022-12-16 22:32:20 +0100

    fixup! [SUBMITTED 20221215] reiserfs: avoid objtool warning after panic
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/reiserfs/reiserfs.h
+++ fs/reiserfs/reiserfs.h
@@ -3192,7 +3192,7 @@ void __cold __reiserfs_panic(struct super_block *s, const char *id,
 		__reiserfs_panic(s, id, __func__, fmt, ##args);	\
 		BUG();						\
 	} while (0)
-void __cold __reiserfs_error(struct super_block *s, const char *id,
+void __reiserfs_error(struct super_block *s, const char *id,
 		      const char *function, const char *fmt, ...);
 #define reiserfs_error(s, id, fmt, args...) \
 	 __reiserfs_error(s, id, __func__, fmt, ##args)
