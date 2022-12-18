25c2ab15e631d7dea278ec0e301c229e76b30cbe
Arnd Bergmann <arnd@arndb.de>
2022-12-18 19:27:46 +0100

    udf: mediatek: fix CONFIG_PM checks
    The driver uses the old SET_SYSTEM_SLEEP_PM_OPS()/SET_RUNTIME_PM_OPS()
    macros with #ifdef checks and gets them wrong:
    
    drivers/ufs/host/ufs-mediatek.c:1621:8: error: implicit declaration of function 'ufshcd_runtime_suspend' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
            ret = ufshcd_runtime_suspend(dev);
    
    Change it to the more robust SYSTEM_SLEEP_PM_OPS()/RUNTIME_PM_OPS()
    macros to avoid this.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/ufs/host/ufs-mediatek.c
+++ drivers/ufs/host/ufs-mediatek.c
@@ -1756,7 +1756,6 @@ static void ufs_mtk_remove(struct platform_device *pdev)
 	ufshcd_remove(hba);
 }
 
-#ifdef CONFIG_PM_SLEEP
 static int ufs_mtk_system_suspend(struct device *dev)
 {
 	struct ufs_hba *hba = dev_get_drvdata(dev);
@@ -1779,7 +1778,6 @@ static int ufs_mtk_system_resume(struct device *dev)
 
 	return ufshcd_system_resume(dev);
 }
-#endif
 
 #ifdef CONFIG_PM
 static int ufs_mtk_runtime_suspend(struct device *dev)
@@ -1807,10 +1805,10 @@ static int ufs_mtk_runtime_resume(struct device *dev)
 #endif
 
 static const struct dev_pm_ops ufs_mtk_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(ufs_mtk_system_suspend,
-				ufs_mtk_system_resume)
-	SET_RUNTIME_PM_OPS(ufs_mtk_runtime_suspend,
-			   ufs_mtk_runtime_resume, NULL)
+	SYSTEM_SLEEP_PM_OPS(ufs_mtk_system_suspend,
+			    ufs_mtk_system_resume)
+	RUNTIME_PM_OPS(ufs_mtk_runtime_suspend,
+		       ufs_mtk_runtime_resume, NULL)
 	.prepare	 = ufshcd_suspend_prepare,
 	.complete	 = ufshcd_resume_complete,
 };
@@ -1820,7 +1818,7 @@ static struct platform_driver ufs_mtk_pltform = {
 	.remove_new = ufs_mtk_remove,
 	.driver = {
 		.name   = "ufshcd-mtk",
-		.pm     = &ufs_mtk_pm_ops,
+		.pm     = pm_ptr(&ufs_mtk_pm_ops),
 		.of_match_table = ufs_mtk_of_match,
 	},
 };
