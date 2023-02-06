17febec77eac71d80d80e3886ee36fd281da6625
Arnd Bergmann <arnd@arndb.de>
2023-02-06 12:20:00 +0100

    Revert "[SUBMITTED 20211122] [v2] nixge: fix mac address error handling again"
    This reverts commit d12c921de0be6bed08afe011a6c3e2d4e42637a0.

--- drivers/net/ethernet/ni/nixge.c
+++ drivers/net/ethernet/ni/nixge.c
@@ -1245,8 +1245,6 @@ static void *nixge_get_nvmem_address(struct device *dev)
 
 	mac = nvmem_cell_read(cell, &cell_size);
 	nvmem_cell_put(cell);
-	if (IS_ERR(mac))
-		return NULL;
 
 	return mac;
 }
