27cc27ac6db49e493e8acb6c563bfa1e4f091962
Arnd Bergmann <arnd@arndb.de>
2023-03-21 09:36:44 +0100

    fixup! [SUBMITTED 20230117] media: mtk_jpeg_core: avoid unused-variable warning
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/media/platform/mediatek/jpeg/mtk_jpeg_core.c
+++ drivers/media/platform/mediatek/jpeg/mtk_jpeg_core.c
@@ -1866,7 +1866,6 @@ static const struct dev_pm_ops mtk_jpeg_pm_ops = {
 	SET_RUNTIME_PM_OPS(mtk_jpeg_pm_suspend, mtk_jpeg_pm_resume, NULL)
 };
 
-#if defined(CONFIG_OF)
 static const struct mtk_jpeg_variant mt8173_jpeg_drvdata = {
 	.clks = mt8173_jpeg_dec_clocks,
 	.num_clks = ARRAY_SIZE(mt8173_jpeg_dec_clocks),
