a57128af5c0c4c65389af6c4f3bfe07e22b392d5
Arnd Bergmann <arnd@arndb.de>
2023-04-01 18:20:53 +0200

    x86: qspinlock:
    kernel/locking/qspinlock_paravirt.h:493:1: error: no previous prototype for '__pv_queued_spin_unlock_slowpath' [-Werror=missing-prototypes]
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/x86/include/asm/qspinlock_paravirt.h
+++ arch/x86/include/asm/qspinlock_paravirt.h
@@ -4,6 +4,8 @@
 
 #include <asm/ibt.h>
 
+void __lockfunc __pv_queued_spin_unlock_slowpath(struct qspinlock *lock, u8 locked);
+
 /*
  * For x86-64, PV_CALLEE_SAVE_REGS_THUNK() saves and restores 8 64-bit
  * registers. For i386, however, only 1 32-bit register needs to be saved
@@ -59,7 +61,7 @@ DEFINE_PARAVIRT_ASM(__raw_callee_save___pv_queued_spin_unlock,
 
 #else /* CONFIG_64BIT */
 
-extern void __lockfunc __pv_queued_spin_unlock(struct qspinlock *lock);
+void __lockfunc __pv_queued_spin_unlock(struct qspinlock *lock);
 __PV_CALLEE_SAVE_REGS_THUNK(__pv_queued_spin_unlock, ".spinlock.text");
 
 #endif /* CONFIG_64BIT */
