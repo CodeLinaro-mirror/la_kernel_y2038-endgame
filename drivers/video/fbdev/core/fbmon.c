f8ee105a1b8446d2a1d86553c90b5c27a0fa4029
Arnd Bergmann <arnd@arndb.de>
2023-09-12 14:03:47 +0200

    fbdev: move EXPORT_SYMBOL statements next to functions
    As a preparation for additional cleanups, make sure each exported
    function only has one global definition and the export is right
    next to it.
    
    This follows the regular kernel coding style, and allows adding
    more #ifdef checks to hide unused functions in configurations that
    disable the deprecated CONFIG_FB_DEVICE and/or CONFIG_FB and
    only use FB_CORE in order to provide a framebuffer console for
    DRM drivers.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/video/fbdev/core/fbmon.c
+++ drivers/video/fbdev/core/fbmon.c
@@ -696,6 +696,7 @@ void fb_destroy_modedb(struct fb_videomode *modedb)
 {
 	kfree(modedb);
 }
+EXPORT_SYMBOL(fb_destroy_modedb);
 
 static int fb_get_monitor_limits(unsigned char *edid, struct fb_monspecs *specs)
 {
@@ -938,6 +939,7 @@ int fb_parse_edid(unsigned char *edid, struct fb_var_screeninfo *var)
 	}
 	return 1;
 }
+EXPORT_SYMBOL(fb_parse_edid);
 
 void fb_edid_to_monspecs(unsigned char *edid, struct fb_monspecs *specs)
 {
@@ -1004,6 +1006,7 @@ void fb_edid_to_monspecs(unsigned char *edid, struct fb_monspecs *specs)
 
 	DPRINTK("========================================\n");
 }
+EXPORT_SYMBOL(fb_edid_to_monspecs);
 
 /*
  * VESA Generalized Timing Formula (GTF)
@@ -1311,6 +1314,7 @@ int fb_get_mode(int flags, u32 val, struct fb_var_screeninfo *var, struct fb_inf
 	kfree(timings);
 	return err;
 }
+EXPORT_SYMBOL(fb_get_mode);
 
 #ifdef CONFIG_VIDEOMODE_HELPERS
 int fb_videomode_from_videomode(const struct videomode *vm,
@@ -1406,22 +1410,6 @@ EXPORT_SYMBOL_GPL(of_get_fb_videomode);
 #endif /* CONFIG_OF */
 #endif /* CONFIG_VIDEOMODE_HELPERS */
 
-#else
-int fb_parse_edid(unsigned char *edid, struct fb_var_screeninfo *var)
-{
-	return 1;
-}
-void fb_edid_to_monspecs(unsigned char *edid, struct fb_monspecs *specs)
-{
-}
-void fb_destroy_modedb(struct fb_videomode *modedb)
-{
-}
-int fb_get_mode(int flags, u32 val, struct fb_var_screeninfo *var,
-		struct fb_info *info)
-{
-	return -EINVAL;
-}
 #endif /* CONFIG_FB_MODE_HELPERS */
 
 /*
@@ -1486,6 +1474,7 @@ int fb_validate_mode(const struct fb_var_screeninfo *var, struct fb_info *info)
 		pixclock < dclkmin || pixclock > dclkmax) ?
 		-EINVAL : 0;
 }
+EXPORT_SYMBOL(fb_validate_mode);
 
 /*
  * We need to ensure that the EDID block is only returned for
@@ -1510,16 +1499,5 @@ const unsigned char *fb_firmware_edid(struct device *device)
 
 	return edid;
 }
-#else
-const unsigned char *fb_firmware_edid(struct device *device)
-{
-	return NULL;
-}
-#endif
 EXPORT_SYMBOL(fb_firmware_edid);
-
-EXPORT_SYMBOL(fb_parse_edid);
-EXPORT_SYMBOL(fb_edid_to_monspecs);
-EXPORT_SYMBOL(fb_get_mode);
-EXPORT_SYMBOL(fb_validate_mode);
-EXPORT_SYMBOL(fb_destroy_modedb);
+#endif
