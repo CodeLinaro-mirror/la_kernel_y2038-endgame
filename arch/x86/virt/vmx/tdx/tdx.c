8d6afe243cdaac97969c29d296c01019d9735a90
Arnd Bergmann <arnd@arndb.de>
2023-12-12 10:41:30 +0100

    [SUBMITTED 20231212] x86: tdx: hide unused tdx_dump_mce_info()
    When TDX is enabled but MCE is not, the tdx_dump_mce_info() function
    fails to link:
    
    ld.lld: error: undefined symbol: mce_is_memory_error
    ld.lld: error: undefined symbol: mce_usable_address
    >>> referenced by usercopy_64.c
    >>>               vmlinux.o:(tdx_dump_mce_info)
    
    In this configuration, there is also no caller for the function, so
    avoid the problem by enclosing it in an #ifdef block.
    
    Fixes: 45f31973967d ("x86/mce: Differentiate real hardware #MCs from TDX erratum ones")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -1219,6 +1219,7 @@ int tdx_enable(void)
 }
 EXPORT_SYMBOL_FOR_KVM(tdx_enable);
 
+#ifdef CONFIG_X86_MCE
 static bool is_pamt_page(unsigned long phys)
 {
 	struct tdmr_info_list *tdmr_list = &tdx_tdmr_list;
@@ -1318,6 +1319,7 @@ const char *tdx_dump_mce_info(struct mce *m)
 
 	return "TDX private memory error. Possible kernel bug.";
 }
+#endif
 
 static __init int record_keyid_partitioning(u32 *tdx_keyid_start,
 					    u32 *nr_tdx_keyids)
