4457b0ebe61a8c08934f53bc604bd28679fea4eb
Arnd Bergmann <arnd@arndb.de>
2024-03-20 21:29:46 +0100

    Revert "[SUBMITTED 20221215] ipvs: use div_s64 for signed division"
    This reverts commit b14c6b156c2edbbc0780bb7596841b10ce2b6ed9.

--- net/netfilter/ipvs/ip_vs_est.c
+++ net/netfilter/ipvs/ip_vs_est.c
@@ -692,13 +692,15 @@ static int ip_vs_est_calc_limits(struct netns_ipvs *ipvs, int *chain_max)
 		}
 		if (diff >= NSEC_PER_SEC)
 			continue;
-		val = div_s64(diff, loops);
+		val = diff;
+		do_div(val, loops);
 		if (!min_est || val < min_est) {
 			min_est = val;
 			/* goal: 95usec per chain */
 			val = 95 * NSEC_PER_USEC;
 			if (val >= min_est) {
-				max = div_s64(val, min_est);
+				do_div(val, min_est);
+				max = (int)val;
 			} else {
 				max = 1;
 			}
