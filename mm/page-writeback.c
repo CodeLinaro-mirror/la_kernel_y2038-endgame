e7362bd002879503440aebbe6e28d9f019ea5046
Kemeng Shi <shikemeng@huaweicloud.com>
2024-04-25 21:17:24 +0800

    mm: remove stale comment __folio_mark_dirty
    The __folio_mark_dirty will not mark inode dirty any longer.  Remove the
    stale comment of it.
    
    Link: https://lkml.kernel.org/r/20240425131724.36778-5-shikemeng@huaweicloud.com
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Reviewed-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Howard Cochran <hcochran@kernelspring.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Miklos Szeredi <mszeredi@redhat.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -2722,8 +2722,7 @@ void folio_account_cleaned(struct folio *folio, struct bdi_writeback *wb)
 }
 
 /*
- * Mark the folio dirty, and set it dirty in the page cache, and mark
- * the inode dirty.
+ * Mark the folio dirty, and set it dirty in the page cache.
  *
  * If warn is true, then emit a warning if the folio is not uptodate and has
  * not been truncated.
