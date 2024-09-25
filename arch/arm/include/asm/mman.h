2377ab6e6fc0624b4a50a121acb23782fea112f5
Arnd Bergmann <arnd@arndb.de>
2024-09-25 08:49:00 +0000

    ARM: remove uapi/asm/mman.h
    The arch_mmap_check() was accidentally left as part of the uapi headers during
    the initial uapi split, it should have been left in the normal headers
    instead.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/include/asm/mman.h
+++ arch/arm/include/asm/mman.h
@@ -3,12 +3,15 @@
 #define __ASM_MMAN_H__
 
 #include <asm/system_info.h>
-#include <uapi/asm/mman.h>
+#include <asm-generic/mman.h>
 
 static inline bool arch_memory_deny_write_exec_supported(void)
 {
 	return cpu_architecture() >= CPU_ARCH_ARMv6;
 }
 #define arch_memory_deny_write_exec_supported arch_memory_deny_write_exec_supported
+
+#define arch_mmap_check(addr, len, flags) \
+	(((flags) & MAP_FIXED && (addr) < FIRST_USER_ADDRESS) ? -EINVAL : 0)
 
 #endif /* __ASM_MMAN_H__ */
