294c1fdcb4db546675927a0e3b6d68482d31ee1a
Arnd Bergmann <arnd@arndb.de>
2025-03-06 14:49:55 +0100

    arm64: jump_label: don't include asm/insns.h
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm64/include/asm/jump_label.h
+++ arch/arm64/include/asm/jump_label.h
@@ -11,7 +11,7 @@
 #ifndef __ASSEMBLER__
 
 #include <linux/types.h>
-#include <asm/insn.h>
+#include <asm/insn-def.h>
 
 #define HAVE_JUMP_LABEL_BATCH
 #define JUMP_LABEL_NOP_SIZE		AARCH64_INSN_SIZE
