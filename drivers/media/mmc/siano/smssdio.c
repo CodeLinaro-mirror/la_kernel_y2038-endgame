2a7d25178631a70d6ea021f1bbeb84dc6f4a10af
Arnd Bergmann <arnd@arndb.de>
2025-03-19 17:32:39 +0100

    media: siano: don't use GFP_DMA
    SDIO does not require DMA allocations, it's up to the MMC controller
    driver to use bounce buffers if necessary.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/media/mmc/siano/smssdio.c
+++ drivers/media/mmc/siano/smssdio.c
@@ -274,7 +274,7 @@ static int smssdio_probe(struct sdio_func *func,
 		goto free;
 	}
 
-	ret = smscore_register_device(&params, &smsdev->coredev, GFP_DMA, NULL);
+	ret = smscore_register_device(&params, &smsdev->coredev, GFP_KERNEL, NULL);
 	if (ret < 0)
 		goto free;
 
