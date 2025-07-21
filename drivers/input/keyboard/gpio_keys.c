9a02a063dea54dda62d34c74ce14fadf9d6a731b
Arnd Bergmann <arnd@arndb.de>
2025-07-21 21:54:21 +0200

    [v5] Input: gpio-keys: make legacy gpiolib optional
    Most users of gpio-keys and gpio-keys-polled use modern gpiolib
    interfaces, but there are still number of ancient sh, arm32 and x86
    machines that have never been converted.
    
    Add an #ifdef block for the parts of the driver that are only used on
    those legacy machines.
    
    The two Rohm PMIC drivers use a gpio-keys device without an actual GPIO,
    passing an IRQ number instead. In order to keep this working both with
    and with CONFIG_GPIOLIB_LEGACY, change the gpio-keys driver to ignore
    the gpio number if an IRQ is passed.
    
    Link: https://lore.kernel.org/all/b3c94552-c104-42e3-be15-7e8362e8039e@gmail.com/
    Link: https://lore.kernel.org/all/afJXG4_rtaj3l2Dk@google.com/
    Link: https://lore.kernel.org/all/ajQ-CtU131FAJ9ES@google.com/
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    v3..v5: resend
        v2: skip the fake GPIO number passing from mfd
    
    This patch now has a small conflict with the rework of the rohm drivers
    to use software nodes. That patch is the one we want, and then
    the drivers/mfd changes here can get dropped, but the two need to
    get merged in the correct order.

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -23,8 +23,8 @@
 #include <linux/input.h>
 #include <linux/gpio_keys.h>
 #include <linux/workqueue.h>
-#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
+#include <linux/gpio/legacy.h>
 #include <linux/of.h>
 #include <linux/of_irq.h>
 #include <linux/spinlock.h>
@@ -528,7 +528,8 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 			 */
 			bdata->gpiod = NULL;
 		}
-	} else if (gpio_is_valid(button->gpio)) {
+#ifdef CONFIG_GPIOLIB_LEGACY
+	} else if (!button->irq && gpio_is_valid(button->gpio)) {
 		/*
 		 * Legacy GPIO number, so request the GPIO here and
 		 * convert it to descriptor.
@@ -546,6 +547,7 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 
 		if (button->active_low ^ gpiod_is_active_low(bdata->gpiod))
 			gpiod_toggle_active_low(bdata->gpiod);
+#endif
 	}
 
 	if (bdata->gpiod) {
@@ -583,8 +585,7 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 			if (irq < 0) {
 				error = irq;
 				dev_err_probe(dev, error,
-					      "Unable to get irq number for GPIO %d\n",
-					      button->gpio);
+					      "Unable to get irq number for GPIO\n");
 				return error;
 			}
 			bdata->irq = irq;
