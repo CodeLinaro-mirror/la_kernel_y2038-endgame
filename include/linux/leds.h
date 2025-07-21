8e40a1cef4bc3d8b5818b069a3198e4dbd4cf648
Arnd Bergmann <arnd@arndb.de>
2025-07-21 22:00:49 +0200

    leds: gpio: make legacy gpiolib interface optional
    There are still a handful of ancient mips/armv5/sh boards that use the
    gpio_led:gpio member to pass an old-style gpio number, but all modern
    users have been converted to gpio descriptors.
    
    Make the code that deals with this optional so the legacy interfaces
    can be left out for all normal builds.
    
    Reviewed-by: Linus Walleij <linusw@kernel.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/leds.h
+++ include/linux/leds.h
@@ -676,7 +676,9 @@ typedef int (*gpio_blink_set_t)(struct gpio_desc *desc, int state,
 struct gpio_led {
 	const char *name;
 	const char *default_trigger;
+#ifdef CONFIG_GPIOLIB_LEGACY
 	unsigned 	gpio;
+#endif
 	unsigned	active_low : 1;
 	unsigned	retain_state_suspended : 1;
 	unsigned	panic_indicator : 1;
