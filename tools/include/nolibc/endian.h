7f1bd05dab1cfed1fda9701eba5a643403833dca
Arnd Bergmann <arnd@arndb.de>
2025-11-10 14:54:26 +0100

    Kbuild: use nolibc for header check
    The header check is currently blocked on CONFIG_CC_CAN_LINK, but
    in practice it is enough to have a set of libc headers for the target
    architecture.
    
    The nolibc directory is almost all that is required here, so add
    a few tiny stubs to have enough data for building the headers being
    checked.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

---
+++ tools/include/nolibc/endian.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: LGPL-2.1 OR MIT */
+
+#ifndef _NOLIBC_ENDIAN_H
+#define _NOLIBC_ENDIAN_H
+
+#define __LITTLE_ENDIAN 1234
+#define __BIG_ENDIAN    4321
+
+#include <asm/byteorder.h>
+
+#endif /* _NOLIBC_SYS_ENDIAN_H */
---
+++ tools/include/nolibc/sys/socket.h
@@ -0,0 +1,22 @@
+#include "../nolibc.h"
+
+#ifndef _NOLIBC_SYS_SOCKET_H
+#define _NOLIBC_SYS_SOCKET_H
+
+#include <linux/socket.h>
+
+typedef __kernel_sa_family_t	sa_family_t;
+
+/*
+ *	1003.1g requires sa_family_t and that sa_data is char.
+ */
+
+struct sockaddr {
+	sa_family_t	sa_family;	/* address family, AF_xxx	*/
+	union {
+		char sa_data_min[14];		/* Minimum 14 bytes of protocol address	*/
+		__DECLARE_FLEX_ARRAY(char, sa_data);
+	};
+};
+
+#endif
