c1f0f5633f6998938b479de8b76912eeb19fe25e
Arnd Bergmann <arnd@arndb.de>
2025-11-28 23:17:24 +0100

    hexagon: fix build
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/hexagon/include/asm/signal.h
+++ arch/hexagon/include/asm/signal.h
@@ -20,9 +20,11 @@
 #ifndef _ASM_SIGNAL_H
 #define _ASM_SIGNAL_H
 
+#ifdef __KERNEL__
 extern unsigned long __rt_sigtramp_template[2];
 
 void do_signal(struct pt_regs *regs);
+#endif
 
 #include <asm-generic/signal.h>
 
