5d07af965833bcc6a10c0ab81342f36f5484dbac
Arnd Bergmann <arnd@arndb.de>
2026-06-12 11:39:45 +0200

    media: replace linux/gpio.h inclusions
    linux/gpio.h should no longer be used, convert these instead to
    either linux/gpio/consumer.h or linux/gpio/legacy.h as needed.
    
    Acked-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/media/platform/samsung/s3c-camif/camif-core.c
+++ drivers/media/platform/samsung/s3c-camif/camif-core.c
@@ -12,7 +12,7 @@
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/errno.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
