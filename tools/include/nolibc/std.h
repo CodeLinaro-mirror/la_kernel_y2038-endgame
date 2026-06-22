2e74b22f57f230148b44ded2120237c0a32f77c5
Arnd Bergmann <arnd@arndb.de>
2026-06-22 16:32:05 +0200

    nolibc: avoid unnecessary padding in data structures
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- tools/include/nolibc/std.h
+++ tools/include/nolibc/std.h
@@ -25,7 +25,7 @@ typedef unsigned int         mode_t;
 typedef   signed int          pid_t;
 typedef unsigned int          uid_t;
 typedef unsigned int          gid_t;
-typedef unsigned long       nlink_t;
+typedef unsigned int        nlink_t;
 typedef  int64_t              off_t;
 typedef   signed long     blksize_t;
 typedef   signed long      blkcnt_t;
